1c94dad77cf26060e3dca00cf61fe72223af4b1d
Alec Ritson <hello@itsalec.co.uk>
2022-01-12 11:13:26 +0000

    Docblock

--- packages/core/src/Base/FieldType.php
+++ packages/core/src/Base/FieldType.php
@@ -4,7 +4,18 @@ namespace GetCandy\Base;
 
 interface FieldType
 {
+    /**
+     * Return the field type value.
+     *
+     * @return mixed
+     */
     public function getValue();
 
+    /**
+     * Set the value for the field type.
+     *
+     * @param mixed $value
+     * @return void
+     */
     public function setValue($value);
 }
