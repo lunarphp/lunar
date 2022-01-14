da7f1a7ed658ed115ff12ef0dcc4ceff160e2ec1
Alec Ritson <hello@itsalec.co.uk>
2022-01-14 13:56:47 +0000

    Clean up and add exceptions

--- packages/core/src/Base/FieldTypeManifest.php
+++ packages/core/src/Base/FieldTypeManifest.php
@@ -2,6 +2,8 @@
 
 namespace GetCandy\Base;
 
+use GetCandy\Exceptions\FieldTypes\FieldTypeMissingException;
+use GetCandy\Exceptions\FieldTypes\InvalidFieldTypeException;
 use GetCandy\FieldTypes\ListField;
 use GetCandy\FieldTypes\Number;
 use GetCandy\FieldTypes\Text;
@@ -36,16 +38,21 @@ class FieldTypeManifest
     public function add($classname)
     {
         if (!class_exists($classname)) {
-            //
+            throw new FieldTypeMissingException($classname);
         }
 
         if (!(app()->make($classname) instanceof FieldType)) {
-            //
+            throw new InvalidFieldTypeException($classname);
         }
 
         $this->fieldTypes->push($classname);
     }
 
+    /**
+     * Return the fieldtypes.
+     *
+     * @return \Illuminate\Support\Collection
+     */
     public function getTypes()
     {
         return $this->fieldTypes->map(fn ($type) => app()->make($type));
---
+++ packages/core/src/Exceptions/FieldTypes/InvalidFieldTypeException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace GetCandy\Exceptions\FieldTypes;
+
+use Exception;
+
+class InvalidFieldTypeException extends Exception
+{
+    public function __construct($classname)
+    {
+        $this->message = __('getcandy::exceptions.invalid_fieldtype', [
+          'class' => $classname,
+        ]);
+    }
+}
