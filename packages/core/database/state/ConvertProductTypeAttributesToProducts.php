208c33d45a260d729dcb28bc3588e95fb07a59ea
Alec Ritson <hello@itsalec.co.uk>
2022-01-17 09:58:39 +0000

    Determine if this can run

--- packages/core/database/state/ConvertProductTypeAttributesToProducts.php
+++ packages/core/database/state/ConvertProductTypeAttributesToProducts.php
@@ -5,12 +5,17 @@ namespace GetCandy\Database\State;
 use GetCandy\Models\Product;
 use GetCandy\Models\ProductType;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class ConvertProductTypeAttributesToProducts
 {
     public function run()
     {
         $prefix = config('getcandy.database.table_prefix');
+
+        if (!$this->canRun()) {
+            return;
+        }
 
         DB::table("{$prefix}attributes")
             ->whereAttributeType(ProductType::class)
@@ -23,5 +28,13 @@ class ConvertProductTypeAttributesToProducts
             ->update([
                 'attributable_type' => Product::class,
             ]);
+    }
+
+    protected function canRun()
+    {
+        $prefix = config('getcandy.database.table_prefix');
+
+        return Schema::hasTable("{$prefix}attributes") &&
+            Schema::hasTable("{$prefix}attribute_groups");
     }
 }
