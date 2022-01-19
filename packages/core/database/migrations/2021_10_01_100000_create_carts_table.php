2688a73bbe555e2ee43ec5bf779b00b4c5144a92
Alec Ritson <hello@itsalec.co.uk>
2022-01-19 13:05:23 +0000

    Add userForeignKey helper

--- packages/core/database/migrations/2021_10_01_100000_create_carts_table.php
+++ packages/core/database/migrations/2021_10_01_100000_create_carts_table.php
@@ -9,12 +9,8 @@ class CreateCartsTable extends Migration
     public function up()
     {
         Schema::create($this->prefix.'carts', function (Blueprint $table) {
-            $userModel = config('auth.providers.users.model');
-
             $table->bigIncrements('id');
-            $table->foreignId('user_id')->nullable()->constrained(
-                (new $userModel())->getTable()
-            );
+            $table->userForeignKey();
             $table->foreignId('merged_id')->nullable()->constrained($this->prefix.'carts');
             $table->foreignId('currency_id')->constrained($this->prefix.'currencies');
             $table->foreignId('channel_id')->constrained($this->prefix.'channels');
