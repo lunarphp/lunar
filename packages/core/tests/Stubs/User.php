189c9491a24f13285f5fbcc383b366d8e7cde06f
Joel Male <joel@joelmale.com>
2022-01-28 07:53:15 +1000

    Fix model extends for user

--- packages/core/tests/Stubs/User.php
+++ packages/core/tests/Stubs/User.php
@@ -3,11 +3,11 @@
 namespace GetCandy\Tests\Stubs;
 
 use GetCandy\Base\Traits\GetCandyUser;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory;
     use Notifiable;
