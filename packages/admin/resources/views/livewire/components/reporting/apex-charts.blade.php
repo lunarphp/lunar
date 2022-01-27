0a252e062c327fe77355cd2dc31dcf638f1ebab6
Alec Ritson <hello@itsalec.co.uk>
2022-01-27 15:24:45 +0000

    Remove duplicate init since upgrade

--- packages/admin/resources/views/livewire/components/reporting/apex-charts.blade.php
+++ packages/admin/resources/views/livewire/components/reporting/apex-charts.blade.php
@@ -1,15 +1,14 @@
 <div
     style="width: 100%; height: 100%;"
     x-data="{
-    init() {
-        var el = document.querySelector('#' + this.$wire.get('key'));
-        var options = this.$wire.get('options');
+        init() {
+            var el = document.querySelector('#' + this.$wire.get('key'));
+            var options = this.$wire.get('options');
 
-        var chart = new ApexCharts(el, options);
-        chart.render();
-    }
-}"
-    x-init="init()"
+            var chart = new ApexCharts(el, options);
+            chart.render();
+        }
+    }"
 >
     <div id="{{ $this->key }}"></div>
 </div>
