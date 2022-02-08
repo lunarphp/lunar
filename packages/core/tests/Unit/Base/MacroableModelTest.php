f8c5312d27db41259c84ed8f1a4e6e9f3db43bef
edcoreweb <edcoreweb@gmail.com>
2022-02-08 16:39:20 +0200

    Add macro support to all models

---
+++ packages/core/tests/Unit/Base/MacroableModelTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace GetCandy\Tests\Unit\Base;
+
+use GetCandy\Models\Product;
+use GetCandy\Tests\TestCase;
+
+/**
+ * @group reference
+ */
+class MacroableModelTest extends TestCase
+{
+    protected $model;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->model = new Product();
+    }
+
+    /** @test */
+    public function can_register_a_new_macro()
+    {
+        $this->model::macro('newMethod', function () {
+            return 'newValue';
+        });
+
+        $this->assertEquals('newValue', $this->model->newMethod());
+        $this->assertEquals('newValue', $this->model::newMethod());
+    }
+
+    /** @test */
+    public function can_register_a_new_macro_and_be_invoked()
+    {
+        $this->model::macro('newMethod', new class()
+        {
+            public function __invoke()
+            {
+                return 'newValue';
+            }
+        });
+
+        $this->assertEquals('newValue', $this->model->newMethod());
+        $this->assertEquals('newValue', $this->model::newMethod());
+    }
+}
