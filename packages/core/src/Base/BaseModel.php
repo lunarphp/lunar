29f614e7fac966f6a9370267b1aed9153f805090
edcoreweb <edcoreweb@gmail.com>
2022-02-08 17:36:05 +0200

    Add macro support to all models (#145)
    * Add macro support to all models
    
    * Fix tests

--- packages/core/src/Base/BaseModel.php
+++ packages/core/src/Base/BaseModel.php
@@ -3,9 +3,14 @@
 namespace GetCandy\Base;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Traits\Macroable;
 
 abstract class BaseModel extends Model
 {
+    use Macroable {
+        __call as macroCall;
+    }
+
     /**
      * Create a new instance of the Model.
      *
@@ -16,5 +21,33 @@ abstract class BaseModel extends Model
         parent::__construct($attributes);
 
         $this->setTable(config('getcandy.database.table_prefix').$this->getTable());
+    }
+
+    /**
+     * Handle dynamic method calls into the model.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        if (static::hasMacro($method)) {
+            return $this->macroCall($method, $parameters);
+        }
+
+        return parent::__call($method, $parameters);
+    }
+
+    /**
+     * Handle dynamic static method calls into the model.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        return parent::__callStatic($method, $parameters);
     }
 }
