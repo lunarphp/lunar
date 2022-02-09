4c94adbf5dea06eb2b6c2390de6b1aea6bb00473
Jakub Szymanowski <jakub.szymanowski@emergeconsulting.pl>
2022-02-09 20:50:30 +0100

    Fix rich text attribute creation in getcandy:install command. Explicitly set the toggle on/off value to true/false.

--- packages/admin/resources/views/field-types/text/settings.blade.php
+++ packages/admin/resources/views/field-types/text/settings.blade.php
@@ -4,5 +4,5 @@
   :error="$errors->first('attribute.configuration.richtext')"
   :disabled="!!$attribute->system"
 >
-  <x-hub::input.toggle :disabled="!!$attribute->system" wire:model="attribute.configuration.richtext" id="fieldType" />
+  <x-hub::input.toggle :disabled="!!$attribute->system" :on-value="true" :off-value="false" wire:model="attribute.configuration.richtext" id="fieldType" />
 </x-hub::input.group>
