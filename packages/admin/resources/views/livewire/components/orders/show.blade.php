dc701f77ddc76e224e702a5d8a7d819af887b09b
markmead <itsmarkmead@gmail.com>
2022-02-24 08:43:10 +0000

    Initial layout work

--- packages/admin/resources/views/livewire/components/orders/show.blade.php
+++ packages/admin/resources/views/livewire/components/orders/show.blade.php
@@ -1,149 +1,208 @@
-<div class="flex-col px-12 mx-auto space-y-4 max-w-7xl">
-  <div class="flex items-center justify-between">
-    <strong class="text-lg font-bold md:text-2xl">Orders // #{{ $order->id }}</strong>
+<div class="flex-col px-12 mx-auto max-w-7xl">
+  <div class="flex items-center">
+    <h1 class="text-lg font-bold md:text-2xl">
+      <span class="text-gray-500">
+        Orders //
+      </span>
+
+      #{{ $order->id }}
+    </h1>
+
+    <time class="block ml-8 text-sm text-gray-500">
+      Today at 08:00pm GMT
+    </time>
+
     {{-- <div>
       <x-hub::button type="button" wire:click="$set('updatingStatus', true)">Update Status</x-hub::button>
     </div> --}}
   </div>
 
-  <div class="flex">
-    <div class="w-2/3">
-      <div class="flex w-full my-4 text-xs text-gray-600">
-        <div class="px-3 pl-0 border-r">
-          <button class="inline-flex items-center">
-            <x-hub::icon ref="printer" style="solid" class="w-3 mr-1" />
-            Print
-          </button>
-        </div>
+  <div class="grid items-start grid-cols-3 gap-8 mt-8">
+    <div class="col-span-2">
+      <div class="flex items-center text-sm text-gray-600 divide-x">
+        <button class="inline-flex items-center">
+          <x-hub::icon
+            ref="printer"
+            style="solid"
+            class="w-4 mr-2"
+          />
 
-        <div class="px-3 border-r">
-          <button class="inline-flex items-center">
-            <x-hub::icon ref="rewind" style="solid" class="w-3 mr-1" />
-            Refund
-          </button>
-        </div>
+          Print
+        </button>
 
-        <div class="px-3 border-r">
-          <button class="inline-flex items-center">
-            <x-hub::icon ref="credit-card" style="solid" class="w-3 mr-1" />
-            Add Payment
-          </button>
-        </div>
+        <button class="inline-flex items-center">
+          <x-hub::icon
+            ref="rewind"
+            style="solid"
+            class="w-4 mr-2"
+          />
 
-        <div class="px-3 border-r">
-          <button class="inline-flex items-center">
-            <x-hub::icon ref="flag" style="solid" class="w-3 mr-1" />
-            Update Status
-          </button>
-        </div>
+          Refund
+        </button>
 
-        <div class="px-3">
-          <button class="inline-flex items-center">
-            More Actions
-            <x-hub::icon ref="chevron-down" style="solid" class="w-3 ml-1" />
-          </button>
-        </div>
+        <button class="inline-flex items-center">
+          <x-hub::icon
+            ref="credit-card"
+            style="solid"
+            class="w-4 mr-2"
+          />
+
+          Add Payment
+        </button>
+
+        <button class="inline-flex items-center">
+          <x-hub::icon
+            ref="flag"
+            style="solid"
+            class="w-4 mr-2"
+          />
+
+          Update Status
+        </button>
+
+        <button class="inline-flex items-center">
+          More Actions
+
+          <x-hub::icon
+            ref="chevron-down"
+            style="solid"
+            class="w-4 ml-2"
+          />
+        </button>
       </div>
 
-      <div>
-        <header>
+      <div class="mt-4">
+        {{-- <header>
           <h3>Order Lines ({{ $this->physicalLines->count() }})</h3>
-        </header>
-        <div class="mt-4 space-y-2">
-          <div class="bg-white rounded shadow ">
-            @foreach($this->visibleLines as $line)
-              <div class="flex w-full p-4 space-x-2 border-b">
-                <x-hub::input.checkbox />
-                <button>
-                  <x-hub::icon ref="chevron-right" style="solid" class="w-6 text-gray-400 hover:text-gray-600" />
-                </button>
+        </header> --}}
 
-                <div class="w-8">
-                  <img src="{{ $line->purchasable->getThumbnail() }}" class="w-6 rounded" />
-                </div>
+        <div class="p-4 bg-white border border-gray-200 rounded-lg">
+          <div class="flow-root">
+            <div class="-my-4 divide-y divide-gray-200">
+              @foreach ($this->visibleLines as $line)
+                <div class="flex items-start w-full py-4 space-x-2">
+                  {{-- <x-hub::input.checkbox /> --}}
 
-                <div>
-                  <p class="text-sm leading-tight text-gray-800">{{ $line->description }}</p>
-                </div>
-
-                <div class="w-1/3 text-sm text-right grow">
-                  <span class="mr-2">{{ $line->quantity }} @ {{ $line->unit_price->formatted }}</span>
-                  {{ $line->sub_total->formatted }}
-                </div>
-
-                <div>
-                  <button class="text-gray-400 hover:text-gray-800">
-                    <x-hub::icon ref="dots-vertical" style="solid" />
-                  </button>
-                </div>
-              </div>
-            @endforeach
-            @if($this->physicalLines->count() > $maxLines)
-              <button
-                type="button"
-                wire:click="$set('allLinesVisible', {{ !$allLinesVisible }})"
-                class="w-full py-1 text-sm text-center text-gray-600 bg-gray-200 hover:bg-gray-300"
-              >
-                @if(!$allLinesVisible)
-                  Show remaining lines
-                @else
-                  Collapse lines
-                @endif
-              </button>
-            @endif
-
-            <div class="p-3 space-y-2 text-sm">
-              @foreach($this->shippingLines as $shippingLine)
-                <div class="flex justify-between w-full p-4 border rounded">
-                  <div class="flex items-center">
-                    <x-hub::icon ref="truck" />
-                    <span class="block ml-2">{!! $shippingLine->description !!}</span>
+                  <div class="flex-shrink-0">
+                    <input
+                      class="w-4 h-4 text-blue-500 border-gray-200 rounded form-checkbox"
+                      type="checkbox"
+                    >
                   </div>
-                  <span>{{ $shippingLine->sub_total->formatted }}</span>
+
+                  <div class="flex-shrink-0">
+                    <img
+                      src="{{ $line->purchasable->getThumbnail() }}"
+                      class="object-contain w-12 h-12 border border-gray-100 rounded"
+                    />
+                  </div>
+
+                  <div class="flex-1">
+                    <details class="group">
+                      <summary class="grid grid-cols-3 gap-4 cursor-pointer">
+                        <div class="flex col-span-2">
+                          <x-hub::icon
+                            ref="chevron-right"
+                            style="solid"
+                            class="w-6 text-gray-400 transition group-hover:text-gray-600 group-open:rotate-90"
+                          />
+
+                          <p class="ml-2 text-sm font-bold leading-tight">
+                            {{ $line->description }}
+                          </p>
+                        </div>
+
+                        <p class="text-xs text-gray-500">
+                          KB123450ASDB
+                        </p>
+                      </summary>
+
+                      <div class="pl-8">
+                        <p>Hello</p>
+                      </div>
+                    </details>
+                  </div>
+
+                  <div class="flex-shrink-0 text-sm text-right">
+                    <span class="mr-2">{{ $line->quantity }} @ {{ $line->unit_price->formatted }}</span>
+                    {{ $line->sub_total->formatted }}
+                  </div>
+
+                  <div>
+                    <button class="text-gray-400 hover:text-gray-800">
+                      <x-hub::icon
+                        ref="dots-vertical"
+                        style="solid"
+                      />
+                    </button>
+                  </div>
                 </div>
               @endforeach
             </div>
+          </div>
+          @if ($this->physicalLines->count() > $maxLines)
+            <button
+              type="button"
+              wire:click="$set('allLinesVisible', {{ !$allLinesVisible }})"
+              class="w-full py-1 text-sm text-center text-gray-600 bg-gray-200 hover:bg-gray-300"
+            >
+              @if (!$allLinesVisible)
+                Show remaining lines
+              @else
+                Collapse lines
+              @endif
+            </button>
+          @endif
 
-            <div class="p-3">
-              <div class="flex p-3 bg-gray-50">
-                <div class="grow">
-                  <strong class="text-sm">Notes</strong>
-                  <p class="text-sm">
-                    @if($order->notes)
-                      {{ $order->notes }}
-                    @else
-                      <span class="text-gray-500">No notes on this order</span>
-                    @endif
-                  </p>
+          <div class="p-3 space-y-2 text-sm">
+            @foreach ($this->shippingLines as $shippingLine)
+              <div class="flex justify-between w-full p-4 border rounded">
+                <div class="flex items-center">
+                  <x-hub::icon ref="truck" />
+                  <span class="block ml-2">{!! $shippingLine->description !!}</span>
                 </div>
+                <span>{{ $shippingLine->sub_total->formatted }}</span>
+              </div>
+            @endforeach
+          </div>
 
-                <div class="w-1/3 text-sm text-right">
-                  <dl class="space-y-2">
-                    <div class="flex items-center justify-between">
-                      <dt>Sub Total</dt>
-                      <dd>{{ $order->sub_total->formatted }}</dd>
-                    </div>
-                    <div class="flex items-center justify-between">
-                      <dt>Shipping Total</dt>
-                      <dd>{{ $order->shipping_total->formatted }}</dd>
-                    </div>
-                    @foreach($order->tax_breakdown as $tax)
+          <div class="p-3">
+            <div class="flex p-3 bg-gray-50">
+              <div class="grow">
+                <strong class="text-sm">Notes</strong>
+                <p class="text-sm">
+                  @if ($order->notes)
+                    {{ $order->notes }}
+                  @else
+                    <span class="text-gray-500">No notes on this order</span>
+                  @endif
+                </p>
+              </div>
+
+              <div class="w-1/3 text-sm text-right">
+                <dl class="space-y-2">
+                  <div class="flex items-center justify-between">
+                    <dt>Sub Total</dt>
+                    <dd>{{ $order->sub_total->formatted }}</dd>
+                  </div>
+                  <div class="flex items-center justify-between">
+                    <dt>Shipping Total</dt>
+                    <dd>{{ $order->shipping_total->formatted }}</dd>
+                  </div>
+                  @foreach ($order->tax_breakdown as $tax)
                     <div class="flex items-center justify-between">
                       <dt>{{ $tax->description }}</dt>
                       <dd>{{ $tax->total->formatted }}</dd>
                     </div>
-                    @endforeach
-                    <div class="flex items-center justify-between font-bold">
-                      <dt>Total</dt>
-                      <dd>{{ $order->total->formatted }}</dd>
-                    </div>
-                  </dl>
-                </div>
+                  @endforeach
+                  <div class="flex items-center justify-between font-bold">
+                    <dt>Total</dt>
+                    <dd>{{ $order->total->formatted }}</dd>
+                  </div>
+                </dl>
               </div>
             </div>
           </div>
-
-
         </div>
 
         <div class="mt-4">
@@ -151,7 +210,7 @@
             <h3>Transactions</h3>
           </header>
 
-          @foreach($order->transactions as $transaction)
+          @foreach ($order->transactions as $transaction)
             <div class="flex p-3 bg-white rounded">
               <div>
                 {{ $transaction->status }}
@@ -174,6 +233,119 @@
           @endforeach
         </div>
       </div>
+    </div>
+
+    <div class="sticky space-y-4 top-4">
+      <div class="flex items-center justify-between">
+        <p class="font-bold truncate">
+          Alec Ritson
+        </p>
+
+        <button
+          class="flex-shrink-0 px-5 py-3 ml-4 text-xs font-bold bg-gray-200 rounded"
+          type="button"
+        >
+          View User
+        </button>
+      </div>
+
+      <div
+        class="flex items-center px-4 py-3 text-green-700 border border-current rounded bg-green-50"
+        role="alert"
+      >
+        <x-hub::icon
+          ref="check-circle"
+          style="outline"
+          class="w-5"
+        />
+
+        <p class="ml-3 text-sm font-bold">
+          Payment Received
+        </p>
+      </div>
+
+      <dl class="space-y-4 text-sm">
+        <div class="grid grid-cols-2 gap-4">
+          <dt class="font-bold">
+            Reference:
+          </dt>
+
+          <dd class="text-right">
+            2022-02-0001
+          </dd>
+        </div>
+
+        <div class="grid grid-cols-2 gap-4">
+          <dt class="font-bold">
+            Channel:
+          </dt>
+
+          <dd class="text-right">
+            Webstore
+          </dd>
+        </div>
+      </dl>
+
+      <section class="p-4 bg-white border border-gray-200 rounded-lg">
+        <div class="flex items-center justify-between">
+          <p class="font-bold">
+            Shipping Address
+          </p>
+
+          <button
+            class="px-3 py-1.5 text-xs font-bold bg-gray-200 rounded"
+            type="button"
+          >
+            Edit
+          </button>
+        </div>
+
+        <address class="mt-4 text-sm not-italic text-gray-700">
+          Alec Ritson <br>
+          16 Mons Way <br>
+          Maldon <br>
+          CM9 6FU <br>
+          United Kingdom
+        </address>
+      </section>
+
+      <section class="p-4 bg-white border border-gray-200 rounded-lg">
+        <p class="font-bold">
+          Billing Address
+        </p>
+
+        <p class="mt-4 text-sm text-gray-700">
+          Same as shipping address
+        </p>
+      </section>
+
+      <section class="p-4 bg-white border border-gray-200 rounded-lg">
+        <p class="font-bold">
+          Additional Details
+        </p>
+
+        <dl class="mt-4 space-y-4 text-sm">
+          <div class="grid grid-cols-2 gap-4">
+            <dt class="font-bold">
+              Metafield:
+            </dt>
+
+            <dd>
+              Lorem ipsum dolor sit amet.
+            </dd>
+          </div>
+
+          <div class="grid grid-cols-2 gap-4">
+            <dt class="font-bold">
+              Metafield 2:
+            </dt>
+
+            <dd>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </dd>
+          </div>
+        </dl>
+      </section>
     </div>
   </div>
 
@@ -217,7 +389,7 @@
           <div>
             <span class="block text-xs">Date</span>
             <strong class="text-sm font-bold">
-              @if($order->placed_at)
+              @if ($order->placed_at)
                 {{ $order->placed_at->format('jS M Y') }}
               @else
                 {{ $order->created_at->format('jS M Y') }}
@@ -234,7 +406,7 @@
           <div>
             <span class="block text-xs">Time</span>
             <strong class="text-sm font-bold">
-              @if($order->placed_at)
+              @if ($order->placed_at)
                 {{ $order->placed_at->format('h:ma') }}
               @else
                 {{ $order->created_at->format('h:ma') }}
@@ -251,10 +423,10 @@
   </div>
 
   <div class="grid grid-cols-3 gap-4">
-    @if($this->shipping)
+    @if ($this->shipping)
       <div class="p-4 bg-white rounded-lg">
         <h3 class="font-semibold text-gray-900">Shipping Option{{ $this->shippingLines->count() > 1 ? 's' : null }}</h3>
-        @foreach($this->shippingLines as $line)
+        @foreach ($this->shippingLines as $line)
           {{ $line->description }}
         @endforeach
       </div>
@@ -264,16 +436,16 @@
           <span class="adr">
             <span class="block">{{ $this->shipping->fullName }}</span>
             <span class="block">{{ $this->shipping->line_one }}</span>
-            @if($this->shipping->line_two)
+            @if ($this->shipping->line_two)
               <span class="block">{{ $this->shipping->line_two }}</span>
             @endif
-            @if($this->shipping->line_three)
+            @if ($this->shipping->line_three)
               <span class="block">{{ $this->shipping->line_three }}</span>
             @endif
-            @if($this->shipping->city)
+            @if ($this->shipping->city)
               <span class="block">{{ $this->shipping->city }}</span>
             @endif
-            @if($this->shipping->state)
+            @if ($this->shipping->state)
               <span class="block">{{ $this->shipping->state }}</span>
             @endif
             <span class="block">{{ $this->shipping->postcode }}</span>
@@ -287,16 +459,16 @@
           <span class="adr">
             <span class="block">{{ $this->billing->fullName }}</span>
             <span class="block">{{ $this->billing->line_one }}</span>
-            @if($this->billing->line_two)
+            @if ($this->billing->line_two)
               <span class="block">{{ $this->billing->line_two }}</span>
             @endif
-            @if($this->billing->line_three)
+            @if ($this->billing->line_three)
               <span class="block">{{ $this->billing->line_three }}</span>
             @endif
-            @if($this->billing->city)
+            @if ($this->billing->city)
               <span class="block">{{ $this->billing->city }}</span>
             @endif
-            @if($this->billing->state)
+            @if ($this->billing->state)
               <span class="block">{{ $this->billing->state }}</span>
             @endif
             <span class="block">{{ $this->billing->postcode }}</span>
@@ -322,7 +494,7 @@
             </tr>
           </thead>
           <tbody>
-            @foreach($this->order->lines as $line)
+            @foreach ($this->order->lines as $line)
             <tr class="text-sm bg-white even:bg-gray-50">
               <td class="p-2">{{ $line->identifier }}</td>
               <td class="p-2">{{ $line->description }}</td>
@@ -341,7 +513,7 @@
               <td class="p-2 text-sm">Sub Total</td>
               <td>{{ $order->sub_total->formatted }}</td>
             </tr>
-            @foreach($order->tax_breakdown as $tax)
+            @foreach ($order->tax_breakdown as $tax)
               <tr>
                 <td colspan="6"></td>
                 <td class="p-2 text-sm">{{ $tax->description }}</td>
@@ -377,7 +549,7 @@
             </tr>
           </thead>
           <tbody>
-            @foreach($order->transactions as $transaction)
+            @foreach ($order->transactions as $transaction)
               <tr>
                 <td class="p-2">{{ $transaction->status }}</td>
                 <td class="p-2">{{ $transaction->success ? 'Yes' : 'No' }}</td>
@@ -397,7 +569,7 @@
 
   <x-hub::slideover title="Update Status" wire:model="updatingStatus">
     <x-hub::input.select wire:model="order.status">
-      @foreach($this->statuses as $value => $label)
+      @foreach ($this->statuses as $value => $label)
         <option value="{{ $value }}">{{ $label }}</option>
       @endforeach
     </x-hub::input.select>
