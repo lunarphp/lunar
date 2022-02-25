a2af992505a4174513d72c7724078f880efd965e
Alec Ritson <hello@itsalec.co.uk>
2022-02-25 13:00:42 +0000

    Support custom formatter for filter

--- packages/admin/src/DataTransferObjects/TableFilter.php
+++ packages/admin/src/DataTransferObjects/TableFilter.php
@@ -2,12 +2,23 @@
 
 namespace GetCandy\Hub\DataTransferObjects;
 
+use Closure;
+
 class TableFilter
 {
     public function __construct(
         public string $heading,
-        public string $field
+        public string $field,
+        public ?Closure $formatter = null
     ) {
         // ..
+    }
+
+    public function format($value)
+    {
+        if ($this->formatter) {
+            return call_user_func($this->formatter, $value);
+        }
+        return $value;
     }
 }
