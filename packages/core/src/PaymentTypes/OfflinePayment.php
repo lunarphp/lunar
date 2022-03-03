8dc1d6b221895a745fb546bea38119a80ebb6df4
Alec Ritson <hello@itsalec.co.uk>
2022-03-03 15:02:10 +0000

    Make offline payment do something

--- packages/core/src/PaymentTypes/OfflinePayment.php
+++ packages/core/src/PaymentTypes/OfflinePayment.php
@@ -2,15 +2,31 @@
 
 namespace GetCandy\PaymentTypes;
 
+use GetCandy\Base\DataTransferObjects\PaymentRelease;
+
 class OfflinePayment extends AbstractPayment
 {
+    /**
+     * {@inheritDoc}
+     */
     public function release()
     {
-        // Charge!
+        if (!$this->order) {
+            if (!$this->order = $this->cart->order) {
+                $this->order = $this->cart->getManager()->createOrder();
+            }
+        }
+
+        $this->order->update([
+            'status' => $this->config['released'] ?? null,
+            'placed_at' => now(),
+        ]);
+
+        return new PaymentRelease(success: true);
     }
 
     public function refund(int $amount)
     {
-
+        //
     }
 }
