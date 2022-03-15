2ac14b81176034e9c70602c90d9d0784984f4557
Alec Ritson <hello@itsalec.co.uk>
2022-03-15 08:40:17 +0000

    Add test URL Generator to the tests

--- packages/admin/tests/TestCase.php
+++ packages/admin/tests/TestCase.php
@@ -5,6 +5,7 @@ namespace GetCandy\Hub\Tests;
 use Cartalyst\Converter\Laravel\ConverterServiceProvider;
 use GetCandy\GetCandyServiceProvider;
 use GetCandy\Hub\AdminHubServiceProvider;
+use GetCandy\Tests\Stubs\TestUrlGenerator;
 use Illuminate\Support\Facades\Config;
 use Kalnoy\Nestedset\NestedSetServiceProvider;
 use Livewire\LivewireServiceProvider;
@@ -20,6 +21,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
         Config::set('auth.guards.staff', [
             'driver' => 'getcandyhub',
         ]);
+        Config::set('getcandy.urls.generator', TestUrlGenerator::class);
     }
 
     protected function getPackageProviders($app)
