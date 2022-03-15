2b562c176624c764c6d3087c2d1c0e1aff422b0b
Alec Ritson <hello@itsalec.co.uk>
2022-03-15 14:17:21 +0000

    Update orders.php

--- packages/admin/resources/lang/en/orders.php
+++ packages/admin/resources/lang/en/orders.php
@@ -9,7 +9,7 @@ return [
     'index.save_search.title' => 'Save Search',
     'index.save_search.btn' => 'Save Search',
     'index.all_orders' => 'All Orders',
-    'index.search_placeholder' => 'Search by reference or customer name',
+    'index.search_placeholder' => 'Search...',
     'index.update_status.btn' => 'Update Status',
     'index.update_status.title' => 'Update Status',
     'index.delete_saved_search.title' => 'Delete saved search',
