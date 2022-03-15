aa23f021ec564d4dd4b3ee79dc0be3bb2025757f
Alec Ritson <hello@itsalec.co.uk>
2022-03-15 08:27:53 +0000

    Generate URLs by default

--- packages/core/config/urls.php
+++ packages/core/config/urls.php
@@ -21,6 +21,5 @@ return [
   | ships with GetCandy, which by default will use the name attribute.
   |
   */
-    'generator' => null,
-    // 'generator' => GetCandy\Generators\UrlGenerator::class
+    'generator' => GetCandy\Generators\UrlGenerator::class
 ];
