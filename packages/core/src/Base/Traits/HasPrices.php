20f68ca6cc49ad91e6c0e9b17f1629de254dcea7
Glenn Jacobs <glenn@neondigital.co.uk>
2022-03-25 10:57:22 +0000

    Initial concept

--- packages/core/src/Base/Traits/HasPrices.php
+++ packages/core/src/Base/Traits/HasPrices.php
@@ -2,7 +2,11 @@
 
 namespace GetCandy\Base\Traits;
 
+use GetCandy\Facades\Pricing;
 use GetCandy\Models\Price;
+use GetCandy\Models\Currency;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Collection;
 
 trait HasPrices
 {
@@ -25,5 +29,38 @@ trait HasPrices
     public function basePrices()
     {
         return $this->prices()->whereTier(1)->whereNull('customer_group_id');
+    }
+
+    /**
+     * Return a Price based upon criteria.
+     *
+     * @param  int $qty
+     * @param  Currency  $currency
+     * @param  Authenticatable  $user
+     * @param  mixed  $customerGroups
+     *
+     * @return \GetCandy\DataTypes\Price
+     */
+    public function getPrice(int $qty = 1, Currency $currency = null, Authenticatable $user = null, $customerGroups = null)
+    {
+        $pricing = Pricing::qty($qty);
+
+        if ($currency) {
+            $pricing->currency($currency);
+        }
+
+        if ($user) {
+            $pricing->user($user);
+        }
+
+        if ($customerGroups) {
+            if ($customerGroups instanceof Collection)) {
+                $pricing->customerGroups($customerGroup);
+            } else {
+                $pricing->customerGroup($customerGroup);
+            }
+        }
+
+        return $pricing->for($this);
     }
 }
