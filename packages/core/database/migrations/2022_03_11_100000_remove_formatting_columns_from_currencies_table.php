bbe97061bf4f07fcd1c8a50ac4e578f7d53406f6
Glenn Jacobs <glenn@neondigital.co.uk>
2022-03-25 12:35:19 +0000

    Price formatting (#222)
    * Initial idea for formatting prices
    
    * Apply fixes from StyleCI
    
    * Remove Currency fields no longer required
    
    * Update Price parsing
    
    * Use decimal value rather than int when formatting
    
    * Remove old currency fields from admin hub screens
    
    * Fix tests
    
    * Apply fixes from StyleCI
    
    * Fix odd NumberFormat issue in tests
    
    * Adding tests for number formatting
    
    * Add documentation
    
    * Updated changelog
    
    * Consider currency decimal places when formatting prices
    
    * Test fix
    
    * Trying to fix up tests again
    
    * Apply fixes from StyleCI
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

---
+++ packages/core/database/migrations/2022_03_11_100000_remove_formatting_columns_from_currencies_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use GetCandy\Base\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveFormattingColumnsFromCurrenciesTable extends Migration
+{
+    public function up()
+    {
+        Schema::table($this->prefix.'currencies', function (Blueprint $table) {
+            $table->dropColumn(['format', 'decimal_point', 'thousand_point']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table($this->prefix.'currencies', function ($table) {
+            $table->string('format');
+            $table->string('decimal_point');
+            $table->string('thousand_point');
+        });
+    }
+}
