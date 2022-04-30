a55cc97bdf2d768be79c12a475b224a88e706f73
Jakub Theimer <jakub@dy.st>
2022-05-01 01:30:45 +0200

    add abstract asset class

---
+++ packages/admin/src/Assets/Asset.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace GetCandy\Hub\Assets;
+
+use DateTime;
+use Illuminate\Contracts\Support\Responsable;
+use Symfony\Component\HttpFoundation\Response;
+
+abstract class Asset implements Responsable
+{
+    /**
+     * Name of asset.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Path of Asset.
+     *
+     * @var string|null
+     */
+    protected $path;
+
+    /**
+     * Construct a new Asset instance.
+     *
+     * @param string|self $name
+     * @param string|null $path
+     */
+    public function __construct($name, $path)
+    {
+        $this->name = $name;
+        $this->path = $path;
+    }
+
+    /**
+     * Get asset name.
+     *
+     * @return string
+     */
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get asset path.
+     *
+     * @return string|null
+     */
+    public function path(): ?string
+    {
+        return $this->path;
+    }
+
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function toResponse($request): Response
+    {
+        return response(
+            file_get_contents($this->path),
+            200,
+            $this->toResponseHeaders(),
+        )->setLastModified(DateTime::createFromFormat('U', (string) filemtime($this->path)));
+    }
+
+    /**
+     * Get asset url.
+     *
+     * @return string
+     */
+    abstract public function url(): string;
+
+    /**
+     * Get response headers.
+     *
+     * @return array<string, string>
+     */
+    abstract public function toResponseHeaders(): array;
+}
