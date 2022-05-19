4b7aabfa2e2659edb4ab02b1379be284951d1b13
Glenn Jacobs <glenn@neondigital.co.uk>
2022-05-19 09:36:55 +0100

    Remove double set on $traits

--- packages/core/src/helpers.php
+++ packages/core/src/helpers.php
@@ -5,7 +5,7 @@ use GetCandy\Base\Traits\GetCandyUser;
 if (! function_exists('is_getcandy_user')) {
     function is_getcandy_user($user)
     {
-        $traits = $traits = class_uses_recursive($user);
+        $traits = class_uses_recursive($user);
 
         return in_array(GetCandyUser::class, $traits);
     }
