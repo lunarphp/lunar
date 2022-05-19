1d29af7a7e2fe8bffd6c7775c2fc8eb41315c2fc
Alec Ritson <hello@itsalec.co.uk>
2022-05-19 14:39:40 +0100

    Feat - Allow product restoring (#296)
    * Show filters and statuses
    
    * Apply fixes from StyleCI
    
    * Update changelog
    
    * Clean up
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- packages/admin/resources/views/livewire/components/products/index.blade.php
+++ packages/admin/resources/views/livewire/components/products/index.blade.php
@@ -8,43 +8,6 @@
     </div>
   </div>
 
-  <div>
-    {{-- <div class="sm:hidden">
-      <label for="tabs" class="sr-only">Select a tab</label>
-      <select id="tabs" name="tabs" class="block w-full py-2 pl-3 pr-10 text-base border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-        <option>My Account</option>
-
-        <option>Company</option>
-
-        <option selected>Team Members</option>
-
-        <option>Billing</option>
-      </select>
-    </div> --}}
-    {{-- <div class="hidden sm:block">
-      <div class="border-b border-gray-200">
-        <nav class="flex -mb-px space-x-8" aria-label="Tabs">
-          <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" -->
-          <a href="#" class="px-1 py-4 text-sm font-medium text-indigo-600 border-b-2 border-indigo-500 whitespace-nowrap">
-            All products
-          </a>
-
-          <a href="#" class="px-1 py-4 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:text-gray-700 hover:border-gray-300 whitespace-nowrap">
-            Featured
-          </a>
-
-          <a href="#" class="px-1 py-4 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:text-gray-700 hover:border-gray-300 whitespace-nowrap" aria-current="page">
-            Out of stock
-          </a>
-
-          <a href="#" class="inline-flex items-center px-1 py-4 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:text-gray-700 hover:border-gray-300 whitespace-nowrap">
-            <x-hub::icon ref="plus" style="solid" />
-            Add custom view
-          </a>
-        </nav>
-      </div>
-    </div> --}}
-  </div>
   <x-hub::table>
     <x-slot name="toolbar">
       <div class="p-4 space-y-4 border-b" x-data="{ filtersVisible: false }">
@@ -53,7 +16,9 @@
           <div>
             <x-hub::dropdown value="Actions" position="right">
               <x-slot name="options" >
-                <x-hub::dropdown.button>Export</x-hub::dropdown.button>
+                <x-hub::dropdown.button>
+                  {{ __('adminhub::global.export') }}
+                </x-hub::dropdown.button>
               </x-slot>
             </x-hub::dropdown>
           </div>
@@ -61,41 +26,24 @@
           <div class="flex items-center w-full space-x-4">
             <x-hub::input.text placeholder="Search by attribute or SKU" class="py-2" wire:model="search" />
 
-            {{-- <x-hub::button theme="gray" class="inline-flex items-center" @click.prevent="filtersVisible = !filtersVisible">
+            <x-hub::button theme="gray" class="inline-flex items-center" @click.prevent="filtersVisible = !filtersVisible">
               <x-hub::icon ref="filter" class="w-4 mr-1" />
-              Filter
-            </x-hub::button> --}}
+              {{ __('adminhub::global.filter') }}
+            </x-hub::button>
           </div>
         </div>
 
         <div class="grid grid-cols-4 gap-4" x-show="filtersVisible" x-cloak>
-          <x-hub::input.group label="Status" for="brand">
+          <x-hub::input.group :label="__('adminhub::global.status')" for="brand">
             <x-hub::input.select wire:model="filters.status">
-              <option>{{ __('adminhub::global.any') }}</option>
+              <option value>{{ __('adminhub::global.any') }}</option>
               <option value="published">{{ __('adminhub::global.published') }}</option>
               <option value="draft">{{ __('adminhub::global.draft') }}</option>
             </x-hub::input.select>
           </x-hub::input.group>
 
-          <x-hub::input.group label="Stock Level" for="brand">
-            <div class="grid grid-cols-2 gap-2">
-              <x-hub::input.text placeholder="From" />
-              <x-hub::input.text placeholder="To" />
-            </div>
-          </x-hub::input.group>
-
-          <x-hub::input.group label="Quantity Sold" for="brand">
-            <div class="grid grid-cols-2 gap-2">
-              <x-hub::input.text placeholder="From" />
-              <x-hub::input.text placeholder="To" />
-            </div>
-          </x-hub::input.group>
-
-          <x-hub::input.group label="Price Range" for="brand">
-            <div class="grid grid-cols-2 gap-2">
-              <x-hub::input.text placeholder="From" />
-              <x-hub::input.text placeholder="To" />
-            </div>
+          <x-hub::input.group :label="__('adminhub::global.show_deleted')" for="shoDeleted">
+            <x-hub::input.toggle wire:model="filters.soft_deleted" />
           </x-hub::input.group>
 
         </div>
@@ -106,21 +54,24 @@
         <x-hub::input.checkbox wire:model="selectPage" />
       </x-hub::table.heading>
       <x-hub::table.heading>
+        {{ __('adminhub::global.status') }}
+      </x-hub::table.heading>
+      <x-hub::table.heading>
       </x-hub::table.heading>
       <x-hub::table.heading>
         {{ __('adminhub::global.name') }}
       </x-hub::table.heading>
       <x-hub::table.heading>
-        Brand
+        {{ __('adminhub::global.brand') }}
       </x-hub::table.heading>
       <x-hub::table.heading>
-        SKU
+        {{ __('adminhub::global.sku') }}
       </x-hub::table.heading>
       <x-hub::table.heading>
-        Type
+        {{ __('adminhub::global.type') }}
       </x-hub::table.heading>
       <x-hub::table.heading>
-        Stock
+        {{ __('adminhub::global.stock') }}
       </x-hub::table.heading>
       <x-hub::table.heading></x-hub::table.heading>
     </x-slot>
@@ -143,10 +94,32 @@
           </x-hub::table.cell>
         </x-hub::table.row>
       @endif
+      @if($this->filters['soft_deleted'] ?? false)
+        <x-hub::table.row class="border-b !bg-red-50">
+          <x-hub::table.cell colspan="24">
+              <span class="text-sm text-red-800">
+                {{ __('adminhub::components.products.index.only_deleted_visible') }}
+              </span>
+          </x-hub::table.cell>
+        </x-hub::table.row>
+      @endif
       @forelse($products as $product)
       <x-hub::table.row wire:key="row-{{ $product->id }}">
         <x-hub::table.cell>
           <x-hub::input.checkbox wire:model="selected" :value="$product->id" />
+        </x-hub::table.cell>
+
+        <x-hub::table.cell>
+          <span
+            @class([
+              'text-xs inline-block py-1 px-2 rounded',
+              'text-green-600 bg-green-50' => $product->status == 'published' && !$product->deleted_at,
+              'text-yellow-600 bg-yellow-50' => $product->status == 'draft' && !$product->deleted_at,
+              'text-red-600 bg-red-50' => $product->deleted_at,
+            ])
+          >
+            {{ __('adminhub::components.products.index.' .  ($product->deleted_at ? 'deleted' : $product->status)) }}
+          </span>
         </x-hub::table.cell>
 
         <x-hub::table.cell class="w-24">
@@ -183,14 +156,25 @@
         </x-hub::table.cell>
 
         <x-hub::table.cell>
+          @if($product->deleted_at)
+            <x-hub::button
+              size="xs"
+              theme="gray"
+              type="button"
+              wire:click="restoreProduct({{ $product->id }})"
+            >
+              {{ __('adminhub::global.restore') }}
+            </x-hub::button>
+          @else
             <a href="{{ route('hub.products.show', $product->id) }}" class="text-indigo-500 hover:underline">
                 {{ __('adminhub::global.edit') }}
             </a>
+          @endif
         </x-hub::table.cell>
       </x-hub::table.row>
       @empty
         <x-hub::table.no-results>
-          Unable to find products matching search/filters.
+          {{ __('adminhub::components.products.index.products_empty') }}
         </x-hub::table.no-results>
       @endforelse
     </x-slot>
