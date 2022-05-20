e6c41d3484265804c0a2a595ddda82fa769c513f
Ryan Mitchell <ryan@thoughtcollective.com>
2022-05-20 10:30:05 +0100

    Use the right driver for orders in the admin hub

--- packages/admin/src/Search/AbstractSearch.php
+++ packages/admin/src/Search/AbstractSearch.php
@@ -4,6 +4,7 @@ namespace GetCandy\Hub\Search;
 
 use GetCandy\Hub\Base\SearchInterface;
 use GetCandy\Hub\DataTransferObjects\Search\SearchResults;
+use Illuminate\Database\Eloquent\Model;
 
 abstract class AbstractSearch implements SearchInterface
 {
@@ -13,6 +14,22 @@ abstract class AbstractSearch implements SearchInterface
     public function __construct()
     {
         $this->driver = config('scout.driver');
+    }
+    
+    /**
+     * Return the right driver for the model
+     *
+     * @param  string  $model
+     * @return string
+     */    
+    public function getDriverForModel(string $model): string
+    {
+        $engines = config('getcandy.search.engine_map', []);
+        if (isset($engines[$model])) {
+            return $engines[$model];    
+        }
+
+        return $this->driver;
     }
 
     /**
