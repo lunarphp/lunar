36ed3503193db7443b7988d6f7546b5615abaaa2
markmead <itsmarkmead@gmail.com>
2022-06-27 17:36:20 +0100

    Updated - Tailwind CSS config

--- packages/admin/tailwind.config.js
+++ packages/admin/tailwind.config.js
@@ -1,36 +1,26 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-    content: [
-        './resources/assets/**/*.js',
-        './resources/views/**/*.blade.php',
-        './resources/views/**/**/*.blade.php',
-        './vendor/livewire-ui/modal/resources/views/**/*.blade.php',
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/**/*.blade.php',
-    ],
-    theme: {
-        fontFamily: {
-            sans: ['Nunito', 'ui-sans-serif', 'system-ui'],
-            serif: ['ui-serif', 'Georgia'],
-            mono: ['ui-monospace', 'SFMono-Regular'],
-        },
-        extend: {
-            zIndex: {
-                75: 75,
-            },
-            cursor: {
-                grab: 'grab',
-            },
-        },
+  content: [
+    "./resources/assets/**/*.js",
+    "./resources/views/**/*.blade.php",
+    "./resources/views/**/**/*.blade.php",
+    "./vendor/livewire-ui/modal/resources/views/**/*.blade.php",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/**/*.blade.php",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+      },
+      zIndex: {
+        75: 75,
+      },
     },
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-            cursor: ['disabled'],
-            backgroundColor: ['even'],
-        },
-    },
-    plugins: [
-        require('@tailwindcss/forms')({
-            strategy: 'class',
-        }),
-    ],
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+  ],
 };
