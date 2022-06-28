78325082725a1bf0e8c3079459bf1b16884dc620
markmead <itsmarkmead@gmail.com>
2022-06-28 15:02:28 +0100

    Updated - Remove unused Alpine JS data

--- packages/admin/resources/views/partials/navigation/side-menu-nested.blade.php
+++ packages/admin/resources/views/partials/navigation/side-menu-nested.blade.php
@@ -1,5 +1,4 @@
-<div x-show="showInnerMenu"
-     x-cloak>
+<div>
     <aside class="hidden h-full lg:block lg:flex-shrink-0 lg:order-first">
         <div class="relative flex flex-col h-full overflow-y-auto bg-white border-r border-gray-100 w-72">
             <div class="px-4 py-16">
