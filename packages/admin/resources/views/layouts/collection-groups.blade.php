c561251ebf370c81e3b40ca60050f7898b8c5d51
markmead <itsmarkmead@gmail.com>
2022-06-28 15:02:40 +0100

    Added - Collection groups sidemenu

--- packages/admin/resources/views/layouts/collection-groups.blade.php
+++ packages/admin/resources/views/layouts/collection-groups.blade.php
@@ -1,25 +1,62 @@
 @extends('adminhub::layouts.app')
 
-@section('main')
+@section('menu')
     <div>
-        <div class="grid items-center grid-cols-2 mb-6 md:mb-8">
-            <div>
+        <aside class="hidden h-full lg:block lg:flex-shrink-0 lg:order-first">
+            <div class="relative flex flex-col h-full overflow-y-auto bg-white border-r border-gray-100 w-72">
+                <div class="px-4 py-16">
+                    @livewire('hub.components.collections.sidemenu', [
+                        'currentGroup' => $group ?? null,
+                    ])
+                </div>
+            </div>
+        </aside>
+    </div>
+@stop
+
+@section('main')
+    <div class="space-y-6">
+        <div x-data="{ showCollectionGroups: false }">
+            <div class="flex items-center justify-between">
                 <strong class="text-xl font-bold md:text-2xl">
                     {{ __('adminhub::catalogue.collections.index.title') }}
                 </strong>
+
+                <div class="block lg:hidden">
+                    <x-hub::button theme="gray"
+                                   x-on:click="showCollectionGroups = !showCollectionGroups">
+                        {{ __('View Collection Groups') }}
+                    </x-hub::button>
+                </div>
+            </div>
+
+            <div class="relative z-40 lg:hidden"
+                 role="dialog"
+                 aria-modal="true">
+                <div class="fixed inset-0 bg-gray-600/75"
+                     x-show="showCollectionGroups"
+                     x-cloak
+                     aria-hidden="true"></div>
+
+                <div class="fixed inset-0 z-40 flex"
+                     x-show="showCollectionGroups"
+                     x-cloak
+                     x-transition:enter="transition ease-out duration-300"
+                     x-transition:enter-start="-translate-x-full"
+                     x-transition:enter-end="translate-x-0"
+                     x-transition:leave="transition ease-in duration-300"
+                     x-transition:leave-start="translate-x-0"
+                     x-transition:leave-end="-translate-x-full">
+                    <div class="w-full max-w-xs p-4 overflow-y-auto bg-white focus:outline-none"
+                         x-on:click.away="showCollectionGroups = false">
+                        @livewire('hub.components.collections.sidemenu', [
+                            'currentGroup' => $group ?? null,
+                        ])
+                    </div>
+                </div>
             </div>
         </div>
 
-        <div class="lg:grid lg:grid-cols-12 lg:gap-x-5">
-            <aside class="hidden px-2 py-6 sm:px-6 lg:py-0 lg:px-0 lg:col-span-3 md:block">
-                @livewire('hub.components.collections.sidemenu', [
-                    'currentGroup' => $group ?? null,
-                ])
-            </aside>
-
-            <div class="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
-                {{ $slot }}
-            </div>
-        </div>
+        {{ $slot }}
     </div>
 @stop
--- packages/admin/resources/views/livewire/components/collections/sidemenu.blade.php
+++ packages/admin/resources/views/livewire/components/collections/sidemenu.blade.php
@@ -1,32 +1,78 @@
 <div>
-  <x-hub::button theme="gray" wire:click.prevent="$set('showCreateModal', true)">
-    {{ __('adminhub::catalogue.collections.sidemenu.create_btn') }}
-  </x-hub::button>
-  <x-hub::modal.dialog form="createCollectionGroup" wire:model="showCreateModal">
-    <x-slot name="title">{{ __('adminhub::catalogue.collections.sidemenu.modal.title') }}</x-slot>
-    <x-slot name="content">
-      <x-hub::input.group :label="__('adminhub::inputs.name')" for="name" required :error="$errors->first('name')">
-        <x-hub::input.text wire:model="name" :error="$errors->first('name')" />
-      </x-hub::input.group>
-    </x-slot>
-    <x-slot name="footer">
-      <x-hub::button type="button" wire:click.prevent="$set('showCreateModal', false)" theme="gray">{{ __('adminhub::global.cancel') }}</x-hub::button>
-      <x-hub::button type="submit">{{ __('adminhub::catalogue.collections.sidemenu.modal.btn') }}</x-hub::button>
-    </x-slot>
-  </x-hub::modal.dialog>
-  <header class="my-4">
-    <h3 class="font-medium">{{ __('adminhub::catalogue.collections.sidemenu.menu_title') }}</h3>
-  </header>
-  <nav class="mt-4 space-y-1">
-    @forelse($this->collectionGroups as $group)
-      <a
-        href="{{ route('hub.collection-groups.show', $group->id) }}"
-        class="flex items-center px-3 py-2 text-sm font-medium @if($currentGroup && $currentGroup->id == $group->id) bg-gray-50 text-indigo-700 hover:text-indigo-700 hover:bg-white @else text-gray-900 hover:text-gray-900 hover:bg-gray-50 @endif rounded-md  group"
-        aria-current="page"
-      >
-        {{ $group->name }}
-      </a>
-    @empty
-    @endforelse
-  </nav>
+    <x-hub::modal.dialog form="createCollectionGroup"
+                         wire:model="showCreateModal">
+        <x-slot name="title">
+            {{ __('adminhub::catalogue.collections.sidemenu.modal.title') }}
+        </x-slot>
+
+        <x-slot name="content">
+            <x-hub::input.group :label="__('adminhub::inputs.name')"
+                                for="name"
+                                required
+                                :error="$errors->first('name')">
+
+                <x-hub::input.text wire:model="name"
+                                   :error="$errors->first('name')" />
+            </x-hub::input.group>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-hub::button type="button"
+                           wire:click.prevent="$set('showCreateModal', false)"
+                           theme="gray">
+                {{ __('adminhub::global.cancel') }}
+            </x-hub::button>
+
+            <x-hub::button type="submit">
+                {{ __('adminhub::catalogue.collections.sidemenu.modal.btn') }}
+            </x-hub::button>
+        </x-slot>
+    </x-hub::modal.dialog>
+
+    <div>
+        <header class="flex items-center justify-between">
+            <h3 class="font-medium">
+                {{ __('adminhub::catalogue.collections.sidemenu.menu_title') }}
+            </h3>
+
+            <button type="button"
+                    x-on:click="showCollectionGroups = false"
+                    class="block lg:hidden">
+                <svg class="w-5 h-5"
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill="none"
+                     viewBox="0 0 24 24"
+                     stroke-width="2"
+                     stroke="currentColor"
+                     aria-hidden="true">
+                    <path stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </header>
+
+        <nav class="mt-4 space-y-2">
+            @forelse($this->collectionGroups as $group)
+                <a href="{{ route('hub.collection-groups.show', $group->id) }}"
+                   @class([
+                       'text-sm font-medium p-2 rounded text-gray-500 block',
+                       'bg-blue-50 text-blue-700 hover:text-blue-600' =>
+                           $currentGroup->id == $group->id,
+                       'hover:bg-blue-50 hover:text-blue-700' => $currentGroup->id == $group->id,
+                   ])
+                   aria-current="page">
+                    {{ $group->name }}
+                </a>
+            @empty
+            @endforelse
+        </nav>
+
+        <div class="mt-8">
+            <x-hub::button theme="gray"
+                           wire:click.prevent="$set('showCreateModal', true)">
+                {{ __('adminhub::catalogue.collections.sidemenu.create_btn') }}
+            </x-hub::button>
+        </div>
+    </div>
 </div>
