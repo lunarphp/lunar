8da92e1fcad095ede6ba7484f4e546d60e6bc1e7
markmead <itsmarkmead@gmail.com>
2022-07-08 14:15:17 +0100

    Update - Settings menu sections

--- packages/admin/src/Menu/SettingsMenu.php
+++ packages/admin/src/Menu/SettingsMenu.php
@@ -26,15 +26,15 @@ class SettingsMenu
     {
         $slot = Menu::slot('settings');
 
-        $storefrontSection = $slot->section('storefront')->name(
-            'Storefront'
+        $storeSection = $slot->section('store')->name(
+            'Store'
         );
 
-        // $customerSection = $slot->section('customers')->name(
-        //     'Customer Reports'
-        // );
+        $adminSection = $slot->section('admin')->name(
+            'Admin'
+        );
 
-        $storefrontSection->addItem(function ($item) {
+        $storeSection->addItem(function ($item) {
             $item->name('Addons')
                 ->handle('hub.addons')
                 ->route('hub.addons.index')
@@ -42,39 +42,7 @@ class SettingsMenu
                 ->icon('puzzle');
         });
 
-        $slot->addItem(function ($item) {
-            $item->name('Staff')
-                ->handle('hub.staff')
-                ->route('hub.staff.index')
-                ->gate('settings:manage-staff')
-                ->icon('identification');
-        });
-
-        $slot->addItem(function ($item) {
-            $item->name('Channels')
-                ->handle('hub.channels')
-                ->route('hub.channels.index')
-                ->gate('settings:core')
-                ->icon('server');
-        });
-
-        $slot->addItem(function ($item) {
-            $item->name('Languages')
-                ->handle('hub.languages')
-                ->route('hub.languages.index')
-                ->gate('settings:core')
-                ->icon('translate');
-        });
-
-        $slot->addItem(function ($item) {
-            $item->name('Currencies')
-                ->handle('hub.currencies')
-                ->route('hub.currencies.index')
-                ->gate('settings:core')
-                ->icon('currency-pound');
-        });
-
-        $slot->addItem(function ($item) {
+        $storeSection->addItem(function ($item) {
             $item->name('Attributes')
                 ->handle('hub.attributes')
                 ->route('hub.attributes.index')
@@ -82,7 +50,31 @@ class SettingsMenu
                 ->icon('beaker');
         });
 
-        $slot->addItem(function ($item) {
+        $storeSection->addItem(function ($item) {
+            $item->name('Channels')
+                ->handle('hub.channels')
+                ->route('hub.channels.index')
+                ->gate('settings:core')
+                ->icon('server');
+        });
+
+        $storeSection->addItem(function ($item) {
+            $item->name('Currencies')
+                ->handle('hub.currencies')
+                ->route('hub.currencies.index')
+                ->gate('settings:core')
+                ->icon('currency-pound');
+        });
+
+        $storeSection->addItem(function ($item) {
+            $item->name('Languages')
+                ->handle('hub.languages')
+                ->route('hub.languages.index')
+                ->gate('settings:core')
+                ->icon('translate');
+        });
+
+        $storeSection->addItem(function ($item) {
             $item->name('Tags')
                 ->handle('hub.tags')
                 ->route('hub.tags.index')
@@ -90,7 +82,15 @@ class SettingsMenu
                 ->icon('tag');
         });
 
-        $slot->addItem(function ($item) {
+        $adminSection->addItem(function ($item) {
+            $item->name('Staff')
+                ->handle('hub.staff')
+                ->route('hub.staff.index')
+                ->gate('settings:manage-staff')
+                ->icon('identification');
+        });
+
+        $adminSection->addItem(function ($item) {
             $item->name('Activity Log')
                 ->handle('hub.activity-log')
                 ->route('hub.activity-log.index')
