9df05f17614649e07e6e3f336aeecda201d5710f
markmead <itsmarkmead@gmail.com>
2022-07-12 09:09:18 +0100

    Update - Fix Livewire root element issue

--- packages/admin/resources/views/livewire/pages/authentication/login.blade.php
+++ packages/admin/resources/views/livewire/pages/authentication/login.blade.php
@@ -1,33 +1,35 @@
-<section class="relative overflow-hidden bg-white dark:bg-gray-900">
-    <x-hub::getcandy.stamp class="absolute inset-0 w-full h-full scale-150 opacity-10 blur-3xl" />
+<div>
+    <section class="relative overflow-hidden bg-white dark:bg-gray-900">
+        <x-hub::getcandy.stamp class="absolute inset-0 w-full h-full scale-150 opacity-10 blur-3xl" />
 
-    <main class="grid min-h-screen place-content-center">
-        <div class="relative w-screen max-w-lg px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
-            <div
-                 class="px-6 py-10 bg-white rounded-lg shadow-2xl dark:bg-gray-800 shadow-blue-500/25 dark:shadow-blue-500/10 sm:px-8 sm:py-12">
-                <x-hub::getcandy.stamp />
+        <main class="grid min-h-screen place-content-center">
+            <div class="relative w-screen max-w-lg px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
+                <div
+                     class="px-6 py-10 bg-white rounded-lg shadow-2xl dark:bg-gray-800 shadow-blue-500/25 dark:shadow-blue-500/10 sm:px-8 sm:py-12">
+                    <x-hub::getcandy.stamp />
 
-                <div class="mt-6 text-center">
-                    <h1 class="text-2xl font-bold text-gray-900 dark:text-white">
-                        {{ __('adminhub::auth.welcome') }}
-                    </h1>
+                    <div class="mt-6 text-center">
+                        <h1 class="text-2xl font-bold text-gray-900 dark:text-white">
+                            {{ __('adminhub::auth.welcome') }}
+                        </h1>
 
-                    <p class="mt-4 text-sm leading-relaxed text-gray-500 dark:text-gray-400">
-                        {{ __('adminhub::auth.prompt') }}
-                    </p>
+                        <p class="mt-4 text-sm leading-relaxed text-gray-500 dark:text-gray-400">
+                            {{ __('adminhub::auth.prompt') }}
+                        </p>
+                    </div>
+
+                    @livewire('hub.components.login-form')
                 </div>
 
-                @livewire('hub.components.login-form')
+                <p class="mt-8 text-sm text-center text-gray-500 dark:text-gray-400">
+                    {{ __('adminhub::auth.forgot-password.message') }}
+
+                    <a href="{{ route('hub.password-reset') }}"
+                       class="text-blue-600 transition hover:text-blue-500">
+                        {{ __('adminhub::auth.forgot-password.link') }}
+                    </a>
+                </p>
             </div>
-
-            <p class="mt-8 text-sm text-center text-gray-500 dark:text-gray-400">
-                {{ __('adminhub::auth.forgot-password.message') }}
-
-                <a href="{{ route('hub.password-reset') }}"
-                   class="text-blue-600 transition hover:text-blue-500">
-                    {{ __('adminhub::auth.forgot-password.link') }}
-                </a>
-            </p>
-        </div>
-    </main>
-</section>
+        </main>
+    </section>
+</div>
--- packages/admin/resources/views/livewire/pages/authentication/password-reset.blade.php
+++ packages/admin/resources/views/livewire/pages/authentication/password-reset.blade.php
@@ -1,29 +1,33 @@
-<section class="relative overflow-hidden bg-white dark:bg-gray-900">
-    <x-hub::getcandy.stamp class="absolute inset-0 w-full h-full scale-150 opacity-10 blur-3xl" />
+<div>
+    <section class="relative overflow-hidden bg-white dark:bg-gray-900">
+        <x-hub::getcandy.stamp class="absolute inset-0 w-full h-full scale-150 opacity-10 blur-3xl" />
 
-    <main class="grid min-h-screen place-content-center">
-        <div class="relative w-screen max-w-lg px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
-            <div
-                 class="px-6 py-10 bg-white rounded-lg shadow-2xl dark:bg-gray-800 shadow-blue-500/25 dark:shadow-blue-500/10 sm:px-8 sm:py-12">
-                <x-hub::getcandy.stamp />
+        <main class="grid min-h-screen place-content-center">
+            <div class="relative w-screen max-w-lg px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
+                <div
+                     class="px-6 py-10 bg-white rounded-lg shadow-2xl dark:bg-gray-800 shadow-blue-500/25 dark:shadow-blue-500/10 sm:px-8 sm:py-12">
+                    <x-hub::getcandy.stamp />
 
-                <div class="mt-6 text-center">
-                    <h1 class="text-2xl font-bold text-gray-900 dark:text-white">
-                        {{ __('adminhub::auth.reset-your-password') }}
-                    </h1>
+                    <div class="mt-6 text-center">
+                        <h1 class="text-2xl font-bold text-gray-900 dark:text-white">
+                            {{ __('adminhub::auth.reset-your-password') }}
+                        </h1>
+                    </div>
+
+                    @livewire('hub.components.password-reset')
                 </div>
 
-                @livewire('hub.components.password-reset')
+                <p class="mt-8 text-sm text-center text-gray-500 dark:text-gray-400">
+                    {{ __('adminhub::auth.forgot-password.remember') }}
+
+                    <a href="{{ route('hub.login') }}"
+                       class="text-blue-600 transition hover:text-blue-500">
+                        {{ __('adminhub::auth.sign-in.btn') }}
+                    </a>
+                </p>
             </div>
+        </main>
+    </section>
 
-            <p class="mt-8 text-sm text-center text-gray-500 dark:text-gray-400">
-                {{ __('adminhub::auth.forgot-password.remember') }}
-
-                <a href="{{ route('hub.login') }}"
-                   class="text-blue-600 transition hover:text-blue-500">
-                    {{ __('adminhub::auth.sign-in.btn') }}
-                </a>
-            </p>
-        </div>
-    </main>
-</section>
+    <x-hub::notification />
+</div>
