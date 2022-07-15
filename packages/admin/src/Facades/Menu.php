4e69e8c471386086e40d44a38af044a86dac699b
Adam Lee <adam@codelabx.ltd>
2022-07-15 01:02:46 +0100

    Add to settings menu + refactor

--- packages/admin/src/Facades/Menu.php
+++ packages/admin/src/Facades/Menu.php
@@ -3,8 +3,14 @@
 namespace GetCandy\Hub\Facades;
 
 use GetCandy\Hub\Menu\MenuRegistry;
+use GetCandy\Hub\Menu\MenuSlot;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static MenuSlot slot(string $handle)
+ *
+ * @see \GetCandy\Hub\Menu\MenuRegistry
+ */
 class Menu extends Facade
 {
     /**
