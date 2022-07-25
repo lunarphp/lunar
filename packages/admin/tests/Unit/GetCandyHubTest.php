959780559489d68c857d2b3ac17544d4aaea9dc2
Jakub Theimer <jakub@dy.st>
2022-07-25 22:42:42 +0200

    add test boilerplate

---
+++ packages/admin/tests/Unit/GetCandyHubTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace GetCandy\Hub\Tests\Unit;
+
+use GetCandy\Hub\Tests\TestCase;
+
+/**
+ * @group hub
+ */
+class GetCandyHubTest extends TestCase
+{
+    /** @test */
+    public function can_register_scripts()
+    {
+    }
+
+    /** @test */
+    public function can_register_styles()
+    {
+    }
+
+    /** @test */
+    public function can_register_remote_scripts()
+    {
+    }
+
+    /** @test */
+    public function can_register_remote_styles()
+    {
+    }
+}
