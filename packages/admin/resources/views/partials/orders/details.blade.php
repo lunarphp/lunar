479695ac2380bd78911257c304b1a0d947441290
Alec Ritson <hello@itsalec.co.uk>
2022-07-27 09:02:38 +0100

    Add customer reference copy

--- packages/admin/resources/views/partials/orders/details.blade.php
+++ packages/admin/resources/views/partials/orders/details.blade.php
@@ -4,52 +4,63 @@
     <dd class="text-right"><x-hub::orders.status :status="$order->status" /></dd>
   </div>
 
+  <div
+    x-data="{
+      copy(text) {
+        if (window.clipboardData && window.clipboardData.setData) {
+            $wire.call('notify', '{{ __('adminhub::notifications.clipboard.copied') }}')
+            // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+            return window.clipboardData.setData('Text', text);
+        } else if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
+            var textarea = document.createElement('textarea');
+            textarea.textContent = text;
+            textarea.style.position = 'fixed';  // Prevent scrolling to bottom of page in Microsoft Edge.
+            document.body.appendChild(textarea);
+            textarea.select();
+            try {
+              $wire.call('notify', '{{ __('adminhub::notifications.clipboard.copied') }}')
+              return document.execCommand('copy');  // Security exception may be thrown by some browsers.
+            }
+            catch (ex) {
+              $wire.call('notify', '{{ __('adminhub::notifications.clipboard.failed_copy') }}')
+            }
+            finally {
+                document.body.removeChild(textarea);
+            }
+        }
+      }
+    }">
+
   <div class="grid grid-cols-2 gap-2 px-4 py-3 border-b">
     <dt class="font-medium text-gray-500">{{ __('adminhub::partials.orders.details.reference') }}</dt>
     <dd class="text-right">
-      <div
-        x-data="{
-          reference: '{{ $order->reference }}',
-          copy() {
-            if (window.clipboardData && window.clipboardData.setData) {
-                $wire.call('notify', '{{ __('adminhub::notifications.clipboard.copied') }}')
-                // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
-                return window.clipboardData.setData('Text', this.reference);
-            } else if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
-                var textarea = document.createElement('textarea');
-                textarea.textContent = this.reference;
-                textarea.style.position = 'fixed';  // Prevent scrolling to bottom of page in Microsoft Edge.
-                document.body.appendChild(textarea);
-                textarea.select();
-                try {
-                  $wire.call('notify', '{{ __('adminhub::notifications.clipboard.copied') }}')
-                  return document.execCommand('copy');  // Security exception may be thrown by some browsers.
-                }
-                catch (ex) {
-                  $wire.call('notify', '{{ __('adminhub::notifications.clipboard.failed_copy') }}')
-                }
-                finally {
-                    document.body.removeChild(textarea);
-                }
-            }
-          }
-        }"
-        class="flex items-center justify-end space-x-4"
-      >
+      <div class="flex items-center justify-end space-x-4">
         <div>
           {{ $order->reference }}
         </div>
-        <button type="button" x-on:click="copy">
+        <button type="button" x-on:click="copy('{{ $order->reference }}')">
           <x-hub::icon ref="clipboard" class="w-4" />
         </button>
       </div>
-
     </dd>
   </div>
 
   <div class="grid grid-cols-2 gap-2 px-4 py-3 border-b">
     <dt class="font-medium text-gray-500">{{ __('adminhub::partials.orders.details.customer_reference') }}</dt>
-    <dd class="text-right">{{ $order->customer_reference }}</dd>
+    <dd class="text-right">
+      <div class="flex items-center justify-end space-x-4">
+        <div>
+          {{ $order->customer_reference }}
+        </div>
+        @if($order->customer_reference)
+        <button type="button" x-on:click="copy('{{ $order->customer_reference }}')">
+          <x-hub::icon ref="clipboard" class="w-4" />
+        </button>
+        @endif
+      </div>
+    </dd>
+  </div>
+
   </div>
 
   <div class="grid grid-cols-2 gap-2 px-4 py-3 border-b">
