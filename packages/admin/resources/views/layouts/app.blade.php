8e3a05d48b0bf42f502804c8e8be118c49712917
Alec Ritson <hello@itsalec.co.uk>
2022-07-29 12:05:11 +0100

    Feat - Move away from CDN (#440)
    * remove trix
    
    * Add flatpickr
    
    * Apply fixes from StyleCI
    
    * Move filepond off CDN
    
    * Move Quill off CDN
    
    * Move Apex charts off CDN
    
    * Remove unused cdn
    
    * Add apexcharts
    
    * Compile
    
    * Apply fixes from StyleCI
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- packages/admin/resources/views/layouts/app.blade.php
+++ packages/admin/resources/views/layouts/app.blade.php
@@ -12,16 +12,7 @@
     <link rel="icon"
           type="image/png"
           href="https://cdn.getcandy.io/hub/favicon.svg">
-    <link rel="stylesheet"
-          href="https://unpkg.com/trix@1.2.3/dist/trix.css" />
-    <link rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-    <link href="https://unpkg.com/filepond@^4/dist/filepond.css"
-          rel="stylesheet" />
-    <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css"
-          rel="stylesheet">
-    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css"
-          rel="stylesheet">
+
     <link rel="preconnect"
           href="https://fonts.googleapis.com">
     <link rel="preconnect"
@@ -38,22 +29,10 @@
         }
     </style>
 
-    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
     <script defer
             src="https://unpkg.com/@alpinejs/persist@3.x.x/dist/cdn.min.js"></script>
     <script defer
             src="https://unpkg.com/alpinejs@3.8.1/dist/cdn.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-    <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
-    <script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js"></script>
-    <script src="https://unpkg.com/filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.js"></script>
-    <script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.14.0/Sortable.js"
-            integrity="sha512-Fc8SDJVBwajCGX0A9z8lBeRPaCjR25Ek577z9PtQLB7CLBz7Mw1XhjbcD2yDWrGszL/uezKGidtGCng6Fhz3+A=="
-            crossorigin="anonymous"
-            referrerpolicy="no-referrer"></script>
-
     @livewireStyles
 </head>
 
