a691dedf1a50e0b3a97da8244bad2ac61a9a79b0
Alec Ritson <hello@itsalec.co.uk>
2022-07-29 13:51:41 +0100

    Open PDF in new tab

--- packages/admin/resources/views/partials/orders/actions.blade.php
+++ packages/admin/resources/views/partials/orders/actions.blade.php
@@ -52,6 +52,7 @@
 <a
   class="inline-flex items-center px-4 py-2 font-bold transition border border-transparent border-gray-200 rounded hover:bg-white bg-gray-50 hover:border-gray-200"
   href="{{ route('hub.orders.pdf', $order->id) }}"
+  target="_blank"
 >
   <x-hub::icon
     ref="download"
