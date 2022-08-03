b593a8c852a637fa120eacfe683c5b4a6ee11648
Alec Ritson <hello@itsalec.co.uk>
2022-08-03 13:50:59 +0100

    UI Tweaks

--- packages/admin/resources/views/livewire/components/field-types/file.blade.php
+++ packages/admin/resources/views/livewire/components/field-types/file.blade.php
@@ -1,90 +1,114 @@
-<div>
-  <div class="flex justify-end w-full mb-4">
-    <x-hub::button wire:click="$set('showUploader', true)">
-      Choose assets
-    </x-hub::button>
-  </div>
-
-  <div class="grid grid-cols-4 gap-4">
-    @foreach ($this->selectedModels as $index => $assetModel)
-      <div class="flex items-center border rounded" wire:key="initial_asset_{{ $assetModel->id }}">
-        <div>
-          @if($assetModel->file->hasGeneratedConversion('small'))
-            <img src="{{ $assetModel->file->getUrl('small') }}" class="w-12  pl-3 rounded block">
-          @else
-            <x-hub::icon ref="document" class="w-10 h-10 p-1 text-gray-300" />
-          @endif
-        </div>
-
-        <div class="px-3 grow">
-          <span class="block truncate text-xs">{{ $assetModel->file->file_name }}</span>
-        </div>
-
-        <div>
-          <button
-            type="button"
-            wire:click="removeSelected({{ $assetModel->id }})"
-            class="text-xs px-2 mt-2">
-            <x-hub::icon ref="x" class="w-4" />
-          </button>
-        </div>
-      </div>
-    @endforeach
-  </div>
-
-  <x-hub::modal.dialog wire:model="showUploader">
-    <x-slot name="title">
-      Select assets
-    </x-slot>
-    <x-slot name="content">
-      <div class="space-y-4">
-        <input type="file" wire:model="file" />
-
-        <div class="border-t pt-4">
-          <div class="grid grid-cols-4 items-center gap-4">
-            @forelse($this->assets as $asset)
-              {{-- @if(!str_starts_with('image', $media->mime_type)) --}}
-                <label
-                  @class([
-                    'border rounded text-center cursor-pointer  p-4',
-                    'border-blue-500' => in_array($asset->id, $selected)
-                  ])>
-                  @if($asset->file->hasGeneratedConversion('small'))
-                    <img src="{{ $asset->file->getUrl('small') }}" class="w-24 mx-auto rounded">
-                  @else
-                    <div>
-                      <x-hub::icon ref="document" class="mx-auto" />
-                    </div>
-                  @endif
-
-                  <span class="block truncate text-xs">{{ $asset->file->file_name }}</span>
-                  <input
-                    wire:model="selected"
-                    type="checkbox"
-                    class="hidden"
-                    value="{{ $asset->id }}"
-                  />
-                </label>
-              {{-- @endif --}}
-            @empty
-              Files you upload will appear here.
-            @endforelse
-          </div>
-        </div>
-      </div>
-    </x-slot>
-    <x-slot name="footer">
-      <div class="flex justify-end space-x-4">
-        <x-hub::button type="button" theme="gray" wire:click="$set('showUploader', false)">{{ __('adminhub::global.cancel') }}</x-hub::button>
-        <x-hub::button
-          type="button"
-          :disabled="!count($selected)"
-          wire:click.prevent="process"
-        >
-        Select files
+<div class="space-y-4">
+    <div class="flex justify-end">
+        <x-hub::button wire:click="$set('showUploader', true)">
+            Choose assets
         </x-hub::button>
-      </div>
-    </x-slot>
+    </div>
 
-  </x-hub::modal.dialog>
+    <div class="grid grid-cols-2 gap-4 lg:grid-cols-3">
+        @foreach ($this->selectedModels as $index => $assetModel)
+            <div class="flex items-center justify-between gap-4 p-2 border rounded"
+                 wire:key="initial_asset_{{ $assetModel->id }}">
+                <div class="flex items-center gap-4 overflow-hidden">
+                    <div class="w-12 h-12 p-2 border rounded shrink-0">
+                        @if ($assetModel->file->hasGeneratedConversion('small'))
+                            <a href="{{ $assetModel->file->getUrl('large') }}"
+                               target="_blank"
+                               class="block">
+                                <img src="{{ $assetModel->file->getUrl('small') }}"
+                                     class="object-contain w-8 h-8 mx-auto">
+                            </a>
+                        @else
+                            <x-hub::icon ref="document"
+                                         class="w-8 h-8 mx-auto text-gray-400" />
+                        @endif
+                    </div>
+
+                    <p class="text-xs truncate">
+                      {{ $assetModel->file->file_name }}
+                    </p>
+                </div>
+
+                <button type="button"
+                        wire:click="removeSelected({{ $assetModel->id }})"
+                        class="p-0.5 rounded hover:bg-gray-100 shrink-0">
+                    <x-hub::icon ref="x"
+                                 class="w-4 h-4" />
+                </button>
+            </div>
+        @endforeach
+    </div>
+
+    <x-hub::modal.dialog wire:model="showUploader">
+        <x-slot name="title">
+            <strong>
+                Select assets
+            </strong>
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="grid grid-cols-2 gap-4 lg:grid-cols-3">
+                <label class="p-6 border border-dashed rounded-md cursor-pointer hover:border-blue-500">
+                    <div>
+                        <x-hub::icon ref="upload"
+                                     class="w-12 h-12 mx-auto text-gray-400" />
+
+                        <p class="mt-4 text-xs font-medium text-center text-gray-600">
+                            {{ __('Upload Asset') }}
+                        </p>
+                    </div>
+
+                    <input type="file"
+                           wire:model="file"
+                           class="hidden" />
+                </label>
+
+                @forelse($this->assets as $asset)
+                    {{-- @if (!str_starts_with('image', $media->mime_type)) --}}
+                    <label @class([
+                        'border rounded-md cursor-pointer p-6 hover:border-blue-500',
+                        'border-blue-500' => in_array($asset->id, $selected),
+                    ])>
+                        <div>
+                            @if ($asset->file->hasGeneratedConversion('small'))
+                                <img src="{{ $asset->file->getUrl('small') }}"
+                                     class="object-contain w-12 h-12 mx-auto">
+                            @else
+                                <x-hub::icon ref="document"
+                                             class="w-12 h-12 mx-auto text-gray-400" />
+                            @endif
+
+                            <p class="mt-4 text-xs font-medium text-center text-gray-600 truncate">
+                                {{ $asset->file->file_name }}
+                            </p>
+                        </div>
+
+                        <input wire:model="selected"
+                               type="checkbox"
+                               class="hidden"
+                               value="{{ $asset->id }}" />
+                    </label>
+                    {{-- @endif --}}
+                @empty
+                    Files you upload will appear here.
+                @endforelse
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            <div class="flex justify-end space-x-4">
+                <x-hub::button type="button"
+                               theme="gray"
+                               wire:click="$set('showUploader', false)">
+                    {{ __('adminhub::global.cancel') }}
+                </x-hub::button>
+
+                <x-hub::button type="button"
+                               :disabled="!count($selected)"
+                               wire:click.prevent="process">
+                    Select files
+                </x-hub::button>
+            </div>
+        </x-slot>
+    </x-hub::modal.dialog>
 </div>
