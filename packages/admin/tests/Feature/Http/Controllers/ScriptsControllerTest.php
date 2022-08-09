1aa45006d5c62a9d31a7fc2262d474d926966be0
Jakub Theimer <jakub@dy.st>
2022-08-10 00:59:31 +0200

    add scripts controller test

---
+++ packages/admin/tests/Feature/Http/Controllers/ScriptsControllerTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace GetCandy\Hub\Tests\Feature\Http\Controllers;
+
+use GetCandy\Hub\GetCandyHub;
+use GetCandy\Hub\Models\Staff;
+use GetCandy\Hub\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * @group hub.assets
+ */
+class ScriptsControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function fails_when_accessing_non_existing_local_script_name()
+    {
+        $staff = Staff::factory()->create([
+            'admin' => false,
+        ]);
+
+        $this->actingAs($staff, 'staff');
+
+        $this->get(route('hub.assets.scripts', ['script' => 'non-existing-script']))
+            ->assertStatus(404);
+    }
+
+    /** @test */
+    public function fails_when_accessing_non_existing_script()
+    {
+        $staff = Staff::factory()->create([
+            'admin' => false,
+        ]);
+
+        $this->actingAs($staff, 'staff');
+
+        $name = 'non-existing-local-script';
+        $path = 'js/non-existing-local-script.js';
+
+        GetCandyHub::script($name, $path);
+
+        $this->get(route('hub.assets.scripts', ['script' => $name]))
+            ->assertStatus(500);
+    }
+
+    /** @test */
+    public function can_return_existing_local_script()
+    {
+        $staff = Staff::factory()->create([
+            'admin' => false,
+        ]);
+
+        $this->actingAs($staff, 'staff');
+
+        $name = 'local-script';
+        $path = __DIR__.'/local-script.js';
+
+        GetCandyHub::script($name, $path);
+
+        // Create the file
+        file_put_contents($path, 'console.log("hello");');
+
+        $this->get(route('hub.assets.scripts', ['script' => $name]))
+            ->assertStatus(200);
+
+        // Remove the file after test
+        unlink($path);
+    }
+}
