5b1c8957009cfb4a804c19ae85cb337a285aba43
Alec Ritson <hello@itsalec.co.uk>
2022-08-11 13:07:33 +0100

    Update StaffTableTest.php

--- packages/admin/tests/Unit/Http/Livewire/Components/Tables/StaffTableTest.php
+++ packages/admin/tests/Unit/Http/Livewire/Components/Tables/StaffTableTest.php
@@ -2,11 +2,9 @@
 
 namespace GetCandy\Hub\Tests\Unit\Http\Livewire\Components;
 
-use GetCandy\Hub\Http\Livewire\Components\Tables\ProductTypesTable;
 use GetCandy\Hub\Http\Livewire\Components\Tables\StaffTable;
 use GetCandy\Hub\Models\Staff;
 use GetCandy\Hub\Tests\TestCase;
-use GetCandy\Models\ProductType;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 
