f798cb32cfa2b45e30e2c33f3f7d988c3dfa79e7
Ryan Mitchell <ryan@thoughtcollective.com>
2022-08-11 12:44:43 +0100

    Revert "Dont use blink facade"
    This reverts commit 4fff8da94870ac00281b92f9a31998b665b9b3fa.

--- packages/core/src/Base/Traits/HasDefaultRecord.php
+++ packages/core/src/Base/Traits/HasDefaultRecord.php
@@ -3,7 +3,7 @@
 namespace GetCandy\Base\Traits;
 
 use Illuminate\Support\Str;
-use Spatie\Blink\Blink;
+use Spatie\LaravelBlink\BlinkFacade as Blink;
 
 trait HasDefaultRecord
 {
@@ -27,7 +27,7 @@ trait HasDefaultRecord
     {
         $key = 'getcandy_default_'.Str::snake(self::class);
 
-        return (new Blink)->once($key, function () {
+        return Blink::once($key, function () {
             return self::query()->default(true)->first();
         });
     }
