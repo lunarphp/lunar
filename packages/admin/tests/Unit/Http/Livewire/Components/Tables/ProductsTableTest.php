953ad1c3ba450b387012b76a7561f7e917046ffc
Alec Ritson <hello@itsalec.co.uk>
2022-08-11 12:15:32 +0100

    Create ProductsTableTest.php

---
+++ packages/admin/tests/Unit/Http/Livewire/Components/Tables/ProductsTableTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace GetCandy\Hub\Tests\Unit\Http\Livewire\Components;
+
+use GetCandy\Hub\Http\Livewire\Components\Tables\ProductsTable;
+use GetCandy\Hub\Tests\TestCase;
+use GetCandy\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
+
+/**
+ * @group hub.tables
+ */
+class ProductsTableTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_mount_table()
+    {
+        Livewire::test(ProductsTable::class);
+    }
+
+    /** @test */
+    public function can_see_table_data()
+    {
+        $products = Product::factory(10)->create();
+        Livewire::test(ProductsTable::class)->assertCanSeeTableRecords($products);
+    }
+
+    /** @test */
+    public function can_see_base_columns()
+    {
+        $products = Product::factory(10)->create();
+        Livewire::test(ProductsTable::class)
+            ->assertCanRenderTableColumn('status')
+            ->assertCanRenderTableColumn('thumbnail')
+            ->assertCanRenderTableColumn('name')
+            ->assertCanRenderTableColumn('brand')
+            ->assertCanRenderTableColumn('productType.name')
+            ->assertCanRenderTableColumn('sku');
+    }
+}
