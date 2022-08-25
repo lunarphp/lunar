149fa21688fe39e850996da9cc4e0798c6212901
Adam Lee <adam@codelabx.ltd>
2022-08-25 11:43:26 +0100

    Refactored and locally tested all scenarios

--- packages/admin/src/Listeners/SetStaffAuthMiddlewareListener.php
+++ packages/admin/src/Listeners/SetStaffAuthMiddlewareListener.php
@@ -2,11 +2,17 @@
 
 namespace GetCandy\Hub\Listeners;
 
+use GetCandy\Hub\Http\Middleware\Authenticate;
 use Illuminate\Routing\Events\RouteMatched;
 use Illuminate\Support\Str;
 
 class SetStaffAuthMiddlewareListener
 {
+    /**
+     * @var \Illuminate\Routing\Events\RouteMatched
+     */
+    protected RouteMatched $event;
+
     /**
      * Handle the event.
      *
@@ -15,7 +21,9 @@ class SetStaffAuthMiddlewareListener
      */
     public function handle(RouteMatched $event)
     {
-        if ($this->isLivewireRoute($event->route->getName()) || $this->allowMiddleware($event->route->name)) {
+        $this->event = $event;
+
+        if (!$this->routeAlreadyHasStaffMiddleware() && $this->shouldApplyStaffMiddleware()) {
             $event->route->middleware(
                 array_merge($event->route->middleware(), ['auth:staff'])
             );
@@ -23,24 +31,58 @@ class SetStaffAuthMiddlewareListener
     }
 
     /**
-     * Matches livewire route which use dot separator.
+     * Check if the route already has the staff middleware.
      *
-     * @param  string|null  $routeName
      * @return bool
      */
-    protected function isLivewireRoute(?string $routeName): bool
+    protected function routeAlreadyHasStaffMiddleware(): bool
     {
-        return str_starts_with($routeName, 'livewire.');
+        return collect($this->event->route->middleware())->flip()->has(Authenticate::class);
     }
 
     /**
-     * Matches routes which are allowed to use with auth:staff middleware.
+     * Matches livewire routes which are allowed to use auth:staff middleware.
      *
-     * @param  string|null  $routeName
      * @return bool
      */
-    protected function allowMiddleware(?string $routeName): bool
+    protected function shouldApplyStaffMiddleware(): bool
     {
-        return Str::of($routeName)->contains(['hub', 'livewire-ui-modal']);
+        return $this->isLivewireRoute() && ($this->onlyHubRoutes() && !$this->disallowRoutes());
+    }
+
+    /**
+     * Matches hub routes.
+     *
+     * @return bool
+     */
+    protected function onlyHubRoutes(): bool
+    {
+        return Str::of($this->event->route->name)->contains([
+            'hub.pages',
+            'hub.components',
+        ]);
+    }
+
+    /**
+     * List of livewire routes which are not allowed to use auth:staff middleware.
+     *
+     * @return bool
+     */
+    protected function disallowRoutes(): bool
+    {
+        return Str::of($this->event->route->name)->contains([
+            'login',
+            'password-reset',
+        ]);
+    }
+
+    /**
+     * Matches livewire route which use dot separator.
+     *
+     * @return bool
+     */
+    protected function isLivewireRoute(): bool
+    {
+        return str_starts_with($this->event->route->getName(), 'livewire.');
     }
 }
