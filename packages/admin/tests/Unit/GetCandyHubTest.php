e5e53e4ac81b68879c687efec064694503726137
Jakub Theimer <jakub@dy.st>
2022-09-05 10:15:57 +0200

    Package assets (#279)
    * make it possible to register package scripts
    
    * add scripts controller, which resolves script from scripts array by its name
    
    * add scripts route
    
    * add abstract asset class
    
    * add script class which extends asset class and defines its url and response headers
    
    * loop through registered package scripts
    
    * update docblocks
    
    * style fix
    
    * style fix
    
    * add asset routes
    
    * add types
    
    * add style asset class
    
    * return url as named route
    
    * add styles controller
    
    * add the possibility to register styles
    
    * add the possibility to register remote assets
    
    * handle remote and local urls
    
    * register package styles into layout
    
    * add methods that set remote assets
    
    * assets docs wip
    
    * style fix
    
    * register assets file to docs navigation
    
    * style fix
    
    * add information about registering assets
    
    * edit copy
    
    * add missing namespace
    
    * remove return types
    
    * add test boilerplate
    
    * fix double =
    
    * prefix asset routes
    
    * fix double prefix
    
    * add tests
    
    * style fix
    
    * run each test in separate process
    
    * add scripts controller test
    
    * add styles controller test
    
    * remove unused import
    
    * add assertions
    
    * reset assets state after each test
    
    Co-authored-by: Alec Ritson <hello@itsalec.co.uk>
    Co-authored-by: Glenn Jacobs <glenn@neondigital.co.uk>

---
+++ packages/admin/tests/Unit/GetCandyHubTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace GetCandy\Hub\Tests\Unit;
+
+use GetCandy\Hub\Assets\Script;
+use GetCandy\Hub\Assets\Style;
+use GetCandy\Hub\GetCandyHub;
+use GetCandy\Hub\Tests\TestCase;
+
+/**
+ * @group hub
+ */
+class GetCandyHubTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function can_register_scripts_by_providing_name_and_path()
+    {
+        $name = 'local-script';
+        $path = 'js/local-script.js';
+
+        GetCandyHub::script($name, $path);
+
+        $this->assertContainsOnlyInstancesOf(Script::class, GetCandyHub::scripts());
+        $this->assertEquals(GetCandyHub::scripts()[0]->name(), $name);
+        $this->assertEquals(GetCandyHub::scripts()[0]->path(), $path);
+
+        GetCandyHub::$scripts = [];
+    }
+
+    /** @test */
+    public function can_register_remote_scripts()
+    {
+        $path = 'https://example.com/script.js';
+
+        GetCandyHub::remoteScript($path);
+
+        $this->assertContainsOnlyInstancesOf(Script::class, GetCandyHub::scripts());
+        $this->assertEquals(GetCandyHub::scripts()[0]->name(), md5($path));
+        $this->assertEquals(GetCandyHub::scripts()[0]->path(), $path);
+        $this->assertEquals(GetCandyHub::scripts()[0]->isRemote(), true);
+
+        $name = 'remote-script';
+        $path2 = 'https://example.com/script2.js';
+
+        GetCandyHub::script($name, $path2, true);
+
+        $this->assertEquals(GetCandyHub::scripts()[1]->name(), $name);
+        $this->assertEquals(GetCandyHub::scripts()[1]->path(), $path2);
+        $this->assertEquals(GetCandyHub::scripts()[1]->isRemote(), true);
+
+        GetCandyHub::$scripts = [];
+    }
+
+    /** @test */
+    public function can_register_styles_by_providing_name_and_path()
+    {
+        $name = 'local-style';
+        $path = 'local-style.css';
+
+        GetCandyHub::style($name, $path);
+
+        $this->assertContainsOnlyInstancesOf(Style::class, GetCandyHub::styles());
+        $this->assertEquals(GetCandyHub::styles()[0]->name(), $name);
+        $this->assertEquals(GetCandyHub::styles()[0]->path(), $path);
+
+        GetCandyHub::$styles = [];
+    }
+
+    /** @test */
+    public function can_register_remote_styles()
+    {
+        $path = 'https://example.com/style.css';
+
+        GetCandyHub::remoteStyle($path);
+
+        $this->assertContainsOnlyInstancesOf(Style::class, GetCandyHub::scripts());
+        $this->assertEquals(GetCandyHub::styles()[0]->name(), md5($path));
+        $this->assertEquals(GetCandyHub::styles()[0]->path(), $path);
+        $this->assertEquals(GetCandyHub::styles()[0]->isRemote(), true);
+
+        $name = 'remote-style';
+        $path2 = 'https://example.com/style2.css';
+
+        GetCandyHub::style($name, $path2, true);
+
+        $this->assertEquals(GetCandyHub::styles()[1]->name(), $name);
+        $this->assertEquals(GetCandyHub::styles()[1]->path(), $path2);
+        $this->assertEquals(GetCandyHub::styles()[1]->isRemote(), true);
+
+        GetCandyHub::$styles = [];
+    }
+}
