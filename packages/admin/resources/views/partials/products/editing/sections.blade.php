c176e8dfc37d0f2d7dd21df5f1da546651290f19
wychoong <67364036+wychoong@users.noreply.github.com>
2022-09-06 23:57:09 +0800

    cleanup sections empty space

--- packages/admin/resources/views/partials/products/editing/sections.blade.php
+++ packages/admin/resources/views/partials/products/editing/sections.blade.php
@@ -35,13 +35,13 @@
 <div class="pb-24 mt-8 lg:gap-8 lg:flex lg:items-start">
     <div class="space-y-6 lg:flex-1">
         <div class="space-y-6">
-            <div>
-                @if (!$this->hasChannelAvailability)
+            @if (!$this->hasChannelAvailability)
+                <div>
                     <x-hub::alert level="danger">
                         {{ __('adminhub::catalogue.products.show.no_channel_availability') }}
                     </x-hub::alert>
-                @endif
-            </div>
+                </div>
+            @endif
 
             @foreach ($this->getSlotsByPosition('top') as $slot)
                 <div id="{{ $slot->handle }}">
