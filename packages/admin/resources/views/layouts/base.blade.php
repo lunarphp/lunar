440005bb6530daeaa611ef631524e9e3b5f09ab7
Adam Lee <53559175+adam-code-labx@users.noreply.github.com>
2022-09-08 15:01:41 +0100

    Customising branding (#504)
    * Customising branding
    
    * Corrected path to logo
    
    * Apply fixes from StyleCI
    
    * Extracted fav icon to component to allow for easier customisation
    
    * Updated docs with brief info on brand customisation
    
    * Update - Feedback and tweaks
    
    * Update - Rebuild assets
    
    * Fix - Side menu mobile logo
    
    * fix renaming
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>
    Co-authored-by: Mark Mead <itsmarkmead@gmail.com>
    Co-authored-by: Mark Mead <50486078+markmead@users.noreply.github.com>
    Co-authored-by: Glenn Jacobs <glenn@neondigital.co.uk>
    Co-authored-by: Alec Ritson <hello@itsalec.co.uk>

--- packages/admin/resources/views/layouts/base.blade.php
+++ packages/admin/resources/views/layouts/base.blade.php
@@ -1,24 +1,33 @@
 <!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-50">
-  <head>
+<html lang="en"
+      class="h-full bg-gray-50">
+
+<head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0" />
     <title>{{ $title ?? 'Hub' }} | {{ config('app.name') }}</title>
-    <link data-n-head="ssr" rel="icon" type="image/png"  href="https://cdn.getcandy.io/hub/favicon.svg">
 
-    <link href="https://fonts.bunny.net/css2?family=Nunito&display=swap" rel="stylesheet">
-    <link href="{{ asset('vendor/getcandy/admin-hub/app.css') }}" rel="stylesheet">
+    <x-hub::branding.favicon />
 
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
+    <link href="https://fonts.bunny.net/css2?family=Nunito&display=swap"
+          rel="stylesheet">
+    <link href="{{ asset('vendor/getcandy/admin-hub/app.css') }}"
+          rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js"
+            defer></script>
 
     @livewireStyles
-  </head>
-  <body>
+</head>
+
+<body>
 
     {{ $slot }}
 
     <script></script>
 
     @livewireScripts
-  </body>
+</body>
+
 </html>
