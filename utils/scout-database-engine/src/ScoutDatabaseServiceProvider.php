1ce56af1fe4ba9587c96abcbd62053a934a6d483
Alec Ritson <hello@itsalec.co.uk>
2022-09-15 08:48:57 +0100

    Add scout-database-engine

---
+++ utils/scout-database-engine/src/ScoutDatabaseServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Lunar\ScoutDatabaseEngine;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Scout\EngineManager;
+
+class ScoutDatabaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        app(EngineManager::class)->extend('database_index', function () {
+            return new DatabaseEngine;
+        });
+
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations'),
+        ], 'scout-database-engine');
+    }
+}
