93da64b727e05cf644f2dfdc18c1fab2f67cf9d9
Alec Ritson <hello@itsalec.co.uk>
2022-09-15 10:16:19 +0100

    Add scout-database-engine

---
+++ utils/scout-database-engine/src/SearchIndex.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lunar\ScoutDatabaseEngine;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SearchIndex extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'search_index';
+}
