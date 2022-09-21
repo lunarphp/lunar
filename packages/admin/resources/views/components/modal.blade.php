a7ca9f8d5275a6d6ba954dade052772a9e83a949
Mark Mead <itsmarkmead@gmail.com>
2022-09-21 14:43:59 +0100

    Handle if focus fails

--- packages/admin/resources/views/components/modal.blade.php
+++ packages/admin/resources/views/components/modal.blade.php
@@ -22,35 +22,25 @@ switch ($maxWidth ?? '2xl') {
 @endphp
 
 <div x-data="{ show: @entangle($attributes->wire('model')) }"
-     x-init="$watch('show', value => value && setTimeout(autofocus, 50))"
-     x-on:keydown.escape.window="show = false"
      x-cloak
      x-show="show"
-     x-trap.inert.noscroll="show"
+     x-on:keydown.escape.window="show = false"
      id="{{ $id }}"
      class="fixed inset-x-0 top-0 px-4 pt-6 z-75 sm:px-0 sm:flex sm:items-top sm:justify-center">
     <div x-cloak
          x-show="show"
          class="fixed inset-0 transition-all"
-         x-on:click="show = false"
-         x-transition:enter="ease-out duration-300"
-         x-transition:enter-start="opacity-0"
-         x-transition:enter-end="opacity-100"
-         x-transition:leave="ease-in duration-200"
-         x-transition:leave-start="opacity-100"
-         x-transition:leave-end="opacity-0">
+         x-transition.opacity>
         <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
     </div>
 
     <div x-cloak
          x-show="show"
-         class="bg-white rounded-lg shadow-xl transition-all sm:w-full z-75 {{ $maxWidth }}"
-         x-transition:enter="ease-out duration-300"
-         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-         x-transition:leave="ease-in duration-200"
-         x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-         x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+         x-trap.inert.noscroll="show"
+         x-on:click.away="show = false"
+         x-init="$watch('show', (isShown) => isShown && $focus.first())"
+         x-transition
+         class="bg-white rounded-lg shadow-xl transition-all sm:w-full z-75 {{ $maxWidth }}">
         {{ $slot }}
     </div>
 </div>
