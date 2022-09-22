6d9db6838aabec61e31a054bb2623392e87cb4e5
Mark Mead <itsmarkmead@gmail.com>
2022-09-22 15:37:29 +0100

    Init

--- packages/admin/resources/views/components/notification.blade.php
+++ packages/admin/resources/views/components/notification.blade.php
@@ -1,5 +1,5 @@
 <div x-data="{
-    messages: {{ json_encode($messages) }},
+    messages: @json($messages),
     level: '{{ $level }}',
     timeout: null,
     remove(message) {
@@ -21,8 +21,9 @@
              x-transition:leave="transition ease-in duration-100"
              x-transition:leave-start="opacity-100"
              x-transition:leave-end="opacity-0"
-             class="w-full max-w-sm p-4 bg-white border border-gray-100 rounded-md shadow-lg pointer-events-auto dark:border-gray-700 dark:bg-gray-800">
-            <div class="flex items-center gap-2">
+             class="w-full max-w-sm p-4 bg-white border border-gray-100 rounded-md shadow-lg pointer-events-auto dark:border-gray-700 dark:bg-gray-800"
+             :class="{ 'border-red-600': level === 'error', 'border-green-600': level !== 'error' }">
+            <div class="flex gap-2">
                 <div class="shrink-0">
                     <span x-show="level === 'error'"
                           class="text-red-600">
@@ -37,14 +38,15 @@
                     </span>
                 </div>
 
-                <div class="flex-1">
+                <div class="flex-1 leading-5 -mt-[2px]">
                     <strong x-text="message"
-                            class="text-sm font-medium text-gray-900 dark:text-white"></strong>
+                            class="text-sm font-medium text-gray-900 dark:text-white">
+                    </strong>
                 </div>
 
                 <div class="shrink-0">
                     <button x-on:click="remove(message)"
-                            class="p-1 text-gray-500 rounded-md hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-gray-400">
+                            class="p-1 -m-1 text-gray-500 rounded hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-gray-400">
                         <x-hub::icon ref="x"
                                      class="w-4 h-4" />
                     </button>
