c1a5c513744d8bfc406e15494e093074b78241e2
Alec Ritson <hello@itsalec.co.uk>
2022-10-06 14:43:10 +0100

    Improve variant disabling/enabling UX

--- packages/admin/resources/views/partials/products/editing/variants.blade.php
+++ packages/admin/resources/views/partials/products/editing/variants.blade.php
@@ -20,6 +20,11 @@
           ])
         </div>
       @endif
+      @if(!$variantsEnabled && $this->getVariantsCount() > 1)
+        <x-hub::alert level="danger">
+            This will remove all variants from this product
+        </x-hub::alert>
+      @endif
       @if($variantsEnabled)
         @if($this->getVariantsCount() <= 1)
           @include('adminhub::partials.products.editing.options')
@@ -27,56 +32,6 @@
           @livewire('hub.components.products.variants.table', [
             'product' => $this->product,
           ])
-          {{-- <x-hub::table>
-            <x-slot name="head">
-              <x-hub::table.heading>{{ __('adminhub::global.options') }}</x-hub::table.heading>
-              <x-hub::table.heading>{{ __('adminhub::global.sku') }}</x-hub::table.heading>
-              <x-hub::table.heading>{{ __('adminhub::global.unit_price_tax') }}</x-hub::table.heading>
-              <x-hub::table.heading>{{ __('adminhub::global.stock_incoming') }}</x-hub::table.heading>
-              <x-hub::table.heading></x-hub::table.heading>
-              <x-hub::table.heading></x-hub::table.heading>
-            </x-slot>
-            <x-slot name="body">
-              @foreach($product->variants as $variant)
-              <x-hub::table.row>
-                <x-hub::table.cell class="w-full">
-                  @foreach($variant->values as $value)
-                    {{ $value->translate('name') }} {{ !$loop->last ? '/' : null }}
-                  @endforeach
-                </x-hub::table.cell>
-                <x-hub::table.cell>
-                  {{ $variant->sku }}
-                </x-hub::table.cell>
-                <x-hub::table.cell>
-                  @php
-                    $price = $variant->basePrices->first(fn($price) => $price->currency->default);
-                  @endphp
-                  {{ $price->price->formatted }}
-                </x-hub::table.cell>
-                <x-hub::table.cell>
-                  {{ $variant->stock }} ({{ $variant->backorder }})
-                </x-hub::table.cell>
-                <x-hub::table.cell class="w-3">
-                  <a href="{{ route('hub.products.variants.show', [
-                    'product' => $product,
-                    'variant' => $variant,
-                  ]) }}" class="text-indigo-500 hover:underline">{{ __('adminhub::partials.products.variants.table_row_action_text') }}</a>
-                </x-hub::table.cell>
-                <x-hub::table.cell>
-                  @if($variant->created_at == $variant->updated_at)
-                    <button
-                      class="text-red-600 hover:underline"
-                      type="button"
-                      wire:click.prevent="deleteVariant('{{ $variant->id }}')"
-                    >
-                      {{ __('adminhub::partials.products.variants.table_row_delete_text') }}
-                    </button>
-                  @endif
-                </x-hub::table.cell>
-              </x-hub::table.row>
-              @endforeach
-            </x-slot>
-          </x-hub::table> --}}
         @endif
       @endif
   </div>
