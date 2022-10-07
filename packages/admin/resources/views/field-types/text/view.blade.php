dc58cb4f7bd4fcfd9d811c997ce525d630ced2e1
Mark Mead <itsmarkmead@gmail.com>
2022-10-07 11:45:54 +0100

    Attribute text input

--- packages/admin/resources/views/field-types/text/view.blade.php
+++ packages/admin/resources/views/field-types/text/view.blade.php
@@ -1,12 +1,11 @@
 <div>
-  @if(($field['configuration']['richtext'] ?? false))
-    <x-hub::input.richtext
-      id="{{ $field['id'] }}"
-      wire:model.defer="{{ $field['signature'] }}{{ isset($language) ? '.' . $language : null }}"
-      :initial-value="isset($language) ? ($field['data'][$language] ?? null) : $field['data']"
-      :options="json_decode($field['configuration']['options'] ?? '[]', true)"
-    />
-  @else
-    <x-hub::input.text wire:model="{{ $field['signature'] }}{{ isset($language) ? '.' . $language : null }}" />
-  @endif
+    @if ($field['configuration']['richtext'] ?? false)
+        <x-hub::input.richtext id="{{ $field['id'] }}"
+                               wire:model.defer="{{ $field['signature'] }}{{ isset($language) ? '.' . $language : null }}"
+                               :initial-value="isset($language) ? $field['data'][$language] ?? null : $field['data']"
+                               :options="json_decode($field['configuration']['options'] ?? '[]', true)" />
+    @else
+        <x-hub::input.text
+                           wire:model.defer="{{ $field['signature'] }}{{ isset($language) ? '.' . $language : null }}" />
+    @endif
 </div>
