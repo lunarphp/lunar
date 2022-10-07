ff587957b01e2758717852635c51b971e37dacce
Mark Mead <itsmarkmead@gmail.com>
2022-10-07 11:46:06 +0100

    Attribute number input

--- packages/admin/resources/views/field-types/number/view.blade.php
+++ packages/admin/resources/views/field-types/number/view.blade.php
@@ -1,7 +1,5 @@
-<x-hub::input.text
-    type="number"
-    id="{{ $field['id'] }}"
-    min="{{ $field['configuration']['min'] }}"
-    max="{{ $field['configuration']['max'] }}"
-    wire:model="{{ $field['signature'] }}"
-/>
+<x-hub::input.text type="number"
+                   id="{{ $field['id'] }}"
+                   min="{{ $field['configuration']['min'] }}"
+                   max="{{ $field['configuration']['max'] }}"
+                   wire:model.defer="{{ $field['signature'] }}" />
