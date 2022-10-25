82414c49820400f762c5cf37512f7743307eed10
Alec Ritson <hello@itsalec.co.uk>
2022-10-25 14:20:53 +0100

    Update discounts.php

--- packages/admin/routes/includes/discounts.php
+++ packages/admin/routes/includes/discounts.php
@@ -9,23 +9,9 @@ use Lunar\Hub\Http\Livewire\Pages\Discounts\DiscountsIndex;
  * Channel routes.
  */
 Route::group([
-    // 'middleware' => 'can:catalogue:manage-discounts',
+    'middleware' => 'can:catalogue:manage-discounts',
 ], function () {
     Route::get('/', DiscountsIndex::class)->name('hub.discounts.index');
     Route::get('create', DiscountCreate::class)->name('hub.discounts.create');
     Route::get('{discount}', DiscountShow::class)->name('hub.discounts.show');
-
-//     Route::get('create', ProductCreate::class)->name('hub.products.create');
-//
-//     Route::group([
-//         'prefix' => '{product}',
-//     ], function () {
-//         Route::get('/', ProductShow::class)->name('hub.products.show');
-//
-//         Route::group([
-//             'prefix' => 'variants',
-//         ], function () {
-//             Route::get('{variant}', VariantShow::class)->name('hub.products.variants.show');
-//         });
-//     });
 });
