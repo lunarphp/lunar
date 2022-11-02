7655fd90d1748bf0e33babbf0f6f13f8abd439de
Mark Mead <itsmarkmead@gmail.com>
2022-11-02 09:35:13 +0000

    Initial commit

--- packages/admin/src/Http/Livewire/Components/Settings/Tables/StaffTable.php
+++ packages/admin/src/Http/Livewire/Components/Settings/Tables/StaffTable.php
@@ -2,14 +2,16 @@
 
 namespace Lunar\Hub\Http\Livewire\Components\Settings\Tables;
 
+use Illuminate\Support\Collection;
 use Lunar\Hub\Http\Livewire\Traits\Notifies;
+use Lunar\Hub\Models\SavedSearch;
 use Lunar\Hub\Models\Staff;
-use Lunar\Hub\Tables\LunarTable;
 use Lunar\LivewireTables\Components\Columns\AvatarColumn;
 use Lunar\LivewireTables\Components\Columns\StatusColumn;
 use Lunar\LivewireTables\Components\Columns\TextColumn;
+use Lunar\LivewireTables\Components\Table;
 
-class StaffTable extends LunarTable
+class StaffTable extends Table
 {
     use Notifies;
 
@@ -18,7 +20,22 @@ class StaffTable extends LunarTable
      */
     public bool $filterable = false;
 
+    /**
+     * {@inheritDoc}
+     */
     public bool $searchable = true;
+
+    /**
+     * {@inheritDoc}
+     */
+    public bool $canSaveSearches = true;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $listeners = [
+        'saveSearch' => 'handleSaveSearch',
+    ];
 
     /**
      * {@inheritDoc}
@@ -39,6 +56,69 @@ class StaffTable extends LunarTable
             }),
             TextColumn::make('email'),
         ]);
+    }
+
+    /**
+     * Remove a saved search record.
+     *
+     * @param  int  $id
+     * @return void
+     */
+    public function deleteSavedSearch($id)
+    {
+        SavedSearch::destroy($id);
+
+        $this->resetSavedSearch();
+
+        $this->notify(
+            __('adminhub::notifications.saved_searches.deleted')
+        );
+    }
+
+        /**
+     * Save a search.
+     *
+     * @return void
+     */
+    public function saveSearch()
+    {
+        $this->validateOnly('savedSearchName', [
+            'savedSearchName' => 'required',
+        ]);
+
+        auth()->getUser()->savedSearches()->create([
+            'name' => $this->savedSearchName,
+            'term' => $this->query,
+            'component' => $this->getName(),
+            'filters' => $this->filters,
+        ]);
+
+        $this->notify(
+            __('adminhub::notifications.saved_searches.saved')
+        );
+
+        $this->savedSearchName = null;
+
+        $this->emit('savedSearch');
+    }
+
+    /**
+     * Return the saved searches available to the table.
+     *
+     * @return Collection
+     */
+    public function getSavedSearchesProperty(): Collection
+    {
+        return auth()->getUser()->savedSearches()->whereComponent(
+            $this->getName()
+        )->get()->map(function ($savedSearch) {
+            return [
+                'key' => $savedSearch->id,
+                'label' => $savedSearch->name,
+                'filters' => $savedSearch->filters,
+                'query' => $savedSearch->term,
+            ];
+        });
     }
 
     /**
