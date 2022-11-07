2bde4792862b890858cdda37aa41af962cd63b8c
Aslam <iupin5212@gmail.com>
2022-11-07 16:17:10 +0700

    Add error class to price input

--- packages/admin/resources/views/components/input/price.blade.php
+++ packages/admin/resources/views/components/input/price.blade.php
@@ -7,7 +7,13 @@
         </span>
       </div>
     @endif
-    <x-hub::input.text {{ $attributes }} class="pr-12 {{ $symbol ? 'pl-7' : '' }}" type="number" step="any" />
+    
+    <x-hub::input.text 
+      {{ $attributes->class(['border-red-400' => !!$error, 'pl-7' => $symbol]) }} 
+      class="pr-12" 
+      type="number" 
+      step="any" />
+
     <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
       <span class="text-gray-500 sm:text-sm">
         {{ $currencyCode }}
