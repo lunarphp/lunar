66972884bfba5ede55f9aa942239f1461a5d64ca
Aslam <iupin5212@gmail.com>
2022-11-07 17:58:36 +0700

    Fix - remove duplicate display class

--- packages/admin/resources/views/components/input/group.blade.php
+++ packages/admin/resources/views/components/input/group.blade.php
@@ -1,5 +1,5 @@
 <div>
-  <label for="{{ $for }}" class="flex items-center block text-sm font-medium text-gray-700">
+  <label for="{{ $for }}" class="flex items-center text-sm font-medium text-gray-700">
     <span class="block">{{ $labelPrefix ?? null }}</span>
     <span class="block">{{ $label }}@if($required)<sup class="text-xs text-red-600">&#42;</sup>@endif</span>
   </label>
