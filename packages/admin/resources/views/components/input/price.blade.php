62e615e47017140be55232f3ff18392b48a59ceb
Aslam <iupin5212@gmail.com>
2022-11-07 15:12:13 +0700

    Add pl-7 only if symbol is not empty

--- packages/admin/resources/views/components/input/price.blade.php
+++ packages/admin/resources/views/components/input/price.blade.php
@@ -1,11 +1,13 @@
 <div>
   <div class="relative rounded-md shadow-sm">
-    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-      <span class="text-gray-500 sm:text-sm">
-        {{ str_replace('{value}', '', $symbol) }}
-      </span>
-    </div>
-    <x-hub::input.text {{ $attributes }} class="pr-12 pl-7" type="number" step="any" />
+    @if ($symbol)
+      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <span class="text-gray-500 sm:text-sm">
+          {{ str_replace('{value}', '', $symbol) }}
+        </span>
+      </div>
+    @endif
+    <x-hub::input.text {{ $attributes }} class="pr-12 {{ $symbol ? 'pl-7' : '' }}" type="number" step="any" />
     <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
       <span class="text-gray-500 sm:text-sm">
         {{ $currencyCode }}
