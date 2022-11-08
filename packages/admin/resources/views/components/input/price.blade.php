e85ed07651622d2e95ef332cf5073bcac1610174
Aslam <iupin5212@gmail.com>
2022-11-08 12:04:55 +0700

    Remove symbol

--- packages/admin/resources/views/components/input/price.blade.php
+++ packages/admin/resources/views/components/input/price.blade.php
@@ -1,15 +1,7 @@
 <div>
-  <div class="relative rounded-md shadow-sm">
-    @if ($symbol)
-      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <span class="text-gray-500 sm:text-sm">
-          {{ str_replace('{value}', '', $symbol) }}
-        </span>
-      </div>
-    @endif
-    
+  <div class="relative rounded-md shadow-sm">    
     <x-hub::input.text 
-      {{ $attributes->class(['border-red-400' => !!$error, 'pl-7' => $symbol]) }} 
+      {{ $attributes->class(['border-red-400' => !!$error]) }} 
       class="pr-12" 
       type="number" 
       step="any" />
