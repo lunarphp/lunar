3dc1e5525550da38dee0d13c7d38a0737cc6266f
Mark Mead <itsmarkmead@gmail.com>
2022-11-15 12:52:54 +0000

    Initial commit

--- utils/livewire-tables/resources/views/columns/image.blade.php
+++ utils/livewire-tables/resources/views/columns/image.blade.php
@@ -2,13 +2,14 @@
     @if ($url)
         <a href="{{ call_user_func($url, $record) }}">
     @endif
+
     @if ($thumbnail = $value)
-        <img class="lt-rounded lt-w-10 lt-h-10"
+        <img class="lt-rounded lt-w-16 lt-h-16"
              src="{{ $thumbnail }}"
              loading="lazy" />
     @else
         <x-hub::icon ref="photograph"
-                     class="lt-w-10 lt-h-10 lt-text-gray-300" />
+                     class="lt-w-16 lt-h-16 lt-text-gray-300" />
     @endif
 
     @if ($url)
