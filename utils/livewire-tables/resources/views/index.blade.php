73e9673117c6129ed09d336476fd810294475ba7
Mark Mead <itsmarkmead@gmail.com>
2022-11-17 13:36:42 +0000

    Add clear to search

--- utils/livewire-tables/resources/views/index.blade.php
+++ utils/livewire-tables/resources/views/index.blade.php
@@ -71,11 +71,30 @@
                                         </span>
                                     </label>
 
+
                                     <input type="text"
                                            id="Search"
                                            placeholder="{{ $this->searchPlaceholder }}"
                                            wire:model.debounce.500ms="query"
-                                           class="lt-w-full lt-pl-10 lt-text-sm lt-text-gray-700 lt-border-gray-200 lt-rounded-md lt-form-input focus:lt-outline-none focus:lt-ring focus:lt-ring-blue-100 focus:lt-border-blue-300">
+                                           class="lt-w-full lt-pl-10 lt-text-sm lt-text-gray-700 lt-border-gray-200 lt-rounded-md lt-form-input focus:lt-outline-none focus:lt-ring focus:lt-ring-blue-100 focus:lt-border-blue-300 peer">
+
+                                    <button wire:click="$set('query', '')"
+                                            class="lt-absolute lt-top-1/2 -lt-translate-y-1/2 lt-right-2 lt-rounded-full lt-p-1 hover:lt-bg-gray-100 lt-transition peer-placeholder-shown:hidden">
+                                        <span class="lt-sr-only">Clear</span>
+
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                             fill="none"
+                                             viewBox="0 0 24 24"
+                                             stroke-width="1.5"
+                                             stroke="currentColor"
+                                             class="lt-w-4 lt-h-4">
+                                            <path stroke-linecap="round"
+                                                  stroke-linejoin="round"
+                                                  d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+
+                                    </button>
+
                                 </div>
                             </div>
                         @endif
