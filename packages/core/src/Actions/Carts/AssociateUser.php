d5dbe98e459c938477b2fe745661e753b49cf6a4
Alec Ritson <hello@itsalec.co.uk>
2022-11-25 13:46:56 +0000

    Fixes #752

--- packages/core/src/Actions/Carts/AssociateUser.php
+++ packages/core/src/Actions/Carts/AssociateUser.php
@@ -19,14 +19,14 @@ class AssociateUser extends AbstractAction
     public function execute(Cart $cart, User $user, $policy = 'merge'): self
     {
         if ($policy == 'merge') {
-            $userCart = Cart::whereUserId($user->getKey())->unMerged()->latest()->first();
+            $userCart = Cart::whereUserId($user->getKey())->active()->unMerged()->latest()->first();
             if ($userCart) {
                 app(MergeCart::class)->execute($userCart, $cart);
             }
         }
 
         if ($policy == 'override') {
-            $userCart = Cart::whereUserId($user->getKey())->unMerged()->latest()->first();
+            $userCart = Cart::whereUserId($user->getKey())->active()->unMerged()->latest()->first();
             if ($userCart && $userCart->id != $cart->id) {
                 $userCart->update([
                     'merged_id' => $userCart->id,
