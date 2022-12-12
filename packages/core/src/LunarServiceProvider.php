4ecbec6d634ebf3ac085b989b25437b78a9d9b23
Alec Ritson <hello@itsalec.co.uk>
2022-12-12 08:21:21 +0000

    Add command to sync orders

--- packages/core/src/LunarServiceProvider.php
+++ packages/core/src/LunarServiceProvider.php
@@ -38,6 +38,7 @@ use Lunar\Console\Commands\AddonsDiscover;
 use Lunar\Console\Commands\Import\AddressData;
 use Lunar\Console\Commands\MigrateGetCandy;
 use Lunar\Console\Commands\ScoutIndexer;
+use Lunar\Console\Commands\Orders\SyncNewCustomerOrders;
 use Lunar\Console\InstallLunar;
 use Lunar\Database\State\ConvertProductTypeAttributesToProducts;
 use Lunar\Database\State\EnsureBrandsAreUpgraded;
@@ -188,6 +189,7 @@ class LunarServiceProvider extends ServiceProvider
                 AddressData::class,
                 ScoutIndexer::class,
                 MigrateGetCandy::class,
+                SyncNewCustomerOrders::class,
             ]);
         }
 
