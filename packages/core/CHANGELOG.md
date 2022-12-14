09ca80d5793354f20ee3a95b0ce68fe9a152cb51
Alec Ritson <hello@itsalec.co.uk>
2022-12-14 16:36:04 +0000

    Fix missing soft delete trait

--- packages/core/CHANGELOG.md
+++ packages/core/CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+[Unreleased]
+
+### Fixed
+
+- Added missing `SoftDeletes` trait to `ProductVariant` model.
+
 ## 0.1.4
 
 ### Fixed
--- packages/core/src/Models/ProductVariant.php
+++ packages/core/src/Models/ProductVariant.php
@@ -3,6 +3,7 @@
 namespace Lunar\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Collection;
 use Lunar\Base\BaseModel;
 use Lunar\Base\Casts\AsAttributeData;
@@ -23,6 +24,7 @@ class ProductVariant extends BaseModel implements Purchasable
     use HasDimensions;
     use HasTranslations;
     use HasMacros;
+    use SoftDeletes;
 
     /**
      * Define the guarded attributes.
