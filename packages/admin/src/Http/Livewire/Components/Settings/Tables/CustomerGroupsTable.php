30ce62d3a92155e800eabc50a2778b2d87d9321f
Alec Ritson <hello@itsalec.co.uk>
2022-12-20 14:42:21 +0000

    Update CustomerGroupsTable.php

--- packages/admin/src/Http/Livewire/Components/Settings/Tables/CustomerGroupsTable.php
+++ packages/admin/src/Http/Livewire/Components/Settings/Tables/CustomerGroupsTable.php
@@ -2,9 +2,7 @@
 
 namespace Lunar\Hub\Http\Livewire\Components\Settings\Tables;
 
-use Illuminate\Support\Collection;
 use Lunar\Hub\Http\Livewire\Traits\Notifies;
-use Lunar\Hub\Models\SavedSearch;
 use Lunar\Hub\Tables\LunarTable;
 use Lunar\LivewireTables\Components\Columns\StatusColumn;
 use Lunar\LivewireTables\Components\Columns\TextColumn;
@@ -13,28 +11,6 @@ use Lunar\Models\CustomerGroup;
 class CustomerGroupsTable extends LunarTable
 {
     use Notifies;
-
-    /**
-     * {@inheritDoc}
-     */
-    public bool $filterable = false;
-
-    /**
-     * {@inheritDoc}
-     */
-    public bool $searchable = false;
-
-    /**
-     * {@inheritDoc}
-     */
-    public bool $canSaveSearches = true;
-
-    /**
-     * {@inheritDoc}
-     */
-    protected $listeners = [
-        'saveSearch' => 'handleSaveSearch',
-    ];
 
     /**
      * {@inheritDoc}
@@ -53,79 +29,10 @@ class CustomerGroupsTable extends LunarTable
     }
 
     /**
-     * Remove a saved search record.
-     *
-     * @param  int  $id
-     * @return void
-     */
-    public function deleteSavedSearch($id)
-    {
-        SavedSearch::destroy($id);
-
-        $this->resetSavedSearch();
-
-        $this->notify(
-            __('adminhub::notifications.saved_searches.deleted')
-        );
-    }
-
-    /**
-     * Save a search.
-     *
-     * @return void
-     */
-    public function saveSearch()
-    {
-        $this->validateOnly('savedSearchName', [
-            'savedSearchName' => 'required',
-        ]);
-
-        auth()->getUser()->savedSearches()->create([
-            'name' => $this->savedSearchName,
-            'term' => $this->query,
-            'component' => $this->getName(),
-            'filters' => $this->filters,
-        ]);
-
-        $this->notify(
-            __('adminhub::notifications.saved_searches.saved')
-        );
-
-        $this->savedSearchName = null;
-
-        $this->emit('savedSearch');
-    }
-
-    /**
-     * Return the saved searches available to the table.
-     *
-     * @return Collection
-     */
-    public function getSavedSearchesProperty(): Collection
-    {
-        return auth()->getUser()->savedSearches()->whereComponent(
-            $this->getName()
-        )->get()->map(function ($savedSearch) {
-            return [
-                'key' => $savedSearch->id,
-                'label' => $savedSearch->name,
-                'filters' => $savedSearch->filters,
-                'query' => $savedSearch->term,
-            ];
-        });
-    }
-
-    /**
      * {@inheritDoc}
      */
     public function getData()
     {
-        $query = CustomerGroup::query();
-
-        if ($this->query) {
-            $query->search($this->query, true);
-        }
-
-        return $query->paginate($this->perPage);
+        return CustomerGroup::paginate($this->perPage);
     }
 }
