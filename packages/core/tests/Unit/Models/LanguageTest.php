9ca0be30fae4547c341dedb32719ce92fcf04a14
Ruben van Erk <ruben@rubenvanerk.nl>
2022-12-25 22:13:43 +0100

    Add failing test for deleting languages

--- packages/core/tests/Unit/Models/LanguageTest.php
+++ packages/core/tests/Unit/Models/LanguageTest.php
@@ -3,7 +3,10 @@
 namespace Lunar\Tests\Unit\Models;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
 use Lunar\Models\Language;
+use Lunar\Models\Product;
+use Lunar\Models\Url;
 use Lunar\Tests\TestCase;
 
 class LanguageTest extends TestCase
@@ -22,5 +25,48 @@ class LanguageTest extends TestCase
         $this->assertEquals('fr', $language->code);
         $this->assertEquals('Français', $language->name);
         $this->assertTrue($language->default);
+    }
+
+    /** @test */
+    public function can_delete_a_language()
+    {
+        $product = Product::factory()->create();
+        $dutch = Language::factory()->create([
+            'code' => 'fr',
+            'name' => 'Français',
+            'default' => true,
+        ]);
+
+        $french = Language::factory()->create([
+            'code' => 'fr',
+            'name' => 'Français',
+            'default' => false,
+        ]);
+
+        foreach ([$dutch, $french] as $language) {
+            $data = [
+                'language_id' => $language->id,
+                'element_id' => $product->id,
+                'element_type' => Product::class,
+                'slug' => Str::slug($product->translateAttribute('name')),
+                'default' => true,
+            ];
+
+            Url::create($data);
+        }
+
+        $this->assertDatabaseHas('lunar_urls', [
+            'language_id' => $french->id,
+            'element_id' => $product->id,
+            'element_type' => Product::class,
+        ]);
+
+        $french->delete();
+
+        $this->assertDatabaseMissing('lunar_urls', [
+            'language_id' => $french->id,
+            'element_id' => $product->id,
+            'element_type' => Product::class,
+        ]);
     }
 }
