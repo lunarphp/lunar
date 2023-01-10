57b58b65a315597eb91a244d09973868ec459921
wychoong <67364036+wychoong@users.noreply.github.com>
2023-01-10 18:20:13 +0800

    show selected options

--- packages/admin/resources/views/partials/products/editing/options.blade.php
+++ packages/admin/resources/views/partials/products/editing/options.blade.php
@@ -4,7 +4,9 @@
   ])
   <div>
     <div class="pt-4 mt-4 space-x-4 border-t">
-      @livewire('hub.components.products.options.option-selector')
+      @livewire('hub.components.products.options.option-selector', [
+        'selected' => $options->pluck('id')->toArray(),
+      ])
     </div>
   </div>
 </div>
