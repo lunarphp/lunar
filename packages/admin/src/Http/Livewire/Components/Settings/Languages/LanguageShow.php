ef828a161cd148d187bad3373193f1e5b8ac6115
Ruben van Erk <ruben@rubenvanerk.nl>
2023-01-10 19:41:58 +0100

    Convert unique language code rule to string

--- packages/admin/src/Http/Livewire/Components/Settings/Languages/LanguageShow.php
+++ packages/admin/src/Http/Livewire/Components/Settings/Languages/LanguageShow.php
@@ -34,12 +34,7 @@ class LanguageShow extends Component
     {
         return [
             'language.name' => 'required|string|max:255',
-            'language.code' => [
-                'required',
-                'string',
-                'max:255',
-                Rule::unique(Language::class, 'code')->ignore($this->language->id)
-            ],
+            'language.code' => 'required|string|max:255|unique:'.Language::class.',code,'.$this->language->id,
             'language.default' => 'nullable|boolean',
         ];
     }
