619fa1a2aa8f4c9193c5acf873dd6bb9e8348afa
Alec Ritson <hello@itsalec.co.uk>
2023-01-12 13:11:57 +0000

    Squashed commit of the following:
    commit 0f445c9ffff1a3614b05b5b5d0ee43b4be1745eb
    Author: Maurice Ellis <maurice@328studios.co>
    Date:   Thu Jan 12 04:49:33 2023 -0700
    
        Hotfix - Properly handle adding media when disk is not local (#701)
    
        * Properly handle adding media when disk is not local
    
        In order to obtain the media from a remote location, addMediaFromDisk has to be used. addMedia only works when the file is stored on the server. You can set a default media disk for Spatie Media Library, so I used that config and then determined if it was local or not, so the image can be found and added properly.
    
        * Added storage facade
    
        * Pint fixes
    
        * Pint
    
        * Updated variables to camelcase
    
        * Update HasImages.php
    
        Co-authored-by: Alec Ritson <hello@itsalec.co.uk>
    
    commit 19ae84ddc81ce76ce084bead7278c7ee2334aeb7
    Author: Ruben van Erk <github@accounts.rubenvanerk.nl>
    Date:   Wed Jan 11 08:25:16 2023 +0100
    
        Check language code unique when updating (#786)
    
        * Add unique rule for code when updating a language
    
        * Convert unique language code rule to string
    
    commit be0aca5b4c9bc358be3c2e698c1cd73e7ff2209b
    Author: Alec Ritson <hello@itsalec.co.uk>
    Date:   Fri Dec 2 10:53:40 2022 +0000
    
        Release - 0.1.5 (#765)
    
        * Update CHANGELOG.md
    
        * Changelog and assets

--- packages/admin/src/Http/Livewire/Traits/HasImages.php
+++ packages/admin/src/Http/Livewire/Traits/HasImages.php
@@ -4,6 +4,7 @@ namespace Lunar\Hub\Http\Livewire\Traits;
 
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Livewire\TemporaryUploadedFile;
 use Spatie\Activitylog\Facades\LogBatch;
@@ -251,6 +252,20 @@ trait HasImages
                         ->beforeLast('.')
                         ->substr(0, 128)
                         ->append('.', $file->getClientOriginalExtension());
+
+                    $mediaLibaryDisk = config('media-library.disk_name');
+                    $mediaLibaryDriverConfig = Storage::disk($mediaLibaryDisk)->getConfig();
+                    $mediaLibaryDriver = $mediaLibaryDriverConfig['driver'];
+
+                    if ($mediaLibaryDriver == 'local') {
+                        $media = $owner->addMedia($file->getRealPath())
+                            ->usingFileName($filename)
+                            ->toMediaCollection('images');
+                    } else {
+                        $media = $owner->addMediaFromDisk($file->getRealPath())
+                            ->usingFileName($filename)
+                            ->toMediaCollection('images');
+                    }
 
                     $media = $owner->addMedia($file->getRealPath())
                         ->usingFileName($filename)
