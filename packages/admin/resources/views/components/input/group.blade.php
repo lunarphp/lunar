c431933640978cfab8f54710e57e30f13b879d98
wychoong <67364036+wychoong@users.noreply.github.com>
2023-01-17 23:20:35 +0800

    update to use errors component for errors msg

--- packages/admin/resources/views/components/input/group.blade.php
+++ packages/admin/resources/views/components/input/group.blade.php
@@ -20,28 +20,5 @@
         <p class="mt-2 text-sm text-gray-500">{{ $instructions }}</p>
     @endif
 
-    @if ($error)
-        <div class="flex items-center gap-1 mt-2">
-            @if ($errorIcon)
-                <x-hub::icon ref="exclamation-circle"
-                             class="w-5 h-5 text-red-500" />
-            @endif
-
-            <p class="text-sm text-red-600">{{ $error }}</p>
-        </div>
-    @endif
-
-    @if (count($errors))
-        <div class="space-y-1">
-            @foreach ($errors as $error)
-                @if (is_array($error))
-                    @foreach ($error as $text)
-                        <p class="text-sm text-red-600">{{ $text }}</p>
-                    @endforeach
-                @else
-                    <p class="text-sm text-red-600">{{ $error }}</p>
-                @endif
-            @endforeach
-        </div>
-    @endif
+    <x-hub::errors :error="$error" :errors="$errors" />
 </div>
