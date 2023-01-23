0c4987d0d2312ed071c29733615c8ff07a765101
Ruben van Erk <github@accounts.rubenvanerk.nl>
2023-01-23 16:25:41 +0100

    Remove @

--- utils/livewire-tables/resources/views/columns/badge.blade.php
+++ utils/livewire-tables/resources/views/columns/badge.blade.php
@@ -1,9 +1,9 @@
 <div>
     <span @class([
         'lt-text-xs lt-inline-block lt-py-1 lt-px-2 lt-rounded',
-        'lt-text-green-600 lt-bg-green-50' => !@empty($success),
-        'lt-text-yellow-600 lt-bg-yellow-50' => !@empty($warning),
-        'lt-text-red-600 lt-bg-red-50' => !@empty($danger),
+        'lt-text-green-600 lt-bg-green-50' => !empty($success),
+        'lt-text-yellow-600 lt-bg-yellow-50' => !empty($warning),
+        'lt-text-red-600 lt-bg-red-50' => !empty($danger),
     ])>
         {{ $value }}
     </span>
