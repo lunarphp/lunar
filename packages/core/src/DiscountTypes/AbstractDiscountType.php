b86607461cb5cbdacd9157194d97fabbb112fe4f
Ryan Mitchell <ryan@thoughtcollective.com>
2023-01-26 12:03:15 +0000

    Increment discount uses as part of order creation (#814)
    * Increment discount uses as part of order creation
    
    * Handle discounts being nullable
    
    * Create test and bug fix

--- packages/core/src/DiscountTypes/AbstractDiscountType.php
+++ packages/core/src/DiscountTypes/AbstractDiscountType.php
@@ -12,7 +12,7 @@ abstract class AbstractDiscountType implements DiscountTypeInterface
      *
      * @var Discount
      */
-    protected Discount $discount;
+    public Discount $discount;
 
     /**
      * Set the data for the discount to user.
@@ -23,6 +23,18 @@ abstract class AbstractDiscountType implements DiscountTypeInterface
     public function with(Discount $discount): self
     {
         $this->discount = $discount;
+
+        return $this;
+    }
+    
+    /**
+     * Mark a discount as used
+     *
+     * @return self
+     */
+    public function markAsUsed(): self
+    {
+        $this->discount->uses = $this->discount->uses + 1; 
 
         return $this;
     }
