78e37d8987aeb73548bb6122f30952ef56621820
Ivanka Todorova <fakeheal@gmail.com>
2023-02-01 16:44:47 +0200

    chore: add tests for validating attribute fields (required/optional) and their additional rules

--- packages/admin/tests/Unit/Http/Livewire/Components/Products/ProductCreateTest.php
+++ packages/admin/tests/Unit/Http/Livewire/Components/Products/ProductCreateTest.php
@@ -4,9 +4,13 @@ namespace Lunar\Hub\Tests\Unit\Http\Livewire\Components\Products;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
+use Lunar\FieldTypes\Number;
+use Lunar\FieldTypes\Text;
+use Lunar\FieldTypes\TranslatedText;
 use Lunar\Hub\Http\Livewire\Components\Products\ProductCreate;
 use Lunar\Hub\Models\Staff;
 use Lunar\Hub\Tests\TestCase;
+use Lunar\Models\Attribute;
 use Lunar\Models\Brand;
 use Lunar\Models\Collection;
 use Lunar\Models\Currency;
@@ -52,7 +56,7 @@ class ProductCreateTest extends TestCase
         ProductType::factory()->create();
     }
 
-    /** @test  */
+    /** @test */
     public function component_mounts_correctly()
     {
         $staff = Staff::factory()->create([
@@ -135,15 +139,17 @@ class ProductCreateTest extends TestCase
                     'name' => $productC->translateAttribute('name'),
                     'type' => 'cross-sell',
                 ],
-            ]))->set('collections', collect([[
-                'id' => $collection->id,
-                'name' => $collection->translateAttribute('name'),
-                'group_id' => $collection->collection_group_id,
-                'group_name' => $collection->group->name,
-                'thumbnail' => null,
-                'breadcrumb' => ['Foo', 'Bar'],
-                'position' => 1,
-            ]]))
+            ]))->set('collections', collect([
+                [
+                    'id' => $collection->id,
+                    'name' => $collection->translateAttribute('name'),
+                    'group_id' => $collection->collection_group_id,
+                    'group_name' => $collection->group->name,
+                    'thumbnail' => null,
+                    'breadcrumb' => ['Foo', 'Bar'],
+                    'position' => 1,
+                ]
+            ]))
             ->call('save')
             ->assertHasNoErrors();
 
@@ -180,5 +186,216 @@ class ProductCreateTest extends TestCase
             'element_type' => Product::class,
             'element_id' => $component->get('product.id'),
         ]);
+    }
+
+    /** @test */
+    public function validates_required_translated_text_attribute()
+    {
+        // value for attribute in default language MUST be REQUIRED
+        // value for attribute in additional languages are optional
+        $staff = Staff::factory()->create([
+            'admin' => true,
+        ]);
+
+        $language = Language::getDefault();
+
+        $attribute = Attribute::factory()->create([
+            'type' => TranslatedText::class,
+            'required' => true,
+        ]);
+
+        $productType = ProductType::all()->first();
+
+        $productType->mappedAttributes()->attach($attribute);
+
+        LiveWire::actingAs($staff, 'staff')
+            ->test(ProductCreate::class)
+            ->call('save')
+            ->assertHasErrors([
+                "attributeMapping.a_{$attribute->id}.data.{$language->code}" => 'required',
+            ])
+            ->assertHasNoErrors([
+                "attributeMapping.a_{$attribute->id}.data.fr" => 'required'
+            ]);
+    }
+
+    /** @test */
+    public function validates_optional_translated_text_attribute()
+    {
+        // optional attribute must only apply user set validation rules
+        $staff = Staff::factory()->create([
+            'admin' => true,
+        ]);
+
+        $language = Language::getDefault();
+
+        $attribute = Attribute::factory()->create([
+            'type' => TranslatedText::class,
+            'required' => false,
+            'validation_rules' => 'string,max:9'
+        ]);
+
+        $productType = ProductType::all()->first();
+
+        $productType->mappedAttributes()->attach($attribute);
+
+        LiveWire::actingAs($staff, 'staff')
+            ->test(ProductCreate::class)
+            ->set("attributeMapping.a_{$attribute->id}.data.{$language->code}", 'more than ten letters')
+            ->call('save')
+            ->assertHasNoErrors([
+                "attributeMapping.a_{$attribute->id}.data.{$language->code}" => 'required',
+            ])
+            ->assertHasErrors([
+                "attributeMapping.a_{$attribute->id}.data.{$language->code}" => 'max'
+            ]);
+    }
+
+    /** @test */
+    public function validates_required_text_attribute()
+    {
+        $staff = Staff::factory()->create([
+            'admin' => true,
+        ]);
+
+        $attribute = Attribute::factory()->create([
+            'type' => Text::class,
+            'required' => true,
+        ]);
+
+        $productType = ProductType::all()->first();
+
+        $productType->mappedAttributes()->attach($attribute);
+
+        LiveWire::actingAs($staff, 'staff')
+            ->test(ProductCreate::class)
+            ->call('save')
+            ->assertHasErrors([
+                "attributeMapping.a_{$attribute->id}.data" => 'required',
+            ]);
+
+        Livewire::actingAs($staff, 'staff')
+            ->test(ProductCreate::class)
+            ->set("attributeMapping.a_{$attribute->id}.data", 'some value')
+            ->call('save')
+            ->assertHasNoErrors([
+                "attributeMapping.a_{$attribute->id}.data" => 'required'
+            ]);
+    }
+
+    /** @test */
+    public function validates_optional_text_attribute()
+    {
+        // with additional rules
+        $staff = Staff::factory()->create([
+            'admin' => true,
+        ]);
+
+        $attribute = Attribute::factory()->create([
+            'type' => Text::class,
+            'required' => false,
+            'validation_rules' => 'numeric'
+        ]);
+
+        $productType = ProductType::all()->first();
+
+        $productType->mappedAttributes()->attach($attribute);
+
+        // it is not required for realsies
+        LiveWire::actingAs($staff, 'staff')
+            ->test(ProductCreate::class)
+            ->call('save')
+            ->assertHasNoErrors([
+                "attributeMapping.a_{$attribute->id}.data" => 'required',
+            ]);
+
+        // fail the additional validation rules
+        LiveWire::actingAs($staff, 'staff')
+            ->test(ProductCreate::class)
+            ->set("attributeMapping.a_{$attribute->id}.data", 'text not a number')
+            ->call('save')
+            ->assertHasErrors([
+                "attributeMapping.a_{$attribute->id}.data" => 'numeric',
+            ]);
+
+        // pass the validation
+        LiveWire::actingAs($staff, 'staff')
+            ->test(ProductCreate::class)
+            ->set("attributeMapping.a_{$attribute->id}.data", '42')
+            ->call('save')
+            ->assertHasNoErrors([
+                "attributeMapping.a_{$attribute->id}.data",
+            ]);
+    }
+
+    /** @test */
+    public function validates_required_number_attribute()
+    {
+        $staff = Staff::factory()->create([
+            'admin' => true,
+        ]);
+
+        $attribute = Attribute::factory()->create([
+            'type' => Number::class,
+            'required' => true,
+            'configuration' => ['min' => 2, 'max' => 5],
+        ]);
+
+        $productType = ProductType::all()->first();
+
+        $productType->mappedAttributes()->attach($attribute);
+
+        // fail the required rule
+        LiveWire::actingAs($staff, 'staff')
+            ->test(ProductCreate::class)
+            ->call('save')
+            ->assertHasErrors([
+                "attributeMapping.a_{$attribute->id}.data" => 'required',
+            ]);
+
+        // pass the required rule
+        LiveWire::actingAs($staff, 'staff')
+            ->test(ProductCreate::class)
+            ->set("attributeMapping.a_{$attribute->id}.data", 3)
+            ->call('save')
+            ->assertHasNoErrors([
+                "attributeMapping.a_{$attribute->id}.data" => 'required',
+            ]);
+    }
+
+    /** @test */
+    public function validates_optional_number_attribute()
+    {
+        $staff = Staff::factory()->create([
+            'admin' => true,
+        ]);
+
+        $attribute = Attribute::factory()->create([
+            'type' => Number::class,
+            'required' => true,
+            'configuration' => ['min' => 2, 'max' => 5],
+        ]);
+
+        $productType = ProductType::all()->first();
+
+        $productType->mappedAttributes()->attach($attribute);
+
+        // fail the configuration rules
+        LiveWire::actingAs($staff, 'staff')
+            ->test(ProductCreate::class)
+            ->set("attributeMapping.a_{$attribute->id}.data", 10)
+            ->call('save')
+            ->assertHasErrors([
+                "attributeMapping.a_{$attribute->id}.data",
+            ]);
+
+        // pass the configuration rules
+        LiveWire::actingAs($staff, 'staff')
+            ->test(ProductCreate::class)
+            ->set("attributeMapping.a_{$attribute->id}.data", 3)
+            ->call('save')
+            ->assertHasNoErrors([
+                "attributeMapping.a_{$attribute->id}.data",
+            ]);
     }
 }
