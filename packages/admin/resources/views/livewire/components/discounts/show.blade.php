2ee7691b6314c608f7f5e3eb7220215f64191f56
wychoong <67364036+wychoong@users.noreply.github.com>
2023-02-11 14:07:33 +0800

    ui tweaks for Delete danger zone and Save button

--- packages/admin/resources/views/livewire/components/discounts/show.blade.php
+++ packages/admin/resources/views/livewire/components/discounts/show.blade.php
@@ -18,27 +18,4 @@
       wire:submit.prevent="save">
         @include('adminhub::partials.forms.discount')
     </form>
-
-    <div class="space-y-4">
-        <div class="bg-white border border-red-300 rounded shadow">
-          <header class="px-6 py-4 text-red-700 bg-white border-b border-red-300 rounded-t">
-            {{ __('adminhub::inputs.danger_zone.title') }}
-          </header>
-          <div class="p-6 space-y-4 text-sm">
-            <div class="grid grid-cols-12 gap-4">
-              <div class="col-span-12 md:col-span-6">
-                <strong>{{ __('adminhub::components.discounts.show.danger_zone.label') }}</strong>
-                <p class="text-xs text-gray-600">{{ __('adminhub::components.discounts.show.danger_zone.instructions') }}</p>
-              </div>
-              <div class="col-span-9 lg:col-span-4">
-                <x-hub::input.text type="email" wire:model="deleteConfirm" />
-              </div>
-              <div class="col-span-3 text-right lg:col-span-2">
-                <x-hub::button theme="danger" :disabled="!$this->canDelete" wire:click="delete" type="button">{{ __('adminhub::global.delete') }}</x-hub::button>
-              </div>
-            </div>
-          </div>
-        </div>
-    </div>
-
 </div>
