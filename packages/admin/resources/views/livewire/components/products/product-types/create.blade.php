15228abbce4a33b303f2d856e185ba220129517a
wychoong <67364036+wychoong@users.noreply.github.com>
2023-02-16 00:27:21 +0800

    [0.2] Hotfix - Show delete discount  (#840)

--- packages/admin/resources/views/livewire/components/products/product-types/create.blade.php
+++ packages/admin/resources/views/livewire/components/products/product-types/create.blade.php
@@ -1,12 +1,6 @@
 <div class="space-y-6">
     @include('adminhub::partials.forms.product-type')
-    <div
-        class="fixed bottom-0 left-0 right-0 z-40 p-6 border-t border-gray-100 lg:left-auto bg-white/75"
-        :class="{
-            'lg:w-[calc(100vw_-_16rem)]': !menuCollapsed,
-            'w-full': menuCollapsed
-        }"
-    >
+    <x-hub::layout.bottom-panel>
         <div class="flex justify-end">
             <form action="#"
                   method="POST"
@@ -16,4 +10,4 @@
                 </x-hub::button>
             </form </div>
         </div>
-    </div>
+    </x-hub::layout.bottom-panel>
