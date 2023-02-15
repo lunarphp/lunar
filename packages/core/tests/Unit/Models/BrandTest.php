244c0ca19221dbe759320311b27462f1e6fa71c5
wychoong <67364036+wychoong@users.noreply.github.com>
2023-02-15 21:41:29 +0800

    add test

--- packages/core/tests/Unit/Models/BrandTest.php
+++ packages/core/tests/Unit/Models/BrandTest.php
@@ -3,7 +3,12 @@
 namespace Lunar\Tests\Unit\Models;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
+use Lunar\Generators\UrlGenerator;
 use Lunar\Models\Brand;
+use Lunar\Models\Language;
+use Lunar\Models\Url;
 use Lunar\Tests\TestCase;
 
 class BrandTest extends TestCase
@@ -17,5 +22,25 @@ class BrandTest extends TestCase
             'name' => 'Test Brand',
         ]);
         $this->assertEquals('Test Brand', $brand->name);
+    }
+
+    /** @test */
+    public function can_make_brand_with_slug()
+    {
+        Config::set('lunar.urls.generator', UrlGenerator::class);
+
+        Language::factory()->create(['default' => true]);
+
+        $brand = Brand::factory()->create([
+            'name' => 'Test Brand',
+        ]);
+
+        $this->assertDatabaseHas((new Url)->getTable(), [
+            'element_type' => Brand::class,
+            'element_id' => $brand->id,
+            'slug' => Str::slug($brand->name),
+        ]);
+
+        $this->assertCount(1, $brand->refresh()->urls);
     }
 }
