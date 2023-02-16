c4eb6312ba01b29168b35add24de5babd7a00e0e
wychoong <67364036+wychoong@users.noreply.github.com>
2023-02-16 19:05:26 +0800

    change default payment type

--- packages/admin/src/Views/Components/Orders/Status.php
+++ packages/admin/src/Views/Components/Orders/Status.php
@@ -42,7 +42,7 @@ class Status extends Component
 
         $match = $statuses[$status] ?? null;
 
-        $this->label = $match['label'] ?? $status;
+        $this->label = $match['label'] ?? 'N/A';
         $this->color = $match['color'] ?? '#7C7C7C';
         $this->status = $record?->status ?: $status;
     }
