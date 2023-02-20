d9990bd1164d03aca9aaa046ef843f5917638da5
Alec Ritson <hello@itsalec.co.uk>
2023-02-20 09:35:15 +0000

    Create CalculateTax.php

---
+++ packages/core/src/Pipelines/Cart/CalculateTax.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Lunar\Pipelines\Cart;
+
+use Closure;
+use Lunar\Base\ValueObjects\Cart\TaxBreakdown;
+use Lunar\DataTypes\Price;
+use Lunar\Facades\Taxes;
+use Lunar\Models\Cart;
+
+class CalculateTax
+{
+    /**
+     * Called just before cart totals are calculated.
+     *
+     * @return void
+     */
+    public function handle(Cart $cart, Closure $next)
+    {
+        $taxBreakDown = new TaxBreakdown;
+
+        foreach ($cart->lines as $cartLine) {
+            $subTotal = $cartLine->subTotal?->value;
+
+            $unitQuantity = $cartLine->purchasable->getUnitQuantity();
+
+            if (!is_null($cartLine->subTotalDiscounted?->value)) {
+                $subTotal = $cartLine->subTotalDiscounted?->value;
+            }
+
+            $taxBreakDown = Taxes::setShippingAddress($cart->shippingAddress)
+                ->setBillingAddress($cart->billingAddress)
+                ->setCurrency($cart->currency)
+                ->setPurchasable($cartLine->purchasable)
+                ->setCartLine($cartLine)
+                ->getBreakdown($subTotal);
+
+            $taxTotal = $taxBreakDown->amounts->sum('price.value');
+
+            $cartLine->taxBreakdown = $taxBreakDown;
+
+            $cart->taxTotal = new Price($taxTotal, $cart->currency, 1);
+            $cartLine->taxAmount = new Price($taxTotal, $cart->currency, $unitQuantity);
+            $cartLine->total = new Price($subTotal + $taxTotal, $cart->currency, $unitQuantity);
+        }
+
+        $taxTotal = $cart->lines->sum('taxAmount.value');
+        $taxBreakDownAmounts = $taxBreakDown->amounts->filter()->flatten();
+
+        if ($shippingAddress = $cart->shippingAddress) {
+            $taxTotal += $shippingAddress->taxTotal?->value;
+            $shippingTaxBreakdown = $shippingAddress->taxBreakdown;
+
+            if ($shippingTaxBreakdown) {
+                $taxBreakDownAmounts = $taxBreakDownAmounts->merge(
+                    $shippingTaxBreakdown->amounts
+                );
+            }
+        }
+
+        $cart->taxTotal = new Price($taxTotal, $cart->currency, 1);
+
+        // Need to include shipping tax breakdown...
+        $cart->taxBreakdown = $taxBreakDownAmounts->groupBy('identifier')->map(function ($amounts) use ($cart) {
+            return [
+                'percentage' => $amounts->first()->percentage,
+                'description' => $amounts->first()->description,
+                'identifier' => $amounts->first()->identifier,
+                'amounts' => $amounts,
+                'total' => new Price($amounts->sum('price.value'), $cart->currency, 1),
+            ];
+        });
+
+        return $next($cart);
+    }
+}
