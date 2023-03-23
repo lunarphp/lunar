ba08f4b9404cd6c72fdc71bdcc319d2296c4a7ab
wychoong <67364036+wychoong@users.noreply.github.com>
2023-03-23 18:30:28 +0800

    add method directly set shipping options

--- packages/core/src/Base/ShippingManifestInterface.php
+++ packages/core/src/Base/ShippingManifestInterface.php
@@ -16,6 +16,14 @@ interface ShippingManifestInterface
     public function addOption(ShippingOption $shippingOption);
 
     /**
+     * Add a collection of shipping options to the manifest.
+     *
+     * @param  \Lunar\DataTypes\ShippingOption  $shippingOption
+     * @return self
+     */
+    public function addOptions(Collection $shippingOptions);
+
+    /**
      * Remove all shipping options
      *
      * @return self
