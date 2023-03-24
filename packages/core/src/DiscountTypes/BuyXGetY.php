c180263fe2b931c21385a07d3c60979cd831ce8d
Ryan Mitchell <ryan@thoughtcollective.com>
2023-03-24 12:40:04 +0000

    Refactor inline with latest 0.2 changes

--- packages/core/src/DiscountTypes/BuyXGetY.php
+++ packages/core/src/DiscountTypes/BuyXGetY.php
@@ -2,6 +2,8 @@
 
 namespace Lunar\DiscountTypes;
 
+use Illuminate\Pipeline\Pipeline;
+use Illuminate\Support\Collection;
 use Lunar\Base\ValueObjects\Cart\DiscountBreakdown;
 use Lunar\Base\ValueObjects\Cart\DiscountBreakdownLine;
 use Lunar\DataTypes\Price;
@@ -78,13 +80,12 @@ class BuyXGetY extends AbstractDiscountType
             $maxRewardQty
         );
 
-
         if (! $totalRewardQty) {
             return $cart;
         }
 
         $remainingRewardQty = $totalRewardQty;
-        
+
         // ignore (for now) any lines added by this discount
         $lines = $cart->lines->reject(function ($line) {
             return in_array($this->discount->id, array_keys($line->meta->added_by_discount ?? []));
@@ -101,7 +102,6 @@ class BuyXGetY extends AbstractDiscountType
             });
         })->sortBy('subTotal.value');
 
-
         foreach ($rewardLines as $rewardLine) {
             if (! $remainingRewardQty) {
                 continue;
@@ -109,11 +109,10 @@ class BuyXGetY extends AbstractDiscountType
 
             $remainder = $rewardLine->quantity % $remainingRewardQty;
 
-
             $qtyToAllocate = (int) round(($remainingRewardQty - $remainder) / $rewardLine->quantity);
 
-            if (!$remainder && $remainingRewardQty < $rewardLine->quantity) {
-               $qtyToAllocate = $remainingRewardQty;
+            if (! $remainder && $remainingRewardQty < $rewardLine->quantity) {
+                $qtyToAllocate = $remainingRewardQty;
             }
 
             if (! $qtyToAllocate) {
@@ -124,7 +123,6 @@ class BuyXGetY extends AbstractDiscountType
                 line: $rewardLine,
                 quantity: $qtyToAllocate
             ));
-
 
             $conditionQtyToAllocate = $qtyToAllocate * $rewardQty;
             $conditions->each(function ($conditionLine) use ($affectedLines, &$conditionQtyToAllocate) {
@@ -169,104 +167,150 @@ class BuyXGetY extends AbstractDiscountType
 
             $cart->freeItems->push($rewardLine->purchasable);
         }
-        
+
         if ($automaticallyAddRewards) {
-            $cart = $this->processAutomaticRewards($cart, $remainingRewardQty);
+            [$affectedLines, $discountTotal] = $this->processAutomaticRewards($cart, $remainingRewardQty, $affectedLines, $discountTotal);
         }
-        
+
+        $this->addDiscountBreakdown($cart, new DiscountBreakdown(
+            discount: $this->discount,
+            lines: $affectedLines,
+            price: new Price($discountTotal, $cart->currency, 1)
+        ));
+
         return $cart;
-        
     }
-      
-    private function processAutomaticRewards(Cart $cart, int $remainingRewardQty) 
-    {  
+
+    private function processAutomaticRewards(Cart $cart, int $remainingRewardQty, Collection $affectedLines, int $discountTotal)
+    {
         $automaticLines = $cart->lines->filter(function ($line) {
             return in_array($this->discount->id, array_keys($line->meta->added_by_discount ?? []));
         });
-        
+
         $remainingRewardQty -= $automaticLines->sum(function ($line) {
             return $line->meta->added_by_discount[$this->discount->id] ?? 0;
         });
-        
+
         // we have lines to add
         if ($remainingRewardQty > 0) {
-            
             while ($remainingRewardQty > 0) {
-                
-                $rewardLine = $this->discount->purchasableRewards->random()->purchasable;
-                $purchasable = $rewardLine->variants->first();
-                
+                $selectedRewardItem = $this->discount->purchasableRewards->random()->purchasable;
+                $purchasable = $selectedRewardItem->variants->first();
+
                 // is it already in cart?
-                $line = $cart->lines->first(function ($line) use ($purchasable) {
+                $rewardLine = $cart->lines->first(function ($line) use ($purchasable) {
                     return $line->purchasable->id == $purchasable->id;
                 });
-                
-                if (! $line) {
-                    $line = $cart->lines()->make([
+
+                if (! $rewardLine) {
+                    $rewardLine = $cart->lines()->make([
                         'purchasable_type' => get_class($purchasable),
                         'purchasable_id' => $purchasable->id,
                         'quantity' => 1,
                     ]);
-                    
+
                     if (! $cart->freeItems) {
                         $cart->freeItems = collect();
                     }
-        
-                    if (! $cart->freeItems->contains($rewardLine->purchasable)) {
-                        $cart->freeItems->push($rewardLine->purchasable);
+
+                    if (! $cart->freeItems->contains($selectedRewardItem)) {
+                        $cart->freeItems->push($selectedRewardItem);
                     }
+
+                    $rewardLine = app(Pipeline::class)
+                    ->send($rewardLine)
+                    ->through(
+                        config('lunar.cart.pipelines.cart_lines', [])
+                    )->thenReturn(function ($cartLine) {
+                        $cartLine->cacheProperties();
+
+                        return $cartLine;
+                    });
+
+                    $unitQuantity = $purchasable->getUnitQuantity();
+
+                    $rewardLine->subTotal = new Price($rewardLine->unitPrice->value, $cart->currency, $unitQuantity);
+                    $rewardLine->taxAmount = new Price(0, $cart->currency, $unitQuantity);
+                    $rewardLine->total = new Price($rewardLine->unitPrice->value, $cart->currency, $unitQuantity);
                 }
-                
-                $meta = $line->meta ?? json_decode('{}');
+
+                $meta = $rewardLine->meta ?? json_decode('{}');
                 if (! isset($meta->added_by_discount)) {
                     $meta->added_by_discount = [];
                 }
-                
+
                 if (! isset($meta->added_by_discount[$this->discount->id])) {
-                    $meta->added_by_discount[$this->discount->id] = 1;    
+                    $meta->added_by_discount[$this->discount->id] = 1;
                 } else {
                     $meta->added_by_discount[$this->discount->id]++;
                 }
-                
-                $line->meta = $meta;
-                $line->save();
-                
+
+                $affectedLine = $affectedLines->first(function ($line) use ($rewardLine) {
+                    return $line->line == $rewardLine;
+                });
+
+                if (! $affectedLine) {
+                    $affectedLines->push(new DiscountBreakdownLine(
+                        line: $rewardLine,
+                        quantity: 1
+                    ));
+                } else {
+                    $affectedLine->quantity++;
+                }
+
+                $unitPrice = $rewardLine->unitPrice->value;
+
+                $discountTotal += $unitPrice;
+
+                $rewardLine->discountTotal = new Price(
+                    ($rewardLine->discountTotal?->value ?? 0) + $unitPrice,
+                    $cart->currency,
+                    1
+                );
+
+                $rewardLine->subTotalDiscounted = new Price(
+                    $rewardLine->subTotal->value - $rewardLine->discountTotal->value,
+                    $cart->currency,
+                    1
+                );
+
+                $rewardLine->meta = $meta;
+                $rewardLine->save();
+
                 $remainingRewardQty--;
             }
-            
+
         // we have lines to remove
-        } else if ($remainingRewardQty < 0) {
-            
+        } elseif ($remainingRewardQty < 0) {
             // while handles the situation where quantity of an item may be more than 1
             while ($remainingRewardQty > 0 && ! empty($automaticLines)) {
-                
                 // loop over automatic lines and decrement quantity
                 foreach ($automaticLines as $index => $line) {
                     if ($remainingRewardQty >= 0) {
                         continue;
                     }
-                    
+
                     $meta = $line->meta;
                     $addedByDiscountQty = $meta->added_by_discount[$this->discount->id] ?? 0;
-                      
-                    if ($addedByDiscountQty > 0) {  
+
+                    if ($addedByDiscountQty > 0) {
                         $line->quantity = $line->quantity - 1;
                         $addedByDiscountQty--;
                         $remainingRewardQty++;
-                        
+
                         if ($addedByDiscountQty < 1) {
-                            unset($meta->added_by_discount[$this->discount->id]);   
+                            unset($meta->added_by_discount[$this->discount->id]);
                         } else {
                             $meta->added_by_discount[$this->discount->id] = $addedByDiscountQty;
                         }
-                        
+
                         if (empty($meta->added_by_discount)) {
                             unset($meta->added_by_discount);
                         }
 
                         $line->meta = $meta;
                     }
-                    
+
                     if ($line->quantity > 0) {
                         $line->save();
                     } else {
@@ -276,18 +320,11 @@ class BuyXGetY extends AbstractDiscountType
 
                     if ($addedByDiscountQty <= 0) {
                         unset($automaticLines[$index]);
-                    }                        
+                    }
                 }
             }
+        }
 
-        }         
-
-        $this->addDiscountBreakdown($cart, new DiscountBreakdown(
-            discount: $this->discount,
-            lines: $affectedLines,
-            price: new Price($discountTotal, $cart->currency, 1)
-        ));
-
-        return $cart;
+        return [$affectedLines, $discountTotal];
     }
 }
