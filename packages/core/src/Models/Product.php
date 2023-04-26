c95c2b0d9110acecfc9fb78f35dda62ba9030562
Alec Ritson <hello@itsalec.co.uk>
2023-04-26 15:33:10 +0100

    Feat [0.3] - Add storefront scopes (#967)

--- packages/core/src/Models/Product.php
+++ packages/core/src/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace Lunar\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
