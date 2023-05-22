8ff65943bffbb159255a56c18c923e997c2ccb96
wychoong <67364036+wychoong@users.noreply.github.com>
2023-05-23 00:19:19 +0800

    Update Cart.php

--- packages/core/src/Models/Cart.php
+++ packages/core/src/Models/Cart.php
@@ -301,7 +301,7 @@ class Cart extends BaseModel
                 $this->add(
                     purchasable: $line['purchasable'],
                     quantity: $line['quantity'],
-                    meta: (array) $line['meta'] ?? null,
+                    meta: (array) ($line['meta'] ?? null),
                     refresh: false
                 );
             });
