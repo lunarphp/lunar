bf707a8da0382bc80b4a0e13407c9be3e960e6a2
Oscar Kolsrud <30559109+OscarKolsrud@users.noreply.github.com>
2023-05-25 23:33:32 +0200

    Add core config option
    Add core config option that will be used for disabling migrations

--- packages/core/config/database.php
+++ packages/core/config/database.php
@@ -15,4 +15,15 @@ return [
     |
     */
     'users_id_type' => 'bigint',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Disable migrations
+    |--------------------------------------------------------------------------
+    |
+    | Prevent Lunar`s default package migrations from running. 
+    | Set to 'true' to disable. Useful for working with 3rd party packages etc.
+    |
+    */
+    'disable_migrations' => 'false',
 ];
