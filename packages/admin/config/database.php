cb25e1fae20c7aba7fcfb5e28bf9e0dbc04081df
Oscar Kolsrud <30559109+OscarKolsrud@users.noreply.github.com>
2023-05-25 23:37:38 +0200

    Add admin config option
    Add admin config option that will be used for disabling migrations for Lunar`s default admin portal

---
+++ packages/admin/config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Disable migrations
+    |--------------------------------------------------------------------------
+    |
+    | Prevent Lunar`s default package migrations from running for the
+    | admin portal. Set to 'true' to disable.
+    | Useful for working with 3rd party packages etc.
+    |
+    */
+    'disable_migrations' => false,
+];
