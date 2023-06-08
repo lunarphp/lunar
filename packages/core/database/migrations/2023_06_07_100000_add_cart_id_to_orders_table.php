88cdbd52b654c1baf1a2f41f7be74e2f963bc7f2
Alec Ritson <hello@itsalec.co.uk>
2023-06-08 14:42:38 +0100

    Fix for SQLite

--- packages/core/database/migrations/2023_06_07_100000_add_cart_id_to_orders_table.php
+++ packages/core/database/migrations/2023_06_07_100000_add_cart_id_to_orders_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Lunar\Base\Migration;
+use Lunar\Facades\DB;
 
 class AddCartIdToOrdersTable extends Migration
 {
@@ -16,7 +17,9 @@ class AddCartIdToOrdersTable extends Migration
     public function down()
     {
         Schema::table($this->prefix.'orders', function (Blueprint $table) {
-            $table->dropForeign(['cart_id']);
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->dropForeign(['cart_id']);
+            }
             $table->dropColumn('cart_id');
         });
     }
