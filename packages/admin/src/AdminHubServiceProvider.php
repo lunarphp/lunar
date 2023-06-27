4ddc2ae3448ee1d883bb0bb5e39d12b66f163253
Alec Ritson <hello@itsalec.co.uk>
2023-06-27 07:59:28 +0100

    Revert "Remove type declaration"
    This reverts commit 084cdd50aabde0472a41163e4608becd1ad5c695.

--- packages/admin/src/AdminHubServiceProvider.php
+++ packages/admin/src/AdminHubServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Lunar\Hub;
 
 use Illuminate\Routing\Events\RouteMatched;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Event;
@@ -525,7 +526,7 @@ class AdminHubServiceProvider extends ServiceProvider
      */
     protected function registerPermissionManifest()
     {
-        Gate::after(function ($user, $ability) {
+        Gate::after(function (Staff $user, $ability) {
             // Are we trying to authorize something within the hub?
             $permission = $this->app->get(Manifest::class)->getPermissions()->first(fn ($permission) => $permission->handle === $ability);
             if ($permission) {
