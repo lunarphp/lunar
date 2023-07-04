656d7957ce7793cdb41cd635b7eefb5a08b30d85
Glenn Jacobs <glenn@neondigital.co.uk>
2023-07-04 09:37:02 +0100

    Update StockManagerInterface.php

--- packages/core/src/Base/StockManagerInterface.php
+++ packages/core/src/Base/StockManagerInterface.php
@@ -2,6 +2,7 @@
 
 namespace Lunar\Base;
 
+use Lunar\Drivers\NullStockDriver;
 use Lunar\Drivers\SimpleStockDriver;
 
 interface StockManagerInterface
@@ -12,6 +13,13 @@ interface StockManagerInterface
      * @return SimpleStockDriver
      */
     public function createSimpleDriver();
+
+    /**
+     * Create the NULL stock driver.
+     *
+     * @return NullStockDriver
+     */
+    public function createNullDriver();
 
     /**
      * Return the default driver reference.
