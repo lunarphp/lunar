7b11fb7256a181c9295b68a7f6e0db0ce0fd3480
Glenn Jacobs <glenn@neondigital.co.uk>
2023-07-06 10:51:36 +0100

    Misc improvements (#1135)
    Adds a few misc tweaks

--- packages/core/src/Actions/AbstractAction.php
+++ packages/core/src/Actions/AbstractAction.php
@@ -11,6 +11,22 @@ abstract class AbstractAction
      */
     protected mixed $passThrough = null;
 
+    /**
+     * E.g. MyAction::make();
+     */
+    public static function make()
+    {
+        return app(static::class);
+    }
+
+    /**
+     * E.g. MyAction::run($item1, $item2);
+     */
+    public static function run(...$arguments)
+    {
+        return static::make()->execute(...$arguments);
+    }
+
     public function then(Closure $callback)
     {
         return $callback($this->passThrough);
