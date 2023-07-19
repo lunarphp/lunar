df9f1ae010a3b596d5343447c9d4bae3b00d37e5
Gennaro Landolfi <gennarolandolfi@rinodrummer.dev>
2023-07-19 10:54:06 +0200

    Translated account.php

---
+++ packages/admin/resources/lang/it/account.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Account',
+    'save_btn' => 'Salva dettagli',
+    'avatar_notice' => 'Il tuo avatar è stato generato usando Gravatar mediante il tuo indirizzo email.',
+    'view-profile' => 'Visualizza profilo',
+    'update_password' => 'Aggiorna password',
+    'logout' => 'Esci',
+];
