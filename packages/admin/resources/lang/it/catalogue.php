7fbb5ebac301903a0940bf25f06cd1ea5b8c5e70
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-07-19 11:24:20 +0200

    Translated collections section in catalogue.php

---
+++ packages/admin/resources/lang/it/catalogue.php
@@ -0,0 +1,139 @@
+<?php
+
+return [
+    /**
+     * Collections.
+     */
+    'collections.sidemenu.create_btn' => 'Crea un gruppo di collezioni',
+    'collections.sidemenu.modal.title' => 'Crea un gruppo di collezioni',
+    'collections.sidemenu.modal.btn' => 'Crea un gruppo di collezioni',
+    'collections.sidemenu.menu_title' => 'Gruppi di collezioni',
+    'collections.groups.delete_group_btn' => 'Elimina gruppo di collezioni',
+    'collections.groups.add_collection_btn' => 'Aggiungi collezioni',
+    'collections.groups.delete.title' => 'Elimina gruppo di collezioni',
+    'collections.groups.delete.strapline' => 'Sei sicuro di dover rimuovere il gruppo di collezioni?',
+    'collections.groups.delete.warning' => 'Tutte le collezioni del gruppo verranno rimosse!.',
+    'collections.groups.delete.confirm' => 'Conferma di voler cancellare il gruppo e tutte le collezioni associate ad esso?',
+    'collections.groups.delete.btn' => 'Elimina gruppo di collezioni',
+    'collections.groups.move.title' => 'Sposta collezione',
+    'collections.groups.move.search_placeholder' => 'Cerca una collezione da usare come genitore',
+    'collections.groups.move.alert' => 'Questo sposterà da :source a :target.',
+    'collections.groups.move.btn' => 'Move Collection',
+    'collections.groups.node.make_root' => 'Crea radice',
+    'collections.groups.node.move' => 'Sposta collezione',
+    'collections.groups.node.edit' => 'Modifica Collezione',
+    'collections.groups.node.add_child' => 'Aggiungi collezione figlia',
+    'collections.groups.node.delete' => 'Elimina collezione',
+    'collections.groups.no_groups' => 'Sembra che tu non abbia creato nessun gruppo di collezioni.',
+    'collections.create.child.title' => 'Aggiungi collezione a :parent.',
+    'collections.create.root.title' => 'Aggiungi collezione primaria (top-level)',
+    'collections.create.btn' => 'Crea collezione',
+    'collections.delete.title' => 'Elimina collezione',
+    'collections.delete.btn' => 'Elimina collezione',
+    'collections.delete.warning' => 'Tutte le collezioni discendenti da questo gruppo verranno rimosse',
+    'collections.delete.child.warning' => 'Questa collezione ha :count collezioni discendenti, anche esse verranno eliminate.',
+    'collections.delete.root.warning' => 'Sei sicuro di dover rimuovere questa collezione?',
+    'collections.index.title' => 'Collezioni',
+    'collections.show.save_btn' => 'Salva collezioni',
+    'collections.show.sort.base_price_asc' => 'Prezzo iniziale Crescente.',
+    'collections.show.sort.base_price_desc' => 'Prezzo iniziale Decrescente.',
+    'collections.show.sort.sku_asc' => 'SKU Crescente.',
+    'collections.show.sort.sku_desc' => 'SKU Decrescente.',
+    'collections.show.sort.custom' => 'Personalizzato',
+    'collections.show.no_products' => 'Questa collezione non contiene nessun prodotto',
+    'collections.show.no_children' => 'Questa collezione non contiene nessuna collezione discendente',
+    'collections.associate_to_other_products' => 'Scegli i prodotti da associate e correlare a questo prodotto',
+    'collections.static_collections' => 'Collezioni statiche',
+    'collections.summer_seasonal' => 'Edizione Estiva',
+    /**
+     * Product show.
+     */
+    'products.show.back_link_title' => 'Go back to product listing',
+    'products.show.save_btn' => 'Save Product',
+    'products.show.no_channel_availability' => 'This product has no availability across channels.',
+    'products.show.delete_title' => 'Delete product',
+    'products.show.delete_strapline' => 'This will send the product into the recycle bin, you will be able to restore it later',
+    'products.show.restore_strapline' => 'Restore this product so it becomes available on your store.',
+    'products.show.delete_btn' => 'Confirm delete',
+    'products.show.restore_title' => 'Restore product',
+    'products.show.restore_btn' => 'Confirm restore',
+
+    /**
+     * Product variant.
+     */
+    'product-variants.delete_confirm.title' => 'Confirm variant deletion',
+    'product-variants.delete_confirm.strapline' => 'This action cannot be undone.',
+    'product-variants.delete_confirm.btn' => 'Delete product variant',
+    'product-variants.add_variant.btn' => 'Add variant',
+    'product-variants.add_variant.already_exists' => 'This variant already exists.',
+    'product-variants.add_variant.null_option' => 'Choose an option',
+    'product-variants.add_variant.title' => 'Add additional variant',
+    'product-variants.add_variant.add_new_option' => 'Add new option',
+    /**
+     * Product type listing.
+     */
+    'product-types.index.title' => 'Product Types',
+    'product-types.index.create_btn' => 'Create Product Type',
+    /**
+     * Product type editing.
+     */
+    'product-types.show.updated_message' => 'Product Type updated',
+    'product-types.create.btn_text' => 'Create product type',
+    'product-types.show.btn_text' => 'Update product type',
+    'product-types.attribute.search.empty' => 'Looks like you have no more attributes to assign.',
+    'product-types.attribute.search.no_results' => 'Unable to find attributes matching ":search"',
+    'product-types.show.title' => 'Edit product type',
+    'product-types.show.delete.btn_text' => 'Delete Product Type',
+    'product-types.show.delete.message' => 'Are you sure you want to permanently delete this product type?',
+    'product-types.show.delete.disabled_message' => 'You cannot remove a product type that has products attached.',
+    'product-types.show.delete.delete_notification' => 'Product type removed',
+    'product-types.show.delete.confirm_text' => 'Confirm Deletion',
+    'product-types.show.delete.minimum_required' => 'You cannot delete the only product type.',
+    'product-types.attribute.no_attributes' => 'There are no attributes to select in this group',
+    /**
+     * Brands.
+     */
+    'brands.index.title' => 'Brands',
+    'brands.create.title' => 'Create Brand',
+    'brands.show.back_link_title' => 'Go back to brands',
+    /**
+     * Customers.
+     */
+    'customers.index.title' => 'Customers',
+    'customers.index.placeholder' => 'Search customers',
+    'customers.show.save_btn' => 'Save Customer',
+    'customers.show.customer_groups_updated' => 'Customer groups updated',
+    'customer.show.customer_groups' => 'Customer Groups',
+    'customer.dashboard.returning_customers' => 'Returning Customers',
+    'customer.dashboard.no_of_orders' => 'No. of Orders',
+    'customer.dashboard.turnover' => 'Turnover',
+    'customer.dashboard.sales_performance' => 'Sales Performance',
+    'customer.dashboard.customer_group_orders' => 'Customer Group Orders',
+    'customer.dashboard.top_selling_products' => 'Top Selling Products',
+    /**
+     * Customer Groups Editing.
+     */
+    'editing.customer-groups.select_groups' => 'Select which customer groups this product is available for.',
+    'editing.customer-groups.publish_on' => 'This product is scheduled to be published on',
+    'editing.customer-groups.schedule_availability' => 'Schedule availability',
+    /**
+     * Orders.
+     */
+    'orders.show.customer_reference' => 'Customer Reference',
+    'orders.show.shipping_option' => 'Shipping Option',
+    'orders.show.shipping_address' => 'Shipping Address',
+    'orders.show.billing_address' => 'Billing Address',
+    'orders.show.order_lines' => 'Order Lines',
+    'orders.show.transactions' => 'Transactions',
+    'orders.dashboard.recent_orders' => 'Recent Orders',
+    /**
+     * Stock.
+     */
+    'stock' => 'Customer Reference',
+    /**
+     * Addons.
+     */
+    'addons.show.addon_details' => 'Addon Details',
+    'addons.show.developer' => 'Developer',
+    'addons.show.marketplace' => 'Marketplace',
+];
