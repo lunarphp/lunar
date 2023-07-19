6122be475a4303c732c5c306b0c89422ebceb24a
Gennaro Landolfi <gennarolandolfi@rinodrummer.dev>
2023-07-19 11:14:31 +0200

    Translated auth.php

---
+++ packages/admin/resources/lang/it/auth.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    /**
+     * Collections.
+     */
+    'permissions.catalogue.collections.name' => 'Gestisci Collezioni',
+    'permissions.catalogue.collections.description' => 'Permetti all membro dello staff di modificare le collezioni e i rispettivi gruppi',
+    'permissions.settings.name' => 'Impostazioni',
+    'permissions.settings.description' => "Dai accesso alle impostazione dell'hub.",
+    'permissions.settings.core.name' => 'Impostazioni del Core',
+    'permissions.settings.core.description' => 'Accesso alle impostazioni fondamentali del negozio, come i canali, le lingue, le valute, ecc.',
+    'permissions.settings.staff.name' => 'Gestisci Staff',
+    'permissions.settings.staff.description' => 'Permetti al membro dello staff di modificare altri membri dello staff.',
+    'permissions.settings.attributes.name' => 'Gestisci Attributi',
+    'permissions.settings.attributes.description' => 'Permetti al membro dello staff di modificare e creare attributi addizionali.',
+    'permissions.catalogue.products.name' => 'Gestisci Prodotti',
+    'permissions.catalogue.products.description' => 'Permetti al membro dello staff di modificare i prodotti.',
+    'permissions.catalogue.orders.name' => 'Gestisci Ordini',
+    'permissions.catalogue.orders.description' => 'Permetti al membro dello staff di gestire gli ordini.',
+    'permissions.catalogue.customers.name' => 'Gestisci Clienti',
+    'permissions.catalogue.customers.description' => 'Permetti al membro dello staff di gestire i clienti.',
+    'permissions.discounts.name' => 'Gestisci Sconti',
+    'permissions.discounts.description' => 'Permetti al membro dello staff di gestire gli sconti',
+    /**
+     * Reset password.
+     */
+    'reset-password.title' => 'Reimposta Password',
+    'reset-your-password' => 'Reimposta la tua password',
+    'reset-password.invalid' => 'Questa pagina non è più valida.',
+    'reset-password.back_link' => 'Torna al login',
+    'reset-password.send_btn' => 'Invia una email di reset',
+    'reset-password.update_btn' => 'Update password and login',
+    'forgot-password.message' => 'Password dimenticata?',
+    'forgot-password.remember' => 'Ricorda la tua password?',
+    'forgot-password.link' => 'Reimposta password',
+    'follow-link.reset' => 'Per favore, segui questo link per reimpostare la tua password:',
+    'click-here.reset' => 'Clicca qui per reimpostare',
+    'reset-link.not_working' => 'Il link non funziona? Copia questo nel tuo browser',
+    /**
+     * Login.
+     */
+    'sign-in.btn' => 'Accedi',
+    'welcome' => 'Benvenuto a :name',
+    'prompt' => 'Accedi per amministrare il tuo negozio e-commerce.',
+];
