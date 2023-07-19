ca39e9f82dd23a638bbdf3ea1ff7c027a1e5a214
Gennaro Landolfi <gennarolandolfi@rinodrummer.dev>
2023-07-19 22:37:42 +0200

    Translated components.php

---
+++ packages/admin/resources/lang/it/components.php
@@ -0,0 +1,289 @@
+<?php
+
+return [
+    'dashboard.top_timeframe' => 'Ultimi :period giorni',
+    'products.option-manager.add_btn' => 'Aggiungi nuova opzione',
+    'products.option-manager.toggle_btn' => 'Alterna Tutte',
+    'products.product-selector.select_btn' => 'Seleziona Opzioni',
+    'products.product-selector.title' => 'Seleziona Opzioni',
+    'products.product-selector.available_tab' => 'Opzioni disponibili',
+    'products.product-selector.selected_tab' => 'Opzioni selezionati',
+    'products.product-selector.selected_empty' => 'Nessuna opzione corrisponde al termine di ricerca indicato.',
+    'products.product-selector.no_results' => 'Nessuna opzione corrisponde al termine di ricerca indicato.',
+    'products.product-selector.no_options' => 'Non ci sono opzioni disponibili, crea una nuova opzione per vederla qui.',
+    'products.product-selector.add_new_btn' => 'Aggiungi nuova opzione',
+    'products.product-selector.use_selected_btn' => 'Usa opzioni selezionate',
+    'products.option-creator.option_placeholder' => 'Es.: Colore',
+    'products.option-creator.value_placeholder' => 'Es.: Blu',
+    'products.option-creator.min_values_notice' => 'Devi avere almeno :min valori',
+    'products.option-creator.values_title' => 'Valori per le opzioni del prodotto',
+    'products.option-creator.title' => 'Crea nuova opzione',
+    'products.option-creator.add_value_btn' => 'Aggiungi valore',
+    'products.option-creator.create_option_btn' => 'Crea opzione',
+    'products.option-creator.values_strapline' => 'Aggiungi tutti i possibili valori disponibili per questa opzione del prodotto.',
+    'product-search.btn' => 'Aggiungi Prodotti',
+    'product-search.first_tab' => 'Cerca prodotti',
+    'product-search.second_tab' => 'Prodotti selezionati',
+    'product-search.max_results_exceeded' => 'Mostrando :max di :total prodotti. Prova ad essere più specifico nella tua ricerca.',
+    'product-search.exists_in_collection' => 'Già associato',
+    'product-search.no_results' => 'Nessun risultato trovato.',
+    'product-search.pre_search_message' => 'Cerca prodotti per attributo o SKU.',
+    'product-search.select_empty' => 'Quando selezionerai dei prodotti, essi compariranno qui.',
+    'product-search.title' => 'Cerca prodotti',
+    'product-search.associate_self' => 'Non puoi associare lo stesso prodotto',
+    'product-search.commit_btn' => 'Seleziona Prodotti',
+    /**
+     * Option Value Create Modal.
+     */
+    'ovcm.title' => 'Aggiungi una nuova opzione a :name',
+    /**
+     * Attribute group create.
+     */
+    'attribute-group-edit.name.placeholder' => 'es.: Dettagli Aggiuntivi',
+    'attribute-group-edit.create_btn' => 'Crea gruppo di attributi',
+    'attribute-group-edit.update_btn' => 'Aggiorna gruppo di attributi',
+    'attribute-group-edit.non_unique_handle' => 'Questo nome è già stato preso',
+    /**
+     * Attribute show.
+     */
+    'attributes.show.create_group_btn' => 'Crea gruppo di attributi',
+    'attributes.show.create_attribute' => 'Crea attributo',
+    'attributes.show.edit_group_btn' => 'Modifica gruppo di attributi',
+    'attributes.show.edit_attribute_btn' => 'Modifica attributo',
+    'attributes.show.delete_group_btn' => 'Elimina gruppo di attributi',
+    'attributes.show.edit_title' => 'Modifica gruppo di attributi',
+    'attributes.show.create_title' => 'Crea gruppo di attributi',
+    'attributes.show.delete_title' => 'Elimina gruppo di attributi',
+    'attributes.show.delete_warning' => "Rimuovere questo gruppo di clienti eliminerà anche tutti gli attributi ad esso associati. Quest'azione è irreversibile.",
+    'attributes.show.group_protected' => 'Questo gruppo contiene attributi richiesti dal sistema, quindi non può essere rimosso.',
+    'attributes.show.no_attributes_text' => 'Non esiste nessun attributo, trascina un attributo esistente o aggiungine uno nuovo da qui.',
+    'attributes.show.delete_attribute_btn' => 'Elimina attributo',
+    'attributes.show.delete_attribute_title' => 'Elimina Attributo',
+    'attributes.show.delete_attribute_warning' => 'Sei sicuro di voler rimuovere questo attributo?',
+    'attributes.show.delete_attribute_protected' => 'Non puoi eliminare un attributo di sistema.',
+    'attributes.show.no_groups' => 'Nessun gruppo di attributi trovato, aggiungine uno prima di potergli aggiungere attributi.',
+    /**
+     * Attribute edit.
+     */
+    'attribute-edit.create_title' => 'Crea attributo',
+    'attribute-edit.update_title' => 'Aggiorna attributo',
+    'attribute-edit.system_locked' => 'Questo attributo è richiesto dal sistema, quindi alcuni campi sono disabilitati.',
+    'attribute-edit.name.placeholder' => 'es.: Nome',
+    'attribute-edit.required.instructions' => 'Questo attributo è richiesto in modifica/creazione?',
+    'attribute-edit.searchable.instructions' => 'Gli utenti possono cercare usando questo attributo?',
+    'attribute-edit.filterable.instructions' => 'Gli utenti possono filtrare usando questo attributo?',
+    'attribute-edit.validation.instructions' => 'Specifica qualsiasi regola di validazione di Laravel per questo campo.',
+    'attribute-edit.cancel_btn' => 'Annulla',
+    'attribute-edit.save_attribute_btn' => 'Salva Attributo',
+
+    /**
+     * Brand search.
+     */
+    'brand-search.btn' => 'Aggiungi Marchi',
+    'brand-search.first_tab' => 'Cerca marchi',
+    'brand-search.second_tab' => 'Marchi selezionati',
+    'brand-search.max_results_exceeded' => 'Mostrando i primi :max di :total marchi. Prova ad essere più specifico nella tua ricerca.',
+    'brand-search.exists_in_collection' => 'Già associato',
+    'brand-search.no_results' => 'Nessun risultato trovato.',
+    'brand-search.pre_search_message' => 'Cerca i marchi per attributo.',
+    'brand-search.select_empty' => 'Quando selezionerai dei marchi, essi compariranno qui.',
+    'brand-search.title' => 'Cerca marchi',
+    'brand-search.commit_btn' => 'Seleziona Marchi',
+
+    /**
+     * Collection search.
+     */
+    'collection-search.btn' => 'Aggiungi Collezioni',
+    'collection-search.first_tab' => 'Cerca collezioni',
+    'collection-search.second_tab' => 'Collezioni selezionate',
+    'collection-search.max_results_exceeded' => 'Mostrando le prima :max di :total collezioni. Prova ad essere più specifico nella tua ricerca.',
+    'collection-search.exists_in_collection' => 'Già associata',
+    'collection-search.no_results' => 'Nessun risultato trovato.',
+    'collection-search.pre_search_message' => 'Cerca le collezioni per attributo.',
+    'collection-search.select_empty' => 'Quando selezionerai delle collezioni, esse compariranno qui.',
+    'collection-search.title' => 'Cerca collezioni',
+    'collection-search.commit_btn' => 'Seleziona Collezioni',
+    /**
+     * Customers.
+     */
+    'customers.show.metrics.total_orders' => 'Totale Ordini',
+    'customers.show.metrics.avg_spend' => 'Spesa Media',
+    'customers.show.metrics.total_spend' => 'Totale Speso',
+    'customers.show.year_spending' => 'Speso lo scorso anno',
+    'customers.show.purchase_history' => 'Cronologia Acquisti',
+    'customers.show.order_history' => 'Cronologia Ordini',
+    'customers.show.users' => 'Utenti',
+    'customers.show.addresses' => 'Indirizzi dei clienti',
+    'customers.show.customer_groups' => 'Gruppi dei clienti',
+    'customers.show.save_customer' => 'Salva Clienti',
+    'customers.show.no_purchase_history' => 'Questo cliente non ha nessun acquisto in cronologia.',
+    'customers.show.no_order_history' => 'Questo cliente non ha nessun ordine in cronologia.',
+    'customers.show.no_users' => 'Questo cliente non ha nessun utente associato.',
+    'customers.show.no_addresses' => 'Questo cliente non ha indirizzi.',
+    'customers.show.remove_address_btn' => 'Rimuovi',
+    'customers.show.remove_address.title' => 'Rimuovi indirizzo',
+    'customers.show.remove_address.confirm' => 'Sei sicuro di voler rimuovere questo indirizzo?',
+    /**
+     * Orders Index
+     */
+    'orders.index.returning_customer' => 'Cliente Ricorrente',
+    'orders.index.new_customer' => 'Cliente nuovo',
+    /**
+     * Order Show.
+     */
+    'orders.show.title' => 'Ordine',
+    'orders.show.save_shipping_btn' => 'Salva Indirizzo',
+    'orders.show.save_billing_btn' => 'Salva Indirizzo',
+    'orders.show.print_btn' => 'Stampa',
+    'orders.show.refund_btn' => 'Rimborso',
+    'orders.show.refund_lines_btn' => 'Dettagli Rimborso',
+    'orders.show.update_status_btn' => 'Aggiorna Stato',
+    'orders.show.more_actions_btn' => 'Più Azioni',
+    'orders.show.show_all_lines_btn' => 'Mostra tutte le righe',
+    'orders.show.additional_lines_text' => 'Ulteriori :count linee sono nascoste',
+    'orders.show.collapse_lines_btn' => 'Collassa linee',
+    'orders.show.transactions_header' => 'Transazioni',
+    'orders.show.timeline_header' => 'Linea temporale',
+    'orders.show.additional_fields_header' => 'Informazioni aggiuntive',
+    'orders.show.billing_matches_shipping' => "Coincide con l'indirizzo di spedizione",
+    'orders.show.billing_header' => 'Indirizzo di fatturazione',
+    'orders.show.shipping_header' => 'Indirizzo di spedizione',
+    'orders.show.requires_capture' => "Quest'ordine è ancora in attesa di pagamento per essere catturato.",
+    'orders.show.capture_payment_btn' => 'Cattura Pagamento',
+    'orders.show.partially_refunded' => "Quest'ordine è stato rimborsato parzialmente.",
+    'orders.show.refunded' => "Quest'ordine è stato rimborsato.",
+    'orders.show.view_customer' => 'Visualizza cliente',
+    'orders.show.tags_header' => 'Tag',
+    'orders.show.download_pdf' => 'Scarica PDF',
+
+    /**
+     * Order Refund.
+     */
+    'orders.refund.confirm_text' => 'CONFERMA',
+    'orders.refund.confirm_message' => 'Per favore, conferma di voler rimborsare questo ammonto.',
+    'orders.refund.no_charges' => "Non ci sono addebiti rimborsabili per quest'ordine",
+    'orders.refund.select_transaction' => 'Seleziona una transazione',
+    'orders.refund.refund_btn' => 'Invia rimborso',
+    'orders.refund.fully_refunded' => 'Le catture per questo ordine sono state rifiutate',
+    /**
+     * Brand Index.
+     */
+    'brands.index.title' => 'Marchi',
+    'brands.index.create_brand' => 'Crea Marchio',
+    'brands.index.table_row_action_text' => 'Modifica Marchio',
+    'brands.index.table_count_header_text' => 'Conteggio Prodotti',
+    'brands.choose_brand_default_option' => 'Scegli marchio',
+    /**
+     * Product Index.
+     */
+    'products.index.title' => 'Prodotti',
+    'products.index.create_product' => 'Crea Prodotto',
+    'products.index.selected_products' => 'Hai selezionato :count prodotti, vuoi selezionarli tutti',
+    'products.index.you_have_selected_all' => 'Hai selezionato tutti i :count prodotti.',
+    'products.index.select_all_btn' => 'Seleziona tutti',
+    'products.index.deselect_all_btn' => 'Deseleziona tutti',
+    'products.index.draft' => 'Bozza',
+    'products.index.published' => 'Pubblicato',
+    'products.index.deleted' => 'Eliminato',
+    'products.index.only_deleted_visible' => 'Attualmente sono mostrati i soli prodotti eliminati',
+    'products.index.products_empty' => 'Nessun prodotto risponde alla ricerca/ai filtri.',
+    /**
+     * Order Capture.
+     */
+    'orders.capture.confirm_text' => 'CONFERMA',
+    'orders.capture.confirm_message' => 'Per favore, conferma di voler catturare questo pagamento',
+    'orders.capture.no_intents' => 'Non ci sono transazioni disponibili alla cattura',
+    'orders.capture.select_transaction' => 'Seleziona una transazione',
+    'orders.capture.capture_btn' => 'Cattura pagamento',
+    /**
+     * Order Status.
+     */
+    'orders.status.update_btn' => 'Aggiorna Stato',
+    'orders.status.select_new' => 'Seleziona un nuovo stato',
+    'orders.status.preview.title' => 'Anteprima del template',
+    'orders.status.preview.alert' => "Questa è un'anteprima di come apparirà la tua email.",
+    'orders.status.no_status_selected_alert' => "Seleziona uno stato d'ordine per vedere i mittenti disponibili.",
+    'orders.status.additional-content.label' => 'Contenuto aggiuntivo',
+    'orders.status.additional-content.instructions' => 'Se supportato, Aggiungi un messaggio aggiuntivo alla notifica o al mittente.',
+    'orders.status.mailers.label' => 'Mittenti',
+    'orders.status.mailers.instructions' => 'Seleziona con quale mittente vorresti inviare.',
+    'orders.status.mailers.empty' => 'Non ci sono mittenti disponibili per questo stato.',
+    'orders.status.notifications.label' => 'Notifiche',
+    'orders.status.notifications.instructions' => 'Seleziona quali notifiche vorresti inviare.',
+    'orders.status.notifications.empty' => 'Non ci sono notifiche disponibili per questo stato.',
+    'orders.status.email_addresses.label' => 'Indirizzi Email',
+    'orders.status.email_addresses.instructions' => 'Seleziona quali indirizzi email to vuoi usare',
+    'orders.status.additional_email.label' => 'Indirizzo email aggiuntivo',
+    'orders.status.additional_email.instructions' => 'Se dovessi aver bisogno di un indirizzo email personalizzato, inseriscilo qui.',
+    /**
+     * Activity Log.
+     */
+    'activity-log.system' => 'Sistema',
+    'activity-log.orders.status_change' => 'Stato aggiornato',
+    'activity-log.orders.order_created' => 'Ordine Creato',
+    'activity-log.orders.capture' => 'Pagamento di :amount sulla carta che finisce con :last_four',
+    'activity-log.orders.authorized' => 'Autorizzati :amount sulla carta che finisce con :last_four',
+    'activity-log.orders.refund' => 'rimborso di :amount sulla carta che finisce con :last_four',
+    /**
+     * Option value edit.
+     */
+    'option.value.edit.create_title' => "Crea valore per l'opzione",
+    'option.value.edit.update_title' => "Aggiorna valore per l'opzione",
+    'option.value.edit.delete_locked' => "Questo valore per l'opzione non può essere rimosso in quanto obbligatorio per :count varianti di prodotti",
+    'option.value.edit.system_locked' => "Questo valore per l'opzione è richiesto dal sistema so some fields are disabled.",
+    'option.value.edit.name.placeholder' => 'es.: Nome',
+    'option.value.edit.required.instructions' => "Questo valore per l'opzione è richiesto in modifica/creazione?",
+    'option.value.edit.searchable.instructions' => 'Gli utenti dovrebbero essere capaci di cercare per questo option.value.',
+    'option.value.edit.filterable.instructions' => 'Gli utenti dovrebbero essere capaci di filtrare i risultati per questo option.value.',
+    'option.value.edit.validation.instructions' => 'Specifica qualsiasi regola di validazione di Laravel per questo campo.',
+    'option.value.edit.cancel_btn' => 'Annulla',
+    'option.value.edit.save_feature.value.btn' => "Salva valore per l'opzione",
+    /**
+     * Option value show.
+     */
+    'option.create_group_btn' => 'Crea opzione',
+    'option.create_option_value' => "Crea valore per l'opzione",
+    'option.update_option_value' => "Aggiorna valore per l'opzione",
+    'option.value_title' => "Valore per l'opzione del Prodotto",
+    'option.save_positions' => 'Salva posizioni',
+    'option.edit_group_btn' => 'Modifica opzione',
+    'option.edit_option.value.btn' => "Modifica valore per l'opzione",
+    'option.delete_group_btn' => 'Elimina opzione',
+    'option.edit_title' => 'Modifica opzione',
+    'option.create_title' => 'Crea opzione',
+    'option.delete_title' => 'Modifica opzione',
+    'option.delete_warning' => "Non puoi eliminare un'opzione che ha dei valori associati.",
+    'option.group_protected' => 'Questo gruppo contiene dei valore per le opzioni richieste dal sistema, quindi non può essere rimosso.',
+    'option.no_option_values_text' => "Non esiste alcun valore per l'opzione.",
+    'option.delete_option.value.btn' => "Elimina valore per l'opzione",
+    'option.delete_option.value.title' => 'Elimina valore della Funzionalità',
+    'option.delete_option.value.warning' => "Sei sicuro di voler eliminare questo valore per l'opzione?",
+    'option.delete_option.value.protected' => 'Non puoi eliminare un option.value di sistema.',
+    'option.no_groups' => 'Nessuna opzione trovata, aggiungine una prima di potergli aggiungere valori.',
+    /**
+     * Option value edit.
+     */
+    'option-edit.create_btn' => 'Crea opzione',
+    'option-edit.update_btn' => 'Aggiorna opzione',
+    'option.value.edit.save_option.value.btn' => "Salva valore per l'opzione",
+    /**
+     * Discounts.
+     */
+    'discounts.index.title' => 'Sconti',
+    'discounts.index.status.pending' => 'In Attesa',
+    'discounts.index.status.active' => 'Attivo',
+    'discounts.index.status.scheduled' => 'Programmato',
+    'discounts.index.status.expired' => 'Scaduto',
+    'discounts.index.create_discount' => 'Crea Sconto',
+    'discounts.create.title' => 'Crea Sconto',
+    'discounts.create_btn' => 'Crea Sconto',
+    'discounts.save_btn' => 'Salva Sconto',
+    'discounts.show.stop.label' => 'Proibisci ad altri sconti di essere applicati dopo questo',
+    'discounts.show.danger_zone.label' => 'Elimina sconto',
+    'discounts.show.danger_zone.instructions' => 'Inserisci il nome dello sconto per confermarne la rimozione.',
+    /**
+     * Model URLs component
+     */
+    'model-url.preview' => 'Anteprima',
+    'model-url.view' => 'Visualizza',
+];
