88e8687b09ec42cd99e768382350b9ed70c05730
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-07-21 15:27:33 +0200

    Translated settings.php

--- packages/admin/resources/lang/it/settings.php
+++ packages/admin/resources/lang/it/settings.php
@@ -24,38 +24,38 @@ return [
      */
     'staff.index.title' => 'Staff',
     'staff.index.search_placeholder' => 'Cerca staff',
-    'staff.index.active_filter' => 'Show Inactive',
+    'staff.index.active_filter' => 'Mostra Inattivo',
     'staff.index.create_btn' => 'Aggiungi Staff',
     'staff.index.table_row_action_text' => 'Modifica staff',
     /**
      * Staff show page.
      */
-    'staff.show.title' => 'Edit Staff',
-    'staff.show.delete_btn' => 'Deactivate account',
-    'staff.show.delete_title' => 'Delete Staff',
-    'staff.show.restore_title' => 'Restore Staff',
+    'staff.show.title' => 'Modifica Staff',
+    'staff.show.delete_btn' => 'Disabilita Account',
+    'staff.show.delete_title' => 'Cancella Staff',
+    'staff.show.restore_title' => 'Ripristina Staff',
     /**
      * Staff create page.
      */
-    'staff.create.title' => 'Create Staff',
+    'staff.create.title' => 'Crea Staff',
     /**
      * Staff form.
      */
-    'staff.form.create_btn' => 'Create staff member',
-    'staff.form.update_btn' => 'Update staff member',
-    'staff.form.permissions_heading' => 'Permissions',
-    'staff.form.permissions_description' => 'Manage a staff members individual permissions.',
-    'staff.form.admin_message' => 'An admin user has access to all permissions.',
-    'staff.form.danger_zone.label' => 'Remove staff member',
-    'staff.form.danger_zone.delete_strapline' => 'Removing a staff member will stop all access to the hub, you can restore it later.',
-    'staff.form.danger_zone.restore_strapline' => 'Restore this staff members account so they can access the hub.',
-    'staff.form.danger_zone.own_account' => 'Removing your own account will instantly log you out.',
+    'staff.form.create_btn' => 'Crea membro staff',
+    'staff.form.update_btn' => 'Aggiorna membro staff',
+    'staff.form.permissions_heading' => 'Permessi',
+    'staff.form.permissions_description' => 'Gestisci i permessi individuali dei membri dello staff .',
+    'staff.form.admin_message' => 'Un utente admin può accedere a tutti i permessi.',
+    'staff.form.danger_zone.label' => 'Rimuovi membro dallo staff',
+    'staff.form.danger_zone.delete_strapline' => 'Rimuovere un membro dallo staff terminerà tutti gli accessi alla hub, ma potrai sempre ripristinarlo successivamente.',
+    'staff.form.danger_zone.restore_strapline' => 'Ripristina questo membro dello staff, così potrà accedere alla hub.',
+    'staff.form.danger_zone.own_account' => 'La rimozione del tuo account, terminerà la sessione.',
 
     /**
      * Addons listing page.
      */
     'addons.index.title' => 'Addons',
-    'addons.index.table_row_action_text' => 'View',
+    'addons.index.table_row_action_text' => 'Mostra',
     /**
      * Addons show page.
      */
@@ -63,119 +63,119 @@ return [
     /*
      * Languages listing page.
      */
-    'languages.index.title' => 'Languages',
-    'languages.index.create_btn' => 'Create Language',
-    'languages.index.table_row_action_text' => 'Edit language',
+    'languages.index.title' => 'Lingue',
+    'languages.index.create_btn' => 'Aggiungi Lingua',
+    'languages.index.table_row_action_text' => 'Cambia Lingua',
     /**
      * Languages create page.
      */
-    'languages.create.title' => 'Create Language',
+    'languages.create.title' => 'Aggiungi Lingua',
     /**
      * Languages show page.
      */
-    'languages.show.title' => 'Edit Language',
+    'languages.show.title' => 'Cambia Lingua',
     /**
      * Language form.
      */
-    'languages.form.create_btn' => 'Create Language',
-    'languages.form.update_btn' => 'Update Language',
-    'languages.form.default_instructions' => 'Set whether this language is the default, this will override the current default.',
+    'languages.form.create_btn' => 'Aggiungi Lingua',
+    'languages.form.update_btn' => 'Modifica Lingua',
+    'languages.form.default_instructions' => 'Scegli se selezionare questa lingua come default, questa operazione soprascriverà quella corrente.',
     /**
      * Currencies table.
      */
-    'currencies.index.title' => 'Currencies',
-    'currencies.index.table_row_action_text' => 'Edit',
-    'currencies.index.no_results' => 'You currently have no currencies in the system.',
+    'currencies.index.title' => 'Valute',
+    'currencies.index.table_row_action_text' => 'Modifica',
+    'currencies.index.no_results' => 'Attualmente non hai nessuna valuta nel sistema.',
     /**
      * Currency show page.
      */
-    'currencies.show.title' => 'Edit Currency',
+    'currencies.show.title' => 'Modifica valuta',
     /**
      * Currency create page.
      */
-    'currencies.create.title' => 'Create Currency',
-    'currencies.index.create_currency_btn' => 'Create Currency',
+    'currencies.create.title' => 'Aggiungi valuta',
+    'currencies.index.create_currency_btn' => 'Aggiungi valuta',
     /**
      * Currency form.
      */
-    'currencies.form.update_btn' => 'Update Currency',
-    'currencies.form.create_btn' => 'Create Currency',
-    'currencies.form.notify.created' => 'Currency created',
+    'currencies.form.update_btn' => 'Modifica valuta',
+    'currencies.form.create_btn' => 'Aggiungi valuta',
+    'currencies.form.notify.created' => 'Valuta aggiunta',
     'currencies.form.format_help_text' => [
-        'This allows you to specify the format that price fields should use for this currency.',
-        'When displaying, Lunar will swap out <code>{value}</code> for the formatted price. E.g. <code>£{value}</code>.',
-        'You must always include <code>{value}</code> for this to work properly.',
+        "Quest'azione ti permetterà di specificare il formato del campo della valuta da dover utilizzare",
+        'Quando mostrato, Lunar sostituirà <code>{value}</code> col prezzo formattato. Es.: <code>€{value}</code>.',
+        '<code>{value}</code> deve essere sempre incluso per poter funzionare correttamente.',
     ],
     /**
      * Attributes.
      */
-    'attributes.index.title' => 'Attributes',
-    'attributes.show.title' => 'Editing :type attributes',
-    'attributes.show.locked' => 'This attribute is required by the system and therefore has been locked for editing.',
-    'attributes.create.title' => 'Create Attribute',
-    'attributes.form.update_btn' => 'Update Attribute',
-    'attributes.form.create_btn' => 'Create Attribute',
-    'attributes.form.notify.created' => 'Attribute created',
+    'attributes.index.title' => 'Attributi',
+    'attributes.show.title' => 'Modifica gli attributi di :type',
+    'attributes.show.locked' => 'Questo attributo è richiesto dal sistema, pertanto le modifica sono bloccate.',
+    'attributes.create.title' => 'Crea Attributo ',
+    'attributes.form.update_btn' => 'Modifica Attributo',
+    'attributes.form.create_btn' => 'Crea Attributo',
+    'attributes.form.notify.created' => 'Attributo creato',
     /**
      * Tags.
      */
-    'tags.show.title' => 'Edit Tag',
+    'tags.show.title' => 'Modifica Tag',
     'tags.index.title' => 'Tags',
-    'tags.index.table_row_action_text' => 'Edit',
-    'tags.form.update_btn' => 'Update Tag',
-    'tags.form.create_btn' => 'Create Tag',
-    'tags.form.notify.updated' => 'Tag updated',
+    'tags.index.table_row_action_text' => 'Modifica',
+    'tags.form.update_btn' => 'Modifica Tag',
+    'tags.form.create_btn' => 'Crea Tag',
+    'tags.form.notify.updated' => 'Tag modificato',
     /**
      * Activity log page.
      */
-    'activity_log.index.title' => 'Activity Log',
+    'activity_log.index.title' => 'Log delle attività',
     /*
      * Product Options
      */
-    'product.options.index.title' => 'Options',
-    'product.options.index.create_btn' => 'Create Option',
-    'product.options.index.table_row_action_text' => 'Edit Option',
+    'product.options.index.title' => 'Opzioni',
+    'product.options.index.create_btn' => 'Crea Opzioni',
+    'product.options.index.table_row_action_text' => 'Modifica Opzioni',
     /**
      * Taxes.
      */
-    'taxes.tax-zones.index.title' => 'Tax Zones',
-    'taxes.tax-zones.confirm_delete.title' => 'Confirm removal',
-    'taxes.tax-zones.confirm_delete.message' => 'Are you sure you want to delete this Tax Zone? This could result in data loss.',
-    'taxes.tax-zones.customer_groups.title' => 'Restrict to customer groups',
-    'taxes.tax-zones.customer_groups.instructions' => 'Select which customer groups you want to restrict this zone to. Leave unchecked for no restrictions.',
-    'taxes.tax-zones.create_title' => 'Create tax zone',
-    'taxes.tax-zones.create_btn' => 'Create tax zone',
-    'taxes.tax-zones.delete_btn' => 'Delete tax zone',
-    'taxes.tax-zones.index.table_row_action_text' => 'Manage',
-    'taxes.tax-classes.index.title' => 'Tax Classes',
-    'taxes.tax-classes.index.create.title' => 'Create tax class',
-    'taxes.tax-classes.index.update.title' => 'Update tax class',
-    'taxes.tax-classes.create_btn' => 'Create tax class',
-    'taxes.tax-zones.price_display.label' => 'Price Display',
-    'taxes.tax-zones.price_display.excl_tax' => 'Exclude Tax',
-    'taxes.tax-zones.price_display.incl_tax' => 'Include Tax',
-    'taxes.tax-zones.zone_type.countries' => 'Limit to countries',
-    'taxes.tax-zones.zone_type.states' => 'Limit to states / provinces',
-    'taxes.tax-zones.zone_type.postcodes' => 'Limit to postcodes',
-    'taxes.tax-zones.tax_rates.title' => 'Tax Rates',
-    'taxes.tax-zones.tax_rates.create_button' => 'Add tax rate',
-    'taxes.tax-zones.save_btn' => 'Save tax zone',
-    'taxes.tax-classes.index.delete_message' => 'Are you sure? This could cause loss of data.',
-    'taxes.tax-classes.index.delete_message_disabled' => 'You cannot delete a tax class that is associated to product variants',
-    'taxes.tax-classes.index.delete_message_default' => 'You must select a new default before deleting',
+    'taxes.tax-zones.index.title' => 'Zone Fiscali',
+    'taxes.tax-zones.confirm_delete.title' => 'Conferma rimozione',
+    'taxes.tax-zones.confirm_delete.message' => 'Sei sicuro di voler rimuovere questa Zona Fiscale?, perderai i dati salvati.',
+    'taxes.tax-zones.customer_groups.title' => 'Limita ad un gruppo di clienti',
+    'taxes.tax-zones.customer_groups.instructions' => 'Selezione a quale gruppo di clienti vuoi applicare le restrizioni. Deseleziona il box se non vuoi applicare le restrizioni. ',
+    'taxes.tax-zones.create_title' => 'Crea zona fiscale',
+    'taxes.tax-zones.create_btn' => 'Crea zona fiscale',
+    'taxes.tax-zones.delete_btn' => 'Cancella zona fiscale',
+    'taxes.tax-zones.index.table_row_action_text' => 'Gestisci',
+    'taxes.tax-classes.index.title' => 'Categorie fiscali',
+    'taxes.tax-classes.index.create.title' => 'Crea una categoria fiscale',
+    'taxes.tax-classes.index.update.title' => 'Modifica una categoria fiscale',
+    'taxes.tax-classes.create_btn' => 'Crea una categoria fiscale',
+    'taxes.tax-zones.price_display.label' => 'Mostra prezzo',
+    'taxes.tax-zones.price_display.excl_tax' => 'Escludi tasse',
+    'taxes.tax-zones.price_display.incl_tax' => 'Includi tasse',
+    'taxes.tax-zones.zone_type.countries' => 'Limita per nazione',
+    'taxes.tax-zones.zone_type.states' => 'Limita per stato / provincia',
+    'taxes.tax-zones.zone_type.postcodes' => 'Limita per codice postale',
+    'taxes.tax-zones.tax_rates.title' => 'Aliquote fiscali',
+    'taxes.tax-zones.tax_rates.create_button' => 'Aggiungi aliquota fiscale',
+    'taxes.tax-zones.save_btn' => 'Salva aliquota fiscale',
+    'taxes.tax-classes.index.delete_message' => 'Sei sicuro? potresti perdere i dati salvati.',
+    'taxes.tax-classes.index.delete_message_disabled' => 'Non puoi rimuovere un aliquota fiscale che non è associata a nessuna variante di prodotto',
+    'taxes.tax-classes.index.delete_message_default' => 'Devi selezionare uno nuovo prima di cancellare',
     /**
      * Customer Groups.
      */
-    'customer-groups.index.title' => 'Customer Groups',
-    'customer-groups.index.create_btn' => 'Create Customer Group',
-    'customer-groups.index.table_row_action_text' => 'Edit Group',
+    'customer-groups.index.title' => 'Gruppo clienti',
+    'customer-groups.index.create_btn' => 'Aggiungi gruppo clienti',
+    'customer-groups.index.table_row_action_text' => 'Modifica gruppo',
     /**
      * Customer Groups show page.
      */
-    'customer-groups.show.title' => 'Edit Customer Group',
+    'customer-groups.show.title' => 'Modifica gruppo clienti',
     /**
      * Customer Groups create page.
      */
-    'customer-groups.create.title' => 'Create Customer Group',
-    'customer-groups.form.default_instructions' => 'Set whether this customer group should be the default',
+    'customer-groups.create.title' => 'Crea un gruppo clienti',
+    'customer-groups.form.default_instructions' => 'Scegli se questo gruppo di clienti è settato come default',
 ];
