0d0eddce94ae5c9e07c6dd08fc25296546681df4
Gennaro Landolfi <gennarolandolfi@rinodrummer.dev>
2023-07-22 16:04:19 +0200

    Fixed inputs.name in product type

--- packages/admin/resources/views/partials/forms/product-type.blade.php
+++ packages/admin/resources/views/partials/forms/product-type.blade.php
@@ -1,6 +1,6 @@
 <div class="flex-col space-y-4">
     <div class="flex-col px-4 py-5 space-y-4 bg-white shadow sm:rounded-md sm:p-6">
-        <x-hub::input.group label="Name"
+        <x-hub::input.group :label="__('adminhub::inputs.name')"
                             for="name"
                             :error="$errors->first('productType.name')"
                             required>
