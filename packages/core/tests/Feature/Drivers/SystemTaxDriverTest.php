lunarphp/lunar

commit 809feeb7515203ded956138bcbd4a287cf07dbb2

Merge in 0.3.1 (#1122)

packages/core/src/Drivers/SystemTaxDriver.php
@@ -11,6 +11,7 @@ use Lunar\Base\ValueObjects\Cart\TaxBreakdownAmount;
 use Lunar\DataTypes\Price;
 use Lunar\Models\CartLine;
 use Lunar\Models\Currency;
+use Spatie\LaravelBlink\BlinkFacade as Blink;
 
 class SystemTaxDriver implements TaxDriver
 {
@@ -96,7 +97,11 @@ class SystemTaxDriver implements TaxDriver
     {
         $taxZone = app(GetTaxZone::class)->execute($this->shippingAddress);
         $taxClass = $this->purchasable->getTaxClass();
-        $taxAmounts = $taxZone->taxAmounts()->whereTaxClassId($taxClass->id)->get();
+        $taxAmounts = Blink::once('tax_zone_rates_'.$taxZone->id.'_'.$taxClass->id, function () use ($taxClass, $taxZone){
+            return $taxZone->taxAmounts->first(
+               fn($amount) => $amount->tax_class_id == $taxClass->id
+            )->get();
+        });
 
         $breakdown = new TaxBreakdown;
 

packages/admin/resources/views/livewire/components/orders/capture.blade.php
@@ -23,7 +23,7 @@
       />
     </x-hub::input.group>
 
-    @if($this->transactionModel->amount->value > ($amount * 100))
+    @if($this->transactionModel->amount->decimal > $amount)
       <x-hub::alert level="danger">
         You're about to capture an amount less than the total transaction value.
       </x-hub::alert>

packages/admin/src/AdminHubServiceProvider.php
@@ -115,6 +115,8 @@ use Lunar\Hub\Http\Livewire\Components\Tables\Actions\UpdateStatus;
 use Lunar\Hub\Http\Livewire\Components\Tags;
 use Lunar\Hub\Http\Livewire\Dashboard;
 use Lunar\Hub\Http\Livewire\HubLicense;
+use Lunar\Hub\Http\Middleware\Authenticate;
+use Lunar\Hub\Http\Middleware\RedirectIfAuthenticated;
 use Lunar\Hub\Listeners\SetStaffAuthMiddlewareListener;
 use Lunar\Hub\Menu\MenuRegistry;
 use Lunar\Hub\Menu\OrderActionsMenu;
@@ -509,6 +511,11 @@ class AdminHubServiceProvider extends ServiceProvider
             'driver' => 'session',
             'provider' => 'staff',
         ]);
+
+        Livewire::addPersistentMiddleware([
+            Authenticate::class,
+            RedirectIfAuthenticated::class,
+        ]);
     }
 
     /**
@@ -518,7 +525,7 @@ class AdminHubServiceProvider extends ServiceProvider
      */
     protected function registerPermissionManifest()
     {
-        Gate::after(function ($user, $ability) {
+        Gate::after(function (Staff $user, $ability) {
             // Are we trying to authorize something within the hub?
             $permission = $this->app->get(Manifest::class)->getPermissions()->first(fn ($permission) => $permission->handle === $ability);
             if ($permission) {

packages/admin/src/Http/Livewire/Components/Products/AbstractProduct.php
@@ -242,7 +242,7 @@ abstract class AbstractProduct extends Component
                 $this->hasPriceValidationRules(),
                 [
                     'variant.stock' => 'required|min:0|numeric|max:10000000',
-                    'variant.backorder' => 'numeric|max:10000000',
+                    'variant.backorder' => 'required|min:0|numeric|max:10000000',
                     'variant.purchasable' => 'string|required',
                     'variant.length_value' => 'numeric|nullable',
                     'variant.length_unit' => 'string|nullable',

packages/admin/src/helpers.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Lunar\Facades\DB;
 use Lunar\DataTypes\Price;
 
 if (! function_exists('max_upload_filesize')) {

packages/core/config/cart.php
@@ -135,11 +135,12 @@ return [
     */
     'eager_load' => [
         'currency',
-        'shippingAddress',
-        'billingAddress',
+        'lines.purchasable.taxClass',
+        'lines.purchasable.values',
+        'lines.purchasable.product.thumbnail',
         'lines.purchasable.prices.currency',
         'lines.purchasable.prices.priceable',
         'lines.purchasable.product',
-        'lines.cart',
+        'lines.cart.currency',
     ],
 ];

packages/core/src/Base/Traits/HasDimensions.php
@@ -67,7 +67,7 @@ trait HasDimensions
      */
     public function getWeightAttribute()
     {
-        $unit = $this->weight_unit ?: 'mm';
+        $unit = $this->weight_unit ?: 'kg';
 
         return Converter::from("weight.{$unit}")->value($this->weight_value ?: 0);
     }

packages/core/src/Base/Traits/Searchable.php
@@ -120,9 +120,7 @@ trait Searchable
      */
     public function addSearchableAttribute($key, $value)
     {
-        if (! isset($this->additionalSearchFields[$key])) {
-            $this->additionalSearchFields[$key] = $value;
-        }
+        $this->additionalSearchFields[$key] = $value;
     }
 
     /**

commit e9bd680ebc8103c41229015d8858cac464ea857d

Fix: SystemTaxDriver only adds tax amounts for the applicable tax zones (#1168)
When a `TaxClass` has multiple `TaxRateAmount`s (e.g. one for every
country), the `SystemTaxDriver` incorrectly applies every one of them,
regardless of whether the `TaxZone` of the `TaxRateAmount` is relevant
for the shipping address.

I believe this is a regression introduced in commit
809feeb7515203ded956138bcbd4a287cf07dbb2 when some code was wrapped in a
`Blink::once()`. However, at the same time, the code inside was altered
slightly, which changed the logic.

Please refer to the test I added in this PR. The `TaxClass` has 4
`TaxRateAmount`s, 2 of which should be applied (the default ones), and 2
of which should not be applied.

Running this test on the current `SystemTaxDriver` will fail.

My proposal is to revert the logic in `SystemTaxDriver` to what it was
before the mentioned commit, while still being wrapped in
`Blink::once()`.

packages/core/src/Drivers/SystemTaxDriver.php
@@ -100,9 +100,7 @@ class SystemTaxDriver implements TaxDriver
         $taxClass = $this->purchasable->getTaxClass();
 
         $taxAmounts = Blink::once('tax_zone_rates_'.$taxZone->id.'_'.$taxClass->id, function () use ($taxClass, $taxZone) {
-            return $taxZone->taxAmounts->first(
-                fn ($amount) => $amount->tax_class_id == $taxClass->id
-            )->get();
+            return $taxZone->taxAmounts()->whereTaxClassId($taxClass->id)->get();
         });
 
         if (prices_inc_tax()) {

packages/core/tests/Feature/Drivers/SystemTaxDriverTest.php
@@ -2,6 +2,7 @@
 
 namespace Lunar\Tests\Feature\Drivers;
 
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Config;
 use Lunar\Base\ValueObjects\Cart\TaxBreakdown;
@@ -10,6 +11,10 @@ use Lunar\Models\Address;
 use Lunar\Models\CartLine;
 use Lunar\Models\Currency;
 use Lunar\Models\ProductVariant;
+use Lunar\Models\TaxClass;
+use Lunar\Models\TaxRate;
+use Lunar\Models\TaxRateAmount;
+use Lunar\Models\TaxZone;
 use Lunar\Tests\TestCase;
 
 /**
@@ -138,5 +143,47 @@ class SystemTaxDriverTest extends TestCase
 
         $this->assertInstanceOf(TaxBreakdown::class, $breakdown);
         $this->assertEquals(166, $breakdown->amounts[0]->price->value);
+    }
+
+    /** @test */
+    public function can_get_breakdown_with_correct_tax_zone()
+    {
+        $address = Address::factory()->create();
+        $currency = Currency::factory()->create();
+
+        $defaultTaxZone = TaxZone::factory()->state(['default' => true])->create();
+        $nonDefaultTaxZone1 = TaxZone::factory()->state(['default' => false])->create();
+        $nonDefaultTaxZone2 = TaxZone::factory()->state(['default' => false])->create();
+
+        $taxClass = TaxClass::factory()->has(
+            TaxRateAmount::factory()
+                ->count(4)
+                ->state(new Sequence(
+                    ['percentage' => 10, 'tax_rate_id' => TaxRate::factory()->state(['tax_zone_id' => $defaultTaxZone])],
+                    ['percentage' => 15, 'tax_rate_id' => TaxRate::factory()->state(['tax_zone_id' => $defaultTaxZone])],
+                    ['percentage' => 20, 'tax_rate_id' => TaxRate::factory()->state(['tax_zone_id' => $nonDefaultTaxZone1])],
+                    ['percentage' => 25, 'tax_rate_id' => TaxRate::factory()->state(['tax_zone_id' => $nonDefaultTaxZone2])],
+                ))
+        )->create();
+
+        $variant = ProductVariant::factory(['tax_class_id' => $taxClass->id])->create();
+        $line = CartLine::factory(['purchasable_id' => $variant->id])->create();
+        $subTotal = 1000; // 10.00 in decimal
+
+        $breakdown = (new SystemTaxDriver)
+            ->setShippingAddress($address)
+            ->setBillingAddress($address)
+            ->setCurrency($currency)
+            ->setPurchasable($variant)
+            ->setCartLine($line)
+            ->getBreakdown($subTotal);
+
+        $this->assertInstanceOf(TaxBreakdown::class, $breakdown);
+
+        //Only the 2 tax rates from the default tax zone should have been applied
+        $this->assertEquals(2, $breakdown->amounts->count());
+
+        $this->assertEquals(100, $breakdown->amounts[0]->price->value);
+        $this->assertEquals(150, $breakdown->amounts[1]->price->value);
     }
 }