b79268d3ee39f4a597e5e4c80a582327b6b11909
Ivanka Todorova <1038697+fakeheal@users.noreply.github.com>
2023-08-18 15:00:13 +0300

    fix: generate unique slugs using random string if necessary (#1204)

--- packages/core/src/Generators/UrlGenerator.php
+++ packages/core/src/Generators/UrlGenerator.php
@@ -2,9 +2,11 @@
 
 namespace Lunar\Generators;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use Lunar\Models\Language;
+use Lunar\Models\Url;
 
 class UrlGenerator
 {
@@ -60,12 +62,86 @@ class UrlGenerator
      */
     protected function createUrl($value)
     {
-        $slug = Str::slug($value);
+        $uniqueSlug = $this->getUniqueSlug(Str::slug($value));
 
         $this->model->urls()->create([
             'default' => true,
             'language_id' => $this->defaultLanguage->id,
-            'slug' => $slug,
+            'slug' => $uniqueSlug,
         ]);
+    }
+
+    /**
+     * Generates unique slug based on the given slug by adding suffix numbers.
+     *
+     * @return string
+     */
+    private function getUniqueSlug($slug)
+    {
+        $separator = '-'; // can be configurable?
+
+        $slugs = $this->getExistingSlugs($slug, $separator);
+
+        // it is already unique
+        if (! $slugs->count() || $slugs->contains($slug) === false) {
+            return $slug;
+        }
+
+        if ($slugs->has($this->model->getKey())) {
+            $currentSlug = $slugs->get($this->model->getKey());
+
+            if ($currentSlug === $slug || str_starts_with($currentSlug, $slug)) {
+                return $currentSlug;
+            }
+        }
+
+        $suffix = $this->getSuffix($slug, $separator, $slugs);
+
+        return $slug.$separator.$suffix;
+    }
+
+    /**
+     * Get all urls similar to the given slug.
+     *
+     * @param  string  $slug
+     * @param  string  $separator
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    private function getExistingSlugs($slug, $separator)
+    {
+        return Url::where(function ($query) use ($slug, $separator) {
+            $query->where('slug', $slug)
+                ->orWhere('slug', 'like', $slug.$separator.'%');
+        })->whereLanguageId($this->defaultLanguage->id)
+            ->select(['element_id', 'slug'])
+            ->get()
+            ->toBase()
+            ->pluck('slug', 'element_id');
+    }
+
+    /**
+     * @param  string  $slug
+     * @param  string  $separator
+     * @param  Collection  $slugs
+     * @return string
+     */
+    private function getSuffix($slug, $separator, $slugs)
+    {
+        $len = strlen($slug.$separator);
+
+        if ($slugs->search($slug) === $this->model->getKey()) {
+            $suffix = explode($separator, $slug);
+
+            return end($suffix);
+        }
+
+        $slugs->transform(function ($value, $key) use ($len) {
+            return (int) substr($value, $len);
+        });
+
+        $max = $slugs->max();
+
+        // starts suffixing from 2, eg: test-product, test-product-2, test-product-3, etc
+        return (string) ($max === 0 ? 2 : $max + 1);
     }
 }
--- packages/core/tests/Unit/Models/BrandTest.php
+++ packages/core/tests/Unit/Models/BrandTest.php
@@ -45,4 +45,50 @@ class BrandTest extends TestCase
             'element_id' => $brand->id,
         ]);
     }
+
+    /** @test */
+    public function generates_unique_urls()
+    {
+        Config::set('lunar.urls.generator', UrlGenerator::class);
+
+        Language::factory()->create([
+            'default' => true,
+        ]);
+
+        $brand1 = Brand::factory()->create([
+            'name' => 'Test Brand',
+        ]);
+
+        $brand2 = Brand::factory()->create([
+            'name' => 'Test Brand',
+        ]);
+
+        $brand3 = Brand::factory()->create([
+            'name' => 'Test Brand',
+        ]);
+
+        $brand4 = Brand::factory()->create([
+            'name' => 'Brand Test',
+        ]);
+
+        $this->assertEquals(
+            'test-brand',
+            $brand1->urls->first()->slug
+        );
+
+        $this->assertEquals(
+            'test-brand-2',
+            $brand2->urls->first()->slug
+        );
+
+        $this->assertEquals(
+            'test-brand-3',
+            $brand3->urls->first()->slug
+        );
+
+        $this->assertEquals(
+            'brand-test',
+            $brand4->urls->first()->slug
+        );
+    }
 }
