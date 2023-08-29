27d10df938fd762c1a6ff59bad3ccb2afad4ed81
Alec Ritson <hello@itsalec.co.uk>
2023-08-29 11:21:08 +0100

    Rename command

--- packages/core/src/Console/Commands/ScoutIndexerCommand.php
+++ packages/core/src/Console/Commands/ScoutIndexerCommand.php
@@ -4,7 +4,7 @@ namespace Lunar\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class ScoutIndexer extends Command
+class ScoutIndexerCommand extends Command
 {
     /**
      * The name and signature of the console command.
