120dce57cc028626e8c934c9a7dff6206801d046
Timmy D'Hooghe <timmy@web-app.be>
2023-10-03 01:30:44 +0200

    Dutch translations

---
+++ packages/admin/resources/lang/nl/menu.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    'sidebar.index' => 'Dashboard',
+    'sidebar.catalogue-manager' => 'Catalogusbeheer',
+    'sidebar.products' => 'Producten',
+    'sidebar.product-types' => 'Product types',
+    'sidebar.brands' => 'Merken',
+    'sidebar.collections' => 'Collecties',
+    'sidebar.order-processing' => 'Orderverwerking',
+    'sidebar.orders' => 'Bestellingen',
+    'sidebar.customers' => 'Klanten',
+    'sidebar.catalogue' => 'Catalogus',
+    'sidebar.sales' => 'Verkoop',
+    'sidebar.discounts' => 'Kortingen',
+    /**
+     * Product side menu.
+     */
+    'product.basic-information' => 'Basis informatie',
+    'product.choose-existing-btn' => 'Kies bestaande',
+    'product.attributes' => 'Attributen',
+    'product.images' => 'Afbeeldingen',
+    'product.image' => 'Afbeelding',
+    'product.availability' => 'Beschikbaarheid',
+    'product.variants' => 'Varianten',
+    'product.pricing' => 'Prijzen',
+    'product.inventory' => 'Voorraad',
+    'product.shipping' => 'Verzending',
+    'product.associations' => 'Associaties',
+    'product.urls' => 'URLs',
+    'product.associations.title' => 'Product associaties',
+    'product.associations.explain' => 'Met productassociaties kunt u producten aan elkaar linken.',
+    'product.identifiers' => 'Identificatiegegevens',
+    'product.collections' => 'Collecties',
+    'product.select-product-image' => 'Selecteer een bestaande productafbeelding',
+    'product.choose-image' => 'Kies een afbeelding',
+    /**
+     * Generic menu items.
+     */
+    'attributes' => 'Attributen',
+    'basic-information' => 'Basis informatie',
+    'images' => 'Afbeeldingen',
+    'availability' => 'Beschikbaarheid',
+    'urls' => 'URLs',
+    'products' => 'Producten',
+    'collections' => 'Onderliggende collecties',
+    'dropdown-options' => 'Open opties',
+    'open-sidebar' => 'Open zijbalk',
+    'close-sidebar' => 'Sluit zijbalk',
+];
