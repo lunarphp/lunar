9a57b671b00259246eaf6c6eb56320ba86c7af8d
Timmy D'Hooghe <timmy@web-app.be>
2023-10-02 23:46:29 +0200

    Dutch translations

---
+++ packages/admin/resources/lang/nl/catalogue.php
@@ -0,0 +1,139 @@
+<?php
+
+return [
+    /**
+     * Collections.
+     */
+    'collections.sidemenu.create_btn' => 'Collectiegroep aanmaken',
+    'collections.sidemenu.modal.title' => 'Collectiegroep aanmaken',
+    'collections.sidemenu.modal.btn' => 'Collectiegroep aanmaken',
+    'collections.sidemenu.menu_title' => 'Collectiegroepen',
+    'collections.groups.delete_group_btn' => 'Collectiegroep verwijderen',
+    'collections.groups.add_collection_btn' => 'Collectie toevoegen',
+    'collections.groups.delete.title' => 'Collectiegroep verwijderen',
+    'collections.groups.delete.strapline' => 'Weet je zeker dat je deze collectiegroep wil verwijderen?',
+    'collections.groups.delete.warning' => 'Alle collecties in deze groep zullen ook verwijderd worden.',
+    'collections.groups.delete.confirm' => 'Bevestig dat je de collectiegroep en alle bijhorende collecties wil verwijderen',
+    'collections.groups.delete.btn' => 'Verwijder collectiegroep',
+    'collections.groups.move.title' => 'verplaats collectiegroep',
+    'collections.groups.move.search_placeholder' => 'Zoek een collectie om onder te plaatsen',
+    'collections.groups.move.alert' => 'Dit zal :source onder :target plaatsen.',
+    'collections.groups.move.btn' => 'Verplaats Collection',
+    'collections.groups.node.make_root' => 'Maak hoofd',
+    'collections.groups.node.move' => 'Verplaats collectie',
+    'collections.groups.node.edit' => 'Bewerk collectie',
+    'collections.groups.node.add_child' => 'Voeg onderliggende collectie toe',
+    'collections.groups.node.delete' => 'Verwijder collectie',
+    'collections.groups.no_groups' => 'Het lijkt erop dat je geen collectiegroepen hebt.',
+    'collections.create.child.title' => 'Voeg collectie toe aan :parent.',
+    'collections.create.root.title' => 'Voeg een collectie op hoofdniveau toe',
+    'collections.create.btn' => 'Collectie aanmaken',
+    'collections.delete.title' => 'Collectie verwijderen',
+    'collections.delete.btn' => 'Collectie verwijderen',
+    'collections.delete.warning' => 'Alle onderliggende collecties zullen ook verwijderd worden.',
+    'collections.delete.child.warning' => 'Deze collectie heeft :count onderliggende collecties, deze zullen ook verwijderd worden.',
+    'collections.delete.root.warning' => 'Weet je zeker dat je deze collectie wil verwijderen?',
+    'collections.index.title' => 'Collecties',
+    'collections.show.save_btn' => 'Collectie opslaan',
+    'collections.show.sort.base_price_asc' => 'Basisprijs Opl.',
+    'collections.show.sort.base_price_desc' => 'Basisprijs Afl.',
+    'collections.show.sort.sku_asc' => 'SKU Opl.',
+    'collections.show.sort.sku_desc' => 'SKU Afl.',
+    'collections.show.sort.custom' => 'Aangepast',
+    'collections.show.no_products' => 'Deze collectie heeft geen gelinkte producten.',
+    'collections.show.no_children' => 'Deze collectie heeft geen onderliggende collecties.',
+    'collections.associate_to_other_products' => 'Kies producten die je aan dit product wil linken.',
+    'collections.static_collections' => 'Statische collecties',
+    'collections.summer_seasonal' => 'Zomers seizoensgebonden',
+    /**
+     * Product show.
+     */
+    'products.show.back_link_title' => 'Ga terug naar de lijst van producten',
+    'products.show.save_btn' => 'Product opslaan',
+    'products.show.no_channel_availability' => 'Dit product heeft geen beschikbaarheid in de verschillende kanalen.',
+    'products.show.delete_title' => 'Product verwijderen',
+    'products.show.delete_strapline' => 'Dit zal het product naar de prullenmand verplaatsen, je zal het later kunnen herstellen',
+    'products.show.restore_strapline' => 'Herstel dit product zodat het opnieuw beschikbaar wordt in jouw winkel.',
+    'products.show.delete_btn' => 'Verwijderen bevestigen',
+    'products.show.restore_title' => 'Product herstellen',
+    'products.show.restore_btn' => 'Herstel bevestigen',
+
+    /**
+     * Product variant.
+     */
+    'product-variants.delete_confirm.title' => 'Verwijderen variant bevestigen',
+    'product-variants.delete_confirm.strapline' => 'Deze actie kan niet ongedaan gemaakt worden.',
+    'product-variants.delete_confirm.btn' => 'Product variant verwijderen',
+    'product-variants.add_variant.btn' => 'Variant toevoegen',
+    'product-variants.add_variant.already_exists' => 'Deze variant bestaat reeds.',
+    'product-variants.add_variant.null_option' => 'Kies een optie',
+    'product-variants.add_variant.title' => 'Voeg nog een variant toe',
+    'product-variants.add_variant.add_new_option' => 'Voeg een nieuwe optie toe',
+    /**
+     * Product type listing.
+     */
+    'product-types.index.title' => 'Product types',
+    'product-types.index.create_btn' => 'Product ype toevoegen',
+    /**
+     * Product type editing.
+     */
+    'product-types.show.updated_message' => 'Product type aangepast',
+    'product-types.create.btn_text' => 'Product type toevoegen',
+    'product-types.show.btn_text' => 'Product type aanpassen',
+    'product-types.attribute.search.empty' => 'Het lijkt erop dat je geen attributen meer hebt om toe te wijzen.',
+    'product-types.attribute.search.no_results' => 'Kan geen attributen vinden die overeenkomen met ":search"',
+    'product-types.show.title' => 'Product type aanpassen',
+    'product-types.show.delete.btn_text' => 'Product type verwijderen',
+    'product-types.show.delete.message' => 'Weet je zeker dat je dit product type permanent wil verwijderen?',
+    'product-types.show.delete.disabled_message' => 'Je kan geen product type verwijderen waar producten aan gelinkt zijn.',
+    'product-types.show.delete.delete_notification' => 'Product type verwijderd',
+    'product-types.show.delete.confirm_text' => 'Verwijderen bevestigen',
+    'product-types.show.delete.minimum_required' => 'Je kan het enige product type niet verwijderen.',
+    'product-types.attribute.no_attributes' => 'Er zijn geen attributen te selecteren in deze groep',
+    /**
+     * Brands.
+     */
+    'brands.index.title' => 'Merken',
+    'brands.create.title' => 'Merk aanmaken',
+    'brands.show.back_link_title' => 'Terug naar merken',
+    /**
+     * Customers.
+     */
+    'customers.index.title' => 'Klanten',
+    'customers.index.placeholder' => 'Klanten zoeken',
+    'customers.show.save_btn' => 'Klant opslaan',
+    'customers.show.customer_groups_updated' => 'Klantgroep aangepast',
+    'customer.show.customer_groups' => 'Klantgroepen',
+    'customer.dashboard.returning_customers' => 'Terugkerende klanten',
+    'customer.dashboard.no_of_orders' => 'Aantal bestellingen',
+    'customer.dashboard.turnover' => 'Omzet',
+    'customer.dashboard.sales_performance' => 'Verkoopprestaties',
+    'customer.dashboard.customer_group_orders' => 'Klantgroep bestellingen',
+    'customer.dashboard.top_selling_products' => 'Best verkopende producten',
+    /**
+     * Customer Groups Editing.
+     */
+    'editing.customer-groups.select_groups' => 'Selecteer voor welke klantgroepen dit product beschikbaar is.',
+    'editing.customer-groups.publish_on' => 'De publicatie van dit product is gepland op',
+    'editing.customer-groups.schedule_availability' => 'Beschikbaarheid plannen',
+    /**
+     * Orders.
+     */
+    'orders.show.customer_reference' => 'Klantreferentie',
+    'orders.show.shipping_option' => 'Verzendoptie',
+    'orders.show.shipping_address' => 'Verzendingsadres',
+    'orders.show.billing_address' => 'Factuuradres',
+    'orders.show.order_lines' => 'Orderregels',
+    'orders.show.transactions' => 'Transacties',
+    'orders.dashboard.recent_orders' => 'Recente bestellingen',
+    /**
+     * Stock.
+     */
+    'stock' => 'Klantreferentie',
+    /**
+     * Addons.
+     */
+    'addons.show.addon_details' => 'Plugin Details',
+    'addons.show.developer' => 'Ontwikkelaar',
+    'addons.show.marketplace' => 'Marktplaats',
+];
---
+++ packages/admin/resources/lang/nl/components.php
@@ -0,0 +1,289 @@
+<?php
+
+return [
+    'dashboard.top_timeframe' => 'Laatste :period dagen',
+    'products.option-manager.add_btn' => 'Optie toevoegen',
+    'products.option-manager.toggle_btn' => 'Alles (de)selecteren',
+    'products.product-selector.select_btn' => 'Kies opties',
+    'products.product-selector.title' => 'Kies opties',
+    'products.product-selector.available_tab' => 'Beschikbare opties',
+    'products.product-selector.selected_tab' => 'Geselecteerde opties',
+    'products.product-selector.selected_empty' => 'Geen opties gevonden voor deze zoekterm.',
+    'products.product-selector.no_results' => 'Geen opties gevonden voor deze zoekterm.',
+    'products.product-selector.no_options' => 'Er zijn geen opties beschikbaar, Maak een nieuwe optie aan om ze hier te zien.',
+    'products.product-selector.add_new_btn' => 'Nieuwe optie toevoegen',
+    'products.product-selector.use_selected_btn' => 'Gebruik geselecteerde opties',
+    'products.option-creator.option_placeholder' => 'Bvb. Kleur',
+    'products.option-creator.value_placeholder' => 'Bvb. Blauw',
+    'products.option-creator.min_values_notice' => 'Je moet minstens :min waarde hebben',
+    'products.option-creator.values_title' => 'Productoptie waarden',
+    'products.option-creator.title' => 'Nieuwe optie aanmaken',
+    'products.option-creator.add_value_btn' => 'Waarde toevoegen',
+    'products.option-creator.create_option_btn' => 'Optie aanmaken',
+    'products.option-creator.values_strapline' => 'Voeg alle mogelijke opties toe die beschikbaar zijn voor deze productoptie.',
+    'product-search.btn' => 'Producten toevoegen',
+    'product-search.first_tab' => 'Producten zoeken',
+    'product-search.second_tab' => 'Geselecteerde producten',
+    'product-search.max_results_exceeded' => 'De eerste :max van in totaal :total producten worden weergegeven. Probeer specifieker te zoeken.',
+    'product-search.exists_in_collection' => 'Reeds gelinkt',
+    'product-search.no_results' => 'Geen resultaten gevonden.',
+    'product-search.pre_search_message' => 'Zoek naar producten op attribuut of SKU.',
+    'product-search.select_empty' => 'Wanneer je producten selecteert, verschijnen ze hier.',
+    'product-search.title' => 'Zoeken naar producten',
+    'product-search.associate_self' => 'Je kan een product niet aan zichzelf linken',
+    'product-search.commit_btn' => 'Producten selecteren',
+    /**
+     * Option Value Create Modal.
+     */
+    'ovcm.title' => 'Nieuwe optie toevoegen aan :name',
+    /**
+     * Attribute group create.
+     */
+    'attribute-group-edit.name.placeholder' => 'Bvb. Extra informatie',
+    'attribute-group-edit.create_btn' => 'Attribuutgroep aanmaken',
+    'attribute-group-edit.update_btn' => 'Attribuutgroep aanpassen',
+    'attribute-group-edit.non_unique_handle' => 'Deze naam is reeds in gebruik',
+    /**
+     * Attribute show.
+     */
+    'attributes.show.create_group_btn' => 'Attribuutgroep aanmaken',
+    'attributes.show.create_attribute' => 'Attribuut aanmaken',
+    'attributes.show.edit_group_btn' => 'Attribuutgroep aanpassen',
+    'attributes.show.edit_attribute_btn' => 'Attribuut aanpassen',
+    'attributes.show.delete_group_btn' => 'Attribuutgroep verwijderen',
+    'attributes.show.edit_title' => 'Attribuutgroep aanpassen',
+    'attributes.show.create_title' => 'Attribuutgroep aanmaken',
+    'attributes.show.delete_title' => 'Attribuutgroep verwijderen',
+    'attributes.show.delete_warning' => 'Het verwijderen van deze klantgroep zal ook alle gelinkte attributen verwijderen. Deze actie kan niet ongedaan gemaakt worden.',
+    'attributes.show.group_protected' => 'Deze groep bevat attributen die het systeem nodig heeft, ze kunnen daarom niet verwijderd worden.',
+    'attributes.show.no_attributes_text' => 'Er bestaan geen attributen, sleep bestaande attributen naar hier of voeg nieuwe toe.',
+    'attributes.show.delete_attribute_btn' => 'Attribuut verwijderen',
+    'attributes.show.delete_attribute_title' => 'Attribuut verwijderen',
+    'attributes.show.delete_attribute_warning' => 'Weet je zeker dat je dit attribuut wil verwijderen?',
+    'attributes.show.delete_attribute_protected' => 'Je kan geen systeemattribuut verwijderen.',
+    'attributes.show.no_groups' => 'Geen attribuutgroepen gevonden, voeg je eerste toe voor je er attributen aan kan toevoegen.',
+    /**
+     * Attribute edit.
+     */
+    'attribute-edit.create_title' => 'Attribuut aanmaken',
+    'attribute-edit.update_title' => 'Attribuut aanpassen',
+    'attribute-edit.system_locked' => 'Dit attribuut wordt door het systeem gebruikt, sommige velden zijn daarom uitgeschakeld.',
+    'attribute-edit.name.placeholder' => 'Bvb. Naam',
+    'attribute-edit.required.instructions' => 'Is dit attribuut verplicht bij het aanmaken/bewerken?',
+    'attribute-edit.searchable.instructions' => 'Moeten gebruikers kunnen zoeken op dit attribuut?',
+    'attribute-edit.filterable.instructions' => 'Moeten gebruikers resultaten moeten kunnen filteren op basis van dit attribuut?',
+    'attribute-edit.validation.instructions' => 'Specifieer alle Laravel validatie regels voor dit veld.',
+    'attribute-edit.cancel_btn' => 'Annuleren',
+    'attribute-edit.save_attribute_btn' => 'Attribuut opslaan',
+
+    /**
+     * Brand search.
+     */
+    'brand-search.btn' => 'Merken toevoegen',
+    'brand-search.first_tab' => 'Merken zoeken',
+    'brand-search.second_tab' => 'Geselecteerde merken',
+    'brand-search.max_results_exceeded' => 'De eerste :max van in totaal :total merken worden weergegeven. Probeer specifieker te zoeken.',
+    'brand-search.exists_in_collection' => 'Reeds gelinkt',
+    'brand-search.no_results' => 'Geen resultaten gevonden.',
+    'brand-search.pre_search_message' => 'Zoek naar merken op attribuut.',
+    'brand-search.select_empty' => 'Wanneer je merken selecteert zullen ze hier verschijnen.',
+    'brand-search.title' => 'Zoeken naar merken',
+    'brand-search.commit_btn' => 'Geselecteerde merken',
+
+    /**
+     * Collection search.
+     */
+    'collection-search.btn' => 'Collectie toevoegen',
+    'collection-search.first_tab' => 'Collecties zoeken',
+    'collection-search.second_tab' => 'Geselecteerde collecties',
+    'collection-search.max_results_exceeded' => 'De eerste :max van in totaal :total collecties worden weergegeven. Probeer specifieker te zoeken',
+    'collection-search.exists_in_collection' => 'Reeds gelinkt',
+    'collection-search.no_results' => 'Geen resultaten gevonden.',
+    'collection-search.pre_search_message' => 'Zoek naar collecties op attribuut.',
+    'collection-search.select_empty' => 'Wanneer je collecties selecteert zullen ze hier verschijnen.',
+    'collection-search.title' => 'Zoeken naar collecties',
+    'collection-search.commit_btn' => 'Select Collections',
+    /**
+     * Customers.
+     */
+    'customers.show.metrics.total_orders' => 'Totaal bestellingen',
+    'customers.show.metrics.avg_spend' => 'Gemiddelde uitgave',
+    'customers.show.metrics.total_spend' => 'Totaal uitgaven',
+    'customers.show.year_spending' => 'Uitgaven afgelopen jaar',
+    'customers.show.purchase_history' => 'Aankoopgeschiedenis',
+    'customers.show.order_history' => 'Bestelhistoriek',
+    'customers.show.users' => 'Gebruikers',
+    'customers.show.addresses' => 'Adres klant',
+    'customers.show.customer_groups' => 'Klantgroepen',
+    'customers.show.save_customer' => 'Klant opslaan',
+    'customers.show.no_purchase_history' => 'Deze klant heeft geen aankoophistoriek.',
+    'customers.show.no_order_history' => 'Deze klant heeft geen bestelhistoriek.',
+    'customers.show.no_users' => 'Deze klant heeft geen gelinkte gebruikers.',
+    'customers.show.no_addresses' => 'Deze klant heeft geen adressen.',
+    'customers.show.remove_address_btn' => 'Verwijderen',
+    'customers.show.remove_address.title' => 'Adres verwijderen',
+    'customers.show.remove_address.confirm' => 'Weet je zeker dat je dit adres wil verwijderen?',
+    /**
+     * Orders Index
+     */
+    'orders.index.returning_customer' => 'Terugkerende klant',
+    'orders.index.new_customer' => 'Nieuwe klant',
+    /**
+     * Order Show.
+     */
+    'orders.show.title' => 'Bestelling',
+    'orders.show.save_shipping_btn' => 'Adres opslaan',
+    'orders.show.save_billing_btn' => 'Adres opslaan',
+    'orders.show.print_btn' => 'Print',
+    'orders.show.refund_btn' => 'Terugbetaling',
+    'orders.show.refund_lines_btn' => 'Terugbetalingslijnen',
+    'orders.show.update_status_btn' => 'Status aanpassen',
+    'orders.show.more_actions_btn' => 'Meer acties',
+    'orders.show.show_all_lines_btn' => 'Toon alle lijnen',
+    'orders.show.additional_lines_text' => 'Er zijn nog :count lijnen verborgen',
+    'orders.show.collapse_lines_btn' => 'Lijnen samenvouwen',
+    'orders.show.transactions_header' => 'Transacties',
+    'orders.show.timeline_header' => 'Tijdlijn',
+    'orders.show.additional_fields_header' => 'Bijkomende informatie',
+    'orders.show.billing_matches_shipping' => 'Zelfde als verzendadres',
+    'orders.show.billing_header' => 'Factuuradres',
+    'orders.show.shipping_header' => 'Verzendadres',
+    'orders.show.requires_capture' => 'Aan deze bestelling moet nog een betaling toegevoegd worden.',
+    'orders.show.capture_payment_btn' => 'Betaling registreren',
+    'orders.show.partially_refunded' => 'Deze bestelling is deels terugbetaald.',
+    'orders.show.refunded' => 'Deze bestelling is terugbetaald.',
+    'orders.show.view_customer' => 'Bekijk klant',
+    'orders.show.tags_header' => 'Labels',
+    'orders.show.download_pdf' => 'Download PDF',
+
+    /**
+     * Order Refund.
+     */
+    'orders.refund.confirm_text' => 'BEVESTIGEN',
+    'orders.refund.confirm_message' => 'Gelieve te bevestigen dat je dit bedrag wil terugbetalen.',
+    'orders.refund.no_charges' => 'Er zijn geen terugbetaalbare kosten aan deze bestelling gelinkt',
+    'orders.refund.select_transaction' => 'Selecteer een transactie',
+    'orders.refund.refund_btn' => 'Terugbetalen',
+    'orders.refund.fully_refunded' => 'De kosten gelinkt aan deze bestelling werden terugbetaald',
+    /**
+     * Brand Index.
+     */
+    'brands.index.title' => 'Merken',
+    'brands.index.create_brand' => 'Merk aanmaken',
+    'brands.index.table_row_action_text' => 'Merk aanpassen',
+    'brands.index.table_count_header_text' => 'Aantal producten',
+    'brands.choose_brand_default_option' => 'Merkloos',
+    /**
+     * Product Index.
+     */
+    'products.index.title' => 'Producten',
+    'products.index.create_product' => 'Product aanmaken',
+    'products.index.selected_products' => 'Je hebt :count producten geselecteerd, wil je ze allemaal selecteren?',
+    'products.index.you_have_selected_all' => 'Je hebt alle :count producten geselecteerd.',
+    'products.index.select_all_btn' => 'Alles selecteren',
+    'products.index.deselect_all_btn' => 'Alles deselecteren',
+    'products.index.draft' => 'Concept',
+    'products.index.published' => 'Gepubliceerd',
+    'products.index.deleted' => 'Verwijderd',
+    'products.index.only_deleted_visible' => 'Alleen verwijderde producten worden momenteel weergegeven',
+    'products.index.products_empty' => 'Geen producten gevonden die overeenkomen met de zoektermen/filters.',
+    /**
+     * Order Capture.
+     */
+    'orders.capture.confirm_text' => 'BEVESTIGEN',
+    'orders.capture.confirm_message' => 'Gelieve te bevestigen dat je deze betaling wil vastleggen',
+    'orders.capture.no_intents' => 'Er zijn geen transacties die vastgelegd kunnen worden',
+    'orders.capture.select_transaction' => 'Selecteer een transactie',
+    'orders.capture.capture_btn' => 'Betaling vastleggen',
+    /**
+     * Order Status.
+     */
+    'orders.status.update_btn' => 'Status aanpassen',
+    'orders.status.select_new' => 'Selecteer een nieuwe status',
+    'orders.status.preview.title' => 'Voorbeeld sjabloon',
+    'orders.status.preview.alert' => 'Dit is een voorbeeld van hoe jouw e-mail eruit zal zien.',
+    'orders.status.no_status_selected_alert' => 'Selecteer een bestellingsstatus om de beschikbare mailers te zien.',
+    'orders.status.additional-content.label' => 'Extra inhoud',
+    'orders.status.additional-content.instructions' => 'Indien ondersteund, voeg een extra bericht toe aan de melding of mailer.',
+    'orders.status.mailers.label' => 'Mailers',
+    'orders.status.mailers.instructions' => 'Selecteer welke mailers je wilt verzenden.',
+    'orders.status.mailers.empty' => 'Er zijn geen mailers beschikbaar voor deze status.',
+    'orders.status.notifications.label' => 'Meldingen',
+    'orders.status.notifications.instructions' => 'Selecteer welke melding je wil verzenden.',
+    'orders.status.notifications.empty' => 'Er zijn geen meldingen beschikbaar voor deze status.',
+    'orders.status.email_addresses.label' => 'E-mailadres',
+    'orders.status.email_addresses.instructions' => 'Selecteer welk e-mailadres je wil gebruiken',
+    'orders.status.additional_email.label' => 'Extra e-mailadres',
+    'orders.status.additional_email.instructions' => 'Als je een aangepast e-mailadres wil gebruiken, vul het dan hier in.',
+    /**
+     * Activity Log.
+     */
+    'activity-log.system' => 'Systeem',
+    'activity-log.orders.status_change' => 'Status aangepast',
+    'activity-log.orders.order_created' => 'Bestelling aangemaakt',
+    'activity-log.orders.capture' => 'Betaling van :amount met kaart eindigend op :last_four',
+    'activity-log.orders.authorized' => 'Geautoriseerde betaling van :amount met kaart eindigend op :last_four',
+    'activity-log.orders.refund' => 'Terugbetaling van :amount op kaart eindigend met :last_four',
+    /**
+     * Option value edit.
+     */
+    'option.value.edit.create_title' => 'Optie waarde aanmaken',
+    'option.value.edit.update_title' => 'Optie waarde aanpassen',
+    'option.value.edit.delete_locked' => 'Deze optie waarde kan niet verwijderd worden omdat het gebruikt wordt door :count productvarianten',
+    'option.value.edit.system_locked' => 'Deze optie waarde wordt gebruikt door het systeem, waardoor sommige velden uitgeschakeld zijn.',
+    'option.value.edit.name.placeholder' => 'Bvb. Naam',
+    'option.value.edit.required.instructions' => 'Is deze optie waarde verplicht bij het aanmaken/bewerken?',
+    'option.value.edit.searchable.instructions' => 'Moeten gebruikers kunnen zoeken op deze optie waarde?',
+    'option.value.edit.filterable.instructions' => 'Moeten gebruikers resultaten moeten kunnen filteren op basis van deze optie waarde?',
+    'option.value.edit.validation.instructions' => 'Specifieer alle Laravel validatie regels voor dit veld.',
+    'option.value.edit.cancel_btn' => 'Annuleren',
+    'option.value.edit.save_feature.value.btn' => 'Optie waarde opslaan',
+    /**
+     * Option value show.
+     */
+    'option.create_group_btn' => 'Optie aanmaken',
+    'option.create_option_value' => 'Optie waarde aanmaken',
+    'option.update_option_value' => 'Optie waarde aanpassen',
+    'option.value_title' => 'Product optie waarden',
+    'option.save_positions' => 'Posities opslaan',
+    'option.edit_group_btn' => 'Optie aanpassen',
+    'option.edit_option.value.btn' => 'Optie waarde aanpassen',
+    'option.delete_group_btn' => 'Optie verwijderen',
+    'option.edit_title' => 'Optie aanpassen',
+    'option.create_title' => 'Optie aanmaken',
+    'option.delete_title' => 'Optie verwijderen',
+    'option.delete_warning' => 'Je kan geen optie verwijderen die waarden heeft.',
+    'option.group_protected' => 'Deze groep bevat optie waarden die het systeem nodig heeft, je kan ze daarom niet verwijderen.',
+    'option.no_option_values_text' => 'Er bestaan geen optie waarden.',
+    'option.delete_option.value.btn' => 'Verwijder optie waarde',
+    'option.delete_option.value.title' => 'Verwijder optie waarde',
+    'option.delete_option.value.warning' => 'Weet je zeker dat je deze optie waarde wil verwijderen?',
+    'option.delete_option.value.protected' => 'Je kan geen optie waarde verwijderen die het systeem nodig heeft.',
+    'option.no_groups' => 'Geen opties gevonden, maak een eerste optie aan voor je er waarden aan kan toevoegen.',
+    /**
+     * Option value edit.
+     */
+    'option-edit.create_btn' => 'Optie aanmaken',
+    'option-edit.update_btn' => 'Optie aanpassen',
+    'option.value.edit.save_option.value.btn' => 'Optie waarde opslaan',
+    /**
+     * Discounts.
+     */
+    'discounts.index.title' => 'Kortingen',
+    'discounts.index.status.pending' => 'In behandeling',
+    'discounts.index.status.active' => 'Actief',
+    'discounts.index.status.scheduled' => 'Gepland',
+    'discounts.index.status.expired' => 'Verlopen',
+    'discounts.index.create_discount' => 'Korting aanmaken',
+    'discounts.create.title' => 'Korting aanmaken',
+    'discounts.create_btn' => 'Korting aanmaken',
+    'discounts.save_btn' => 'Korting opslaan',
+    'discounts.show.stop.label' => 'Stop met het toepassen van andere kortingen na deze',
+    'discounts.show.danger_zone.label' => 'Korting verwijderen',
+    'discounts.show.danger_zone.instructions' => 'Voer de naam van de korting in om de verwijdering ervan te bevestigen.',
+    /**
+     * Model URLs component
+     */
+    'model-url.preview' => 'Voorbeeld',
+    'model-url.view' => 'Weergave',
+];
