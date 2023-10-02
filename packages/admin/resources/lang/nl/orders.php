0eef7e9b1ab4f73254a04ea9c7f7779ccd86106c
Timmy D'Hooghe <timmy@web-app.be>
2023-10-03 01:16:21 +0200

    Dutch translations

---
+++ packages/admin/resources/lang/nl/orders.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /**
+     * Order listing.
+     */
+    'index.title' => 'Bestellingen',
+    'index.export_btn' => 'Exporteer bestellingen',
+    'index.save_search.title' => 'Zoekopdracht opslaan',
+    'index.save_search.btn' => 'Opslaan',
+    'index.all_orders' => 'Alle bestellingen',
+    'index.search_placeholder' => 'Zoeken...',
+    'index.update_status.btn' => 'Status aanpassen',
+    'index.update_status.title' => 'Status aanpassen',
+    'index.delete_saved_search.title' => 'Opgeslagen zoekopdracht verwijderen',
+    'index.delete_saved_search.confirm' => 'Ben je zeker dat je deze opgeslagen zoekopdracht wil verwijderen?',
+    'index.delete_saved_search.btn' => 'Verwijderen bevestigen',
+    'update_status.btn' => 'Status aanpassen',
+    'update_status.title' => 'Status aanpassen',
+    'show.title' => 'Bestelling #:id',
+];
