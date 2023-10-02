805273413e1b585c36902547da70c580f19baf78
Timmy D'Hooghe <timmy@web-app.be>
2023-10-03 01:02:23 +0200

    Dutch translations

---
+++ packages/admin/resources/lang/nl/licensing.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'banner.unlicensed.text' => "Oeps! Het lijkt erop dat je geen licentie hebt voor het gebruik van de Lunar Hub, of je licentie is ongeldig en moet hersteld worden.",
+    'banner.unlicensed.btn_text' => 'Neem een licentie',
+];
