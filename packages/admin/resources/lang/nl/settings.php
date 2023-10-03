0595ff89bd7022d1dc73651309cbdeb4423398b5
Timmy D'Hooghe <timmy@web-app.be>
2023-10-03 02:55:14 +0200

    Dutch translations

---
+++ packages/admin/resources/lang/nl/settings.php
@@ -0,0 +1,181 @@
+<?php
+
+return [
+    /**
+     * Channels.
+     */
+    'channels.index.title' => 'Verkoopskanalen',
+    'channels.index.create_btn' => 'Nieuw kanaal',
+    'channels.index.table_row_action_text' => 'Kanaal aanpassen',
+    /**
+     * Channels show page.
+     */
+    'channels.show.title' => 'Verkoopskanaal aanpassen',
+    /**
+     * Channels create page.
+     */
+    'channels.create.title' => 'Nieuw verkoopskanaal',
+    /**
+     * Settings layout.
+     */
+    'layout.menu_btn' => 'Instellingen menu',
+    /**
+     * Staff listing page.
+     */
+    'staff.index.title' => 'Medewerkers',
+    'staff.index.search_placeholder' => 'Medewerker zoeken',
+    'staff.index.active_filter' => 'Toon inactieve',
+    'staff.index.create_btn' => 'Medewerker toevoegen',
+    'staff.index.table_row_action_text' => 'Medewerker aanpassen',
+    /**
+     * Staff show page.
+     */
+    'staff.show.title' => 'Medewerker aanpassen',
+    'staff.show.delete_btn' => 'Account uitschakelen',
+    'staff.show.delete_title' => 'Medewerker verwijderen',
+    'staff.show.restore_title' => 'Medewerker herstellen',
+    /**
+     * Staff create page.
+     */
+    'staff.create.title' => 'Nieuwe medewerker',
+    /**
+     * Staff form.
+     */
+    'staff.form.create_btn' => 'Medewerker aanmaken',
+    'staff.form.update_btn' => 'Medewerker aanpassen',
+    'staff.form.permissions_heading' => 'Rechten',
+    'staff.form.permissions_description' => 'Beheer de individuele rechten van deze medewerker.',
+    'staff.form.admin_message' => 'Een admin gebruiker heeft alle rechten.',
+    'staff.form.danger_zone.label' => 'Medewerker verwijderen',
+    'staff.form.danger_zone.delete_strapline' => 'Een medewerker verwijderen ontneemt alle toegang tot de hub, je kan dit later ongedaan maken.',
+    'staff.form.danger_zone.restore_strapline' => 'Herstel de account van deze medewerker zodat die terug toegang krijgt tot de hub.',
+    'staff.form.danger_zone.own_account' => 'Wanneer je je eigen account verwijderd word je onmiddellijk afgemeld.',
+
+    /**
+     * Addons listing page.
+     */
+    'addons.index.title' => 'Plugins',
+    'addons.index.table_row_action_text' => 'Bekijk',
+    /**
+     * Addons show page.
+     */
+    'addons.show.title' => 'Plugin',
+    /*
+     * Languages listing page.
+     */
+    'languages.index.title' => 'Talen',
+    'languages.index.create_btn' => 'Taal aanmaken',
+    'languages.index.table_row_action_text' => 'Taal aanpassen',
+    /**
+     * Languages create page.
+     */
+    'languages.create.title' => 'Taal aanmaken',
+    /**
+     * Languages show page.
+     */
+    'languages.show.title' => 'Taal aanpassen',
+    /**
+     * Language form.
+     */
+    'languages.form.create_btn' => 'Taal aanmaken',
+    'languages.form.update_btn' => 'Taal aanpassen',
+    'languages.form.default_instructions' => 'Stel in of dit de standaard taal is, dit zal de huidige standaard overschrijven.',
+    /**
+     * Currencies table.
+     */
+    'currencies.index.title' => 'Valuta',
+    'currencies.index.table_row_action_text' => 'Wijzig',
+    'currencies.index.no_results' => 'Je hebt momenteel geen valuta in het systeem.',
+    /**
+     * Currency show page.
+     */
+    'currencies.show.title' => 'Valuta wijzigen',
+    /**
+     * Currency create page.
+     */
+    'currencies.create.title' => 'Valuta aanmaken',
+    'currencies.index.create_currency_btn' => 'Valuta aanmaken',
+    /**
+     * Currency form.
+     */
+    'currencies.form.update_btn' => 'Valuta aanpassen',
+    'currencies.form.create_btn' => 'Valuta aanmaken',
+    'currencies.form.notify.created' => 'Valuta aangemaakt',
+    'currencies.form.format_help_text' => [
+        'Dit staat je toe te kiezen in welk formaat de prijs velden je valuta moeten weergeven.',
+        'Bij weergave zal Lunar <code>{value}</code> vervangen door de geformatteerde prijs. Bvb. <code>€{value}</code>.',
+        'Je moet altijd <code>{value}</code> toevoegen om dit goed te doen werken.',
+    ],
+    /**
+     * Attributes.
+     */
+    'attributes.index.title' => 'Attributen',
+    'attributes.show.title' => ':type attributen aanpassen',
+    'attributes.show.locked' => 'Dit attribuut is vereist voor een goede werking van het systeem en kan daarom niet aangepast worden.',
+    'attributes.create.title' => 'Attribuut aanmaken',
+    'attributes.form.update_btn' => 'Attribuut aanpassen',
+    'attributes.form.create_btn' => 'Attribuut aanmaken',
+    'attributes.form.notify.created' => 'Attribuut toegevoegd',
+    /**
+     * Tags.
+     */
+    'tags.show.title' => 'Label aanpassen',
+    'tags.index.title' => 'Labels',
+    'tags.index.table_row_action_text' => 'Aanpassen',
+    'tags.form.update_btn' => 'Label aanpassen',
+    'tags.form.create_btn' => 'Label aanmaken',
+    'tags.form.notify.updated' => 'Label aangepast',
+    /**
+     * Activity log page.
+     */
+    'activity_log.index.title' => 'Activiteitlog',
+    /*
+     * Product Options
+     */
+    'product.options.index.title' => 'Opties',
+    'product.options.index.create_btn' => 'Optie aanmaken',
+    'product.options.index.table_row_action_text' => 'Optie aanpassen',
+    /**
+     * Taxes.
+     */
+    'taxes.tax-zones.index.title' => 'BTW tarieven',
+    'taxes.tax-zones.confirm_delete.title' => 'Verwijdering bevestigen',
+    'taxes.tax-zones.confirm_delete.message' => 'Weet je zeker dat je dit BTW tarief wil verwijderen? Dit zou tot gegevensverlies kunnen leiden.',
+    'taxes.tax-zones.customer_groups.title' => 'Beperk tot klantengroepen',
+    'taxes.tax-zones.customer_groups.instructions' => 'Selecteer tot welke klantengroepen je dit BTW tarief wil beperken. Laat het selectievakje uitgeschakeld om geen beperkingen op te leggen.',
+    'taxes.tax-zones.create_title' => 'BTW tarief aanmaken',
+    'taxes.tax-zones.create_btn' => 'BTW tarief aanmaken',
+    'taxes.tax-zones.delete_btn' => 'BTW tarief verwijderen',
+    'taxes.tax-zones.index.table_row_action_text' => 'Beheren',
+    'taxes.tax-classes.index.title' => 'BTW tarieven',
+    'taxes.tax-classes.index.create.title' => 'BTW tarief aanmaken',
+    'taxes.tax-classes.index.update.title' => 'BTW tarief aanpassen',
+    'taxes.tax-classes.create_btn' => 'BTW tarief aanmaken',
+    'taxes.tax-zones.price_display.label' => 'Prijsweergave',
+    'taxes.tax-zones.price_display.excl_tax' => 'Exclusief BTW',
+    'taxes.tax-zones.price_display.incl_tax' => 'Inclusief BTW',
+    'taxes.tax-zones.zone_type.countries' => 'Beperk tot landen',
+    'taxes.tax-zones.zone_type.states' => 'Beperk tot staten/provincies',
+    'taxes.tax-zones.zone_type.postcodes' => 'Beperk tot postcodes',
+    'taxes.tax-zones.tax_rates.title' => 'BTW tarieven',
+    'taxes.tax-zones.tax_rates.create_button' => 'BTW tarief aanmaken',
+    'taxes.tax-zones.save_btn' => 'BTW tarief opslaan',
+    'taxes.tax-classes.index.delete_message' => 'Weet je het zeker? Dit zou tot gegevensverlies kunnen leiden.',
+    'taxes.tax-classes.index.delete_message_disabled' => 'Je kan geen BTW tarief verwijderen dat gelinkt is aan product varianten',
+    'taxes.tax-classes.index.delete_message_default' => 'Je moet een nieuwe standaard kiezen voor je deze verwijdert',
+    /**
+     * Customer Groups.
+     */
+    'customer-groups.index.title' => 'Klantengroepen',
+    'customer-groups.index.create_btn' => 'Klantengroep aanmaken',
+    'customer-groups.index.table_row_action_text' => 'Klantengroep aanpassen',
+    /**
+     * Customer Groups show page.
+     */
+    'customer-groups.show.title' => 'Klantengroep aanpassen',
+    /**
+     * Customer Groups create page.
+     */
+    'customer-groups.create.title' => 'Klantengroep aanmaken',
+    'customer-groups.form.default_instructions' => 'Stel in of deze klantengroep de standaard moet zijn',
+];
