e89344e3690ba106cffa46cc02c5cf5c004552fb
wychoong <67364036+wychoong@users.noreply.github.com>
2023-11-07 01:32:40 +0800

    Update LogsActivity.php

--- packages/core/src/Base/Traits/LogsActivity.php
+++ packages/core/src/Base/Traits/LogsActivity.php
@@ -9,6 +9,8 @@ trait LogsActivity
 {
     use SpatieLogsActivity;
 
+    public static array $logExcept = [];
+
     /**
      * Get the log options for the activity log.
      */
@@ -17,8 +19,20 @@ trait LogsActivity
         return LogOptions::defaults()
             ->useLogName('lunar')
             ->logAll()
-            ->logExcept(array_merge(['updated_at'], static::$logExcept ?? [])) /** @phpstan-ignore-line */
+            ->logExcept(array_merge(['updated_at'], static::getActivitylogExcept()))
             ->logOnlyDirty()
             ->dontSubmitEmptyLogs();
+    }
+
+    public static function addActivitylogExcept(array | string $fields)
+    {
+        $fields = Arr::wrap($fields);
+
+        static::$logExcept = array_merge(static::$logExcept, $fields);
+    }
+
+    public static function getActivitylogExcept(): array
+    {
+        return array_merge(static::$defaultLogExcept ?? [], static::$logExcept); /** @phpstan-ignore-line */
     }
 }
