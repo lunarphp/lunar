83f8c75e36f24f1158367c47fa2f15b7e057f335
Lionel Guichard <lguichard@business-first.io>
2023-12-20 14:02:19 +0100

    Move default pagination to 20, 50, 100, all

--- packages/admin/src/Support/Pages/BaseListRecords.php
+++ packages/admin/src/Support/Pages/BaseListRecords.php
@@ -3,9 +3,18 @@
 namespace Lunar\Admin\Support\Pages;
 
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Table;
 
 abstract class BaseListRecords extends ListRecords
 {
     use Concerns\ExtendsHeaderActions;
     use \Lunar\Admin\Support\Concerns\CallsHooks;
+
+    public function table(Table $table): Table
+    {
+        parent::table($table);
+
+        return $table
+            ->paginated([20, 50, 100, 'all']);
+    }
 }
