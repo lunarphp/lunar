433f1d39b2ecd9333e37eed9e18123ff8bb26b85
Alec Ritson <hello@itsalec.co.uk>
2023-12-21 11:01:06 +0000

    WIP

--- packages/admin/src/Filament/Resources/OrderResource/Pages/ManageOrder.php
+++ packages/admin/src/Filament/Resources/OrderResource/Pages/ManageOrder.php
@@ -24,6 +24,7 @@ use Livewire\Attributes\Computed;
 use Lunar\Admin\Filament\Resources\CustomerResource;
 use Lunar\Admin\Filament\Resources\OrderResource;
 use Lunar\Admin\Filament\Resources\OrderResource\Pages\Components\OrderItemsTable;
+use Lunar\Admin\Support\Actions\Orders\UpdateStatusAction;
 use Lunar\Admin\Support\ActivityLog\Concerns\CanDispatchActivityUpdated;
 use Lunar\Admin\Support\Infolists\Components\Alert;
 use Lunar\Admin\Support\Infolists\Components\Livewire;
@@ -631,32 +632,13 @@ class ManageOrder extends ViewRecord
         return [
             $this->getCaptureAction(),
             $this->getRefundAction(),
-            Actions\Action::make('update_status')
-                ->label(__('lunarpanel::order.action.update_status.label'))
-                ->form(fn () => [
-                    Forms\Components\Select::make('status')
-                        ->label(__('lunarpanel::order.form.status.label'))
-                        ->default(fn ($record) => $record->status)
-                        ->options(fn () => collect(config('lunar.orders.statuses', []))
-                            ->mapWithKeys(fn ($data, $status) => [$status => $data['label']]))
-                        ->required(),
-                    Forms\Components\Textarea::make('additional_content'),
-                    Forms\Components\Section::make('mailers')
-                        ->heading('Mailers')
-                        ->schema([
-                            Forms\Components\Placeholder::make('empty_text')->label('')
-                                ->hidden(false)
-                                ->content('Select a status to see available mailers'),
-                        ])
-                        ->compact(),
-                ])
-                ->modalWidth('lg')
-                ->slideOver()
-                ->action(fn ($record, $data) => $record
-                    ->update([
-                        'status' => $data['status'],
-                    ]))
-                ->after(fn () => $this->dispatchActivityUpdated() && Notification::make()->title(__('lunarpanel::order.action.update_status.notification'))->success()->send()),
+            UpdateStatusAction::make('update_status')
+                ->after(
+                    function () {
+                        $this->dispatchActivityUpdated();
+                        Notification::make()->title(__('lunarpanel::order.action.update_status.notification'))->success()->send();
+                    }
+                ),
             Actions\Action::make('download_pdf')
                 ->label(__('lunarpanel::order.action.download_order_pdf.label'))
                 ->action(function ($record) {
---
+++ packages/admin/src/Support/Actions/Orders/UpdateStatusAction.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Lunar\Admin\Support\Actions\Orders;
+
+use Filament\Actions\Action;
+use Filament\Forms;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Lunar\Admin\Support\Forms\Components\MailerCheckbox;
+use Lunar\Models\Order;
+
+class UpdateStatusAction extends Action
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->label(
+            __('lunarpanel::order.action.update_status.label')
+        );
+
+        $this->modalWidth('lg');
+        $this->slideOver();
+
+        $this->form(fn () => [
+            Forms\Components\Select::make('status')
+                ->label(__('lunarpanel::order.form.status.label'))
+                ->default(fn ($record) => $record->status)
+                ->options(fn () => collect(config('lunar.orders.statuses', []))
+                    ->mapWithKeys(fn ($data, $status) => [$status => $data['label']]))
+                ->required()
+                ->live(),
+            Forms\Components\Textarea::make('additional_content'),
+            MailerCheckbox::make('mailers')->options(function (Forms\Get $get) {
+                $mailers = config('lunar.orders.statuses.'.$get('status').'.mailers', []);
+                return collect($mailers)->mapWithKeys(function ($mailer) {
+                    return [
+                        $mailer => Str::title(
+                            Str::snake(class_basename($mailer), ' ')
+                        ),
+                    ];
+                });
+            })->live(),
+            Forms\Components\CheckboxList::make('email_addresses')
+                ->hidden(function (Forms\Get $get) {
+                    return !count($get('mailers') ?: []);
+                })->options(function (Order $record) {
+                    return collect([
+                        'billing' => $record->billingAddress->contact_email,
+                        'shipping' => $record->shippingAddress->contact_email,
+                    ])->filter()->toArray();
+                })->descriptions([
+                    'billing' => 'Billing Email',
+                    'shipping' => 'Shipping Email',
+                ]),
+            Forms\Components\TextInput::make('additional_email')->hidden(function (Forms\Get $get) {
+                return !count($get('mailers') ?: []);
+            })
+        ]);
+
+        $this->action(
+            function (Order $record, array $data) {
+
+                $record->update([
+                    'status' => $data['status'],
+                ]);
+
+                $emails = collect(
+                    [...$data['email_addresses'],$data['additional_email']]
+                )->filter()->unique();
+
+                foreach ($data['mailers'] ?? [] as $mailerClass) {
+                    $mailable = new $mailerClass($record, $data['additional_content']);
+                    foreach ($emails as $email) {
+                        Mail::to($email)
+                            ->queue($mailable);
+
+                        $storedPath = 'orders/activity/' . Str::random() . '.html';
+
+                        Storage::put(
+                            $storedPath,
+                            $mailable->render()
+                        );
+
+                        activity()
+                            ->causedBy(auth()->user())
+                            ->performedOn($record)
+                            ->event('email-notification')
+                            ->withProperties([
+                                'template' => $storedPath,
+                                'email' => $email,
+                                'mailer' => $mailerClass,
+                            ])->log('email-notification');
+                    }
+
+
+                }
+            }
+        );
+    }
+
+}
+
+//Actions\Action::make('update_status')
+//    ->label(__('lunarpanel::order.action.update_status.label'))
+//    ->form(fn () => [
+//        Forms\Components\Select::make('status')
+//            ->label(__('lunarpanel::order.form.status.label'))
+//            ->default(fn ($record) => $record->status)
+//            ->options(fn () => collect(config('lunar.orders.statuses', []))
+//                ->mapWithKeys(fn ($data, $status) => [$status => $data['label']]))
+//            ->required()
+//            ->live(),
+//        Forms\Components\Textarea::make('additional_content'),
+
+//    ])
+//    ->modalWidth('lg')
+//    ->slideOver()
+
+//    ->after(fn () => $this->dispatchActivityUpdated() && Notification::make()->title(__('lunarpanel::order.action.update_status.notification'))->success()->send())
