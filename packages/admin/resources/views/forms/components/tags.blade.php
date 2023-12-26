d127fea5d0553cb21c450e4a22bc61c377ea6bf8
Lionel Guichard <lguichard@business-first.io>
2023-12-26 18:05:31 +0100

    Revert waiting feedback

--- packages/admin/resources/views/forms/components/tags.blade.php
+++ packages/admin/resources/views/forms/components/tags.blade.php
@@ -1,7 +1,10 @@
-<x-filament::fieldset
-      :label="$getLabel()"
->
-      @livewire('lunar.admin.livewire.components.tags', [
-          'taggable' => $getRecord()
-      ])
-</x-filament::fieldset>
+<div>
+    <label class="fi-fo-field-wrp-label inline-flex items-center gap-x-3">
+        <span class="text-sm font-medium leading-6 text-gray-950 dark:text-white">
+            {{ $getLabel() }}
+        </span>
+    </label>
+    @livewire('lunar.admin.livewire.components.tags', [
+        'taggable' => $getRecord()
+    ])
+</div>
