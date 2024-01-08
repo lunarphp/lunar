2074f346adcefd3d44df9b02afd9dfa509dd9c29
Alec Ritson <hello@itsalec.co.uk>
2024-01-08 14:43:33 +0000

    Add initial discount listing (#1455)
    Co-authored-by: Glenn Jacobs <glenn@neondigital.co.uk>

--- packages/admin/src/LunarPanelManager.php
+++ packages/admin/src/LunarPanelManager.php
@@ -7,7 +7,6 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Navigation\NavigationGroup;
-use Filament\Navigation\NavigationItem;
 use Filament\Panel;
 use Filament\Support\Colors\Color;
 use Filament\Support\Facades\FilamentColor;
@@ -51,6 +50,7 @@ class LunarPanelManager
         Resources\CurrencyResource::class,
         Resources\CustomerGroupResource::class,
         Resources\CustomerResource::class,
+        Resources\DiscountResource::class,
         Resources\LanguageResource::class,
         Resources\OrderResource::class,
         Resources\ProductOptionResource::class,
@@ -111,6 +111,7 @@ class LunarPanelManager
             'lunar::customers' => 'lucide-users',
             'lunar::customer-groups' => 'lucide-users',
             'lunar::dashboard' => 'lucide-bar-chart-big',
+            'lunar::discounts' => 'lucide-percent-circle',
             'lunar::languages' => 'lucide-languages',
             'lunar::media' => 'lucide-image',
             'lunar::orders' => 'lucide-inbox',
@@ -220,15 +221,7 @@ class LunarPanelManager
                 NavigationGroup::make()
                     ->label('Settings')
                     ->collapsed(),
-            ])
-            ->navigationItems([
-                NavigationItem::make('Discounts')
-                    ->url('#')
-                    ->icon('lucide-percent-circle')
-                    ->group('Sales')
-                    ->sort(3),
-            ])
-            ->sidebarCollapsibleOnDesktop();
+            ])->sidebarCollapsibleOnDesktop();
     }
 
     public function registerExtension(BaseExtension|ResourceExtension $extension, string $pageClass): self
