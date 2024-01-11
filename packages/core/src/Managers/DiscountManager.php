de7aad1aeb900e2ab75609ff3979db0af342ae6d
Alec Ritson <hello@itsalec.co.uk>
2024-01-11 09:43:36 +0000

    Pint fixes

--- packages/core/src/Managers/DiscountManager.php
+++ packages/core/src/Managers/DiscountManager.php
@@ -138,12 +138,12 @@ class DiscountManager implements DiscountManagerInterface
                 function ($query, $value) {
                     return $query->where(function ($query) use ($value) {
                         return $query->where(fn ($query) => $query->products(
-                                    $value->lines->pluck('purchasable.product_id')->filter()->values()
-                                )
-                            )
+                            $value->lines->pluck('purchasable.product_id')->filter()->values()
+                        )
+                        )
                             ->orWhere(fn ($query) => $query->productVariants(
-                                    $value->lines->pluck('purchasable.id')->filter()->values()
-                                )
+                                $value->lines->pluck('purchasable.id')->filter()->values()
+                            )
                             );
                     });
                 }
