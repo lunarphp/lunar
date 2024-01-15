1833935b36e07acc9b2041eba50b539e0feb8d09
Alec Ritson <hello@itsalec.co.uk>
2024-01-15 12:11:55 +0000

    shipping updates

--- tests/shipping/Unit/Drivers/ShippingMethods/ShipByTest.php
+++ tests/shipping/Unit/Drivers/ShippingMethods/ShipByTest.php
@@ -85,6 +85,67 @@ test('can get shipping option by cart total', function () {
     expect($shippingOption->price->value)->toEqual(500);
 });
 
+test('can get shipping option by cart total when prices include tax', function () {
+
+    \Illuminate\Support\Facades\Config::set('lunar.pricing.stored_inclusive_of_tax', true);
+
+    $currency = Currency::factory()->create([
+        'default' => true,
+    ]);
+
+    TaxClass::factory()->create([
+        'default' => true,
+    ]);
+
+    $shippingZone = ShippingZone::factory()->create([
+        'type' => 'countries',
+    ]);
+
+    $shippingMethod = ShippingMethod::factory()->create([
+        'driver' => 'ship-by',
+        'data' => [
+            'charge_by' => 'cart_total',
+        ],
+    ]);
+
+    $shippingRate = \Lunar\Shipping\Models\ShippingRate::factory()
+        ->create([
+            'shipping_method_id' => $shippingMethod->id,
+            'shipping_zone_id' => $shippingZone->id,
+        ]);
+
+    $shippingRate->prices()->createMany([
+        [
+            'price' => 1000,
+            'tier' => 1,
+            'currency_id' => $currency->id,
+        ],
+        [
+            'price' => 500,
+            'tier' => 700,
+            'currency_id' => $currency->id,
+        ],
+    ]);
+
+    expect($shippingRate->prices)->toHaveCount(2);
+
+    $cart = $this->createCart($currency, 700);
+
+    $driver = new ShipBy();
+
+    $request = new ShippingOptionRequest(
+        shippingRate: $shippingRate,
+        cart: $cart,
+    );
+
+    $shippingOption = $driver->resolve($request);
+
+    expect($shippingOption)->toBeInstanceOf(ShippingOption::class);
+
+    expect($shippingOption->price->value)->toEqual(500);
+
+})->group('thisone');
+
 test('can get shipping option if outside tier without default price', function () {
     // Boom.
     $currency = Currency::factory()->create([
