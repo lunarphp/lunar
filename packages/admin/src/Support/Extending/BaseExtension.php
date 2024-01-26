9d3e6d24cb62df8f41c7da1c27099f6b5a90896e
Alec Ritson <hello@itsalec.co.uk>
2024-01-26 15:08:53 +0000

    Update BaseExtension.php

--- packages/admin/src/Support/Extending/BaseExtension.php
+++ packages/admin/src/Support/Extending/BaseExtension.php
@@ -2,6 +2,7 @@
 
 namespace Lunar\Admin\Support\Extending;
 
+use Filament\Forms\Form;
 use Filament\Tables\Table;
 
 abstract class BaseExtension
@@ -10,9 +11,13 @@ abstract class BaseExtension
     {
         return $actions;
     }
-
     public function extendTable(Table $table): Table
     {
         return $table;
+    }
+
+    public function extendForm(Form $form): Form
+    {
+        return $form;
     }
 }
