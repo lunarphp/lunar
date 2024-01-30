4c356f027cbe1c6be73a99d26c85290373ab1a37
Stéphane Bour <sbour@welcoming.com>
2024-01-30 15:59:36 +0100

    Add french language for admin (#1489)

---
+++ packages/admin/resources/lang/fr/licensing.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'banner.unlicensed.text' => "Oups ! Il semble que vous n'ayez pas de licence pour utiliser le Lunar Hub, ou qu'elle soit invalide et nécessite une correction.",
+    'banner.unlicensed.btn_text' => 'Obtenir une licence',
+];
