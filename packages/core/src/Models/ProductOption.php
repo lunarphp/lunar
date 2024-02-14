63b65a1458b775c69af22ee6819842a62965fb3b
glennjacobs <glennjacobs@users.noreply.github.com>
2024-02-14 23:48:31 +0000

    chore: fix code style

--- packages/core/src/Models/ProductOption.php
+++ packages/core/src/Models/ProductOption.php
@@ -4,7 +4,6 @@ namespace Lunar\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\AsCollection;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
