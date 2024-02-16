704ea109d45aa93954d32b6de09b7018cb2cbb55
Glenn Jacobs <glenn@neondigital.co.uk>
2024-02-16 10:01:48 +0000

    Update BaseListRecords.php

--- packages/admin/src/Support/Pages/BaseListRecords.php
+++ packages/admin/src/Support/Pages/BaseListRecords.php
@@ -33,11 +33,17 @@ abstract class BaseListRecords extends ListRecords
                 fn ($result) => str_replace(static::getModel().'::', '', $result)
             );
 
+            $placeholders = implode(',', array_fill(0, count($ids), '?'));
+
             $query->whereIn(
                 'id',
                 $ids
-            )
-                ->orderByRaw('FIELD(id, '.$ids->implode(',').')'); // TODO: Only supports MySQL
+            );
+
+            $query->when(
+                ! $ids->isEmpty(),
+                fn ($query) => $query->orderByRaw("field(id, {$placeholders})", $ids->toArray()) // TODO: Only supports MySQL
+            );
         }
 
         return $query;
