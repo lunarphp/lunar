faf9d7f970e1265691ef15b743772f010763449a
Glenn Jacobs <glenn@neondigital.co.uk>
2024-02-22 15:08:11 +0000

    Create RelationPageExtensionTest.php

---
+++ tests/admin/Feature/Support/Extending/RelationPageExtensionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+use Lunar\Admin\Filament\Resources\ProductResource\Pages\ManageProductMedia;
+use Lunar\Admin\Support\Facades\LunarPanel;
+
+uses(\Lunar\Tests\Admin\Feature\Filament\TestCase::class)
+    ->group('extending');
+
+it('can customise page headings', function () {
+    $class = new class extends \Lunar\Admin\Support\Extending\RelationPageExtension
+    {
+        public function heading($title, Model $record): string
+        {
+            return 'New Heading';
+        }
+
+        public function subheading($title, Model $record): ?string
+        {
+            return 'New Subheading';
+        }
+    };
+
+    \Lunar\Models\Language::factory()->create();
+    $product = \Lunar\Models\Product::factory()->create();
+
+    LunarPanel::registerExtension($class, ManageProductMedia::class);
+
+    $this->asStaff(admin: true);
+
+    \Livewire\Livewire::test(ManageProductMedia::class, [
+        'record' => $product->getRouteKey(),
+    ])
+    ->assertSee('New Heading')
+    ->assertSee('New Subheading');
+});
