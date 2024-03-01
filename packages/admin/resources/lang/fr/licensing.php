7216508b891f6ae3ec2477c4e6437b3142b52c12
Alec Ritson <hello@itsalec.co.uk>
2024-03-01 10:49:24 +0000

    Remove translations

--- packages/admin/resources/lang/fr/licensing.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'banner.unlicensed.text' => "Oups ! Il semble que vous n'ayez pas de licence pour utiliser le Lunar Hub, ou qu'elle soit invalide et nécessite une correction.",
-    'banner.unlicensed.btn_text' => 'Obtenir une licence',
-];
