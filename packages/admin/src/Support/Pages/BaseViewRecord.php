00666f197d363e3d29fc896c3f3ee65da575a7f6
alecritson <alecritson@users.noreply.github.com>
2024-03-14 07:47:00 +0000

    chore: fix code style

--- packages/admin/src/Support/Pages/BaseViewRecord.php
+++ packages/admin/src/Support/Pages/BaseViewRecord.php
@@ -7,7 +7,7 @@ use Filament\Resources\Pages\ViewRecord;
 abstract class BaseViewRecord extends ViewRecord
 {
     use Concerns\ExtendsHeaderActions;
-    use Concerns\ExtendsHeadings;
     use Concerns\ExtendsHeaderWidgets;
+    use Concerns\ExtendsHeadings;
     use \Lunar\Admin\Support\Concerns\CallsHooks;
 }
