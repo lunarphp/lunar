95a4988da8f091c2bff324c7b2a7014555c3c70b
Alec Ritson <hello@itsalec.co.uk>
2024-03-18 12:57:40 +0000

    Update tests

--- tests/core/Unit/Base/ModelManifestTest.php
+++ tests/core/Unit/Base/ModelManifestTest.php
@@ -1,11 +1,10 @@
 <?php
 
-uses(\Lunar\Tests\Core\TestCase::class);
+uses(\Lunar\Tests\Core\TestCase::class)->group('model_extending');
 
 use Lunar\Base\ModelManifestInterface;
 use Lunar\Facades\ModelManifest;
 use Lunar\Models\Product;
-use Lunar\Models\ProductOption;
 
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
@@ -15,45 +14,67 @@ test('can instantiate class', function () {
     expect($manifest)->toBeInstanceOf(\Lunar\Base\ModelManifest::class);
 });
 
-test('can register models', function () {
-    expect(ModelManifest::getRegisteredModels())->toHaveCount(0);
+test('can add model', function () {
+    ModelManifest::add(
+        \Lunar\Models\Contracts\Product::class,
+        \Lunar\Tests\Core\Stubs\Models\Product::class,
+    );
 
-    ModelManifest::register(collect([
-        Product::class => \Lunar\Tests\Core\Stubs\Models\Product::class,
-        ProductOption::class => \Lunar\Tests\Core\Stubs\Models\ProductOption::class,
-    ]));
-
-    expect(ModelManifest::getRegisteredModels())->toHaveCount(2);
+    expect(Product::modelClass())->toBe(\Lunar\Tests\Core\Stubs\Models\Product::class);
 });
 
-test('can get registered model from base model', function () {
-    ModelManifest::register(collect([
-        Product::class => \Lunar\Tests\Core\Stubs\Models\Product::class,
-    ]));
+test('can replace model', function () {
+    ModelManifest::replace(
+        \Lunar\Models\Contracts\Product::class,
+        \Lunar\Tests\Core\Stubs\Models\Product::class,
+    );
 
-    $model = ModelManifest::getRegisteredModel(Product::class);
-
-    expect($model)->toBeInstanceOf(\Lunar\Tests\Core\Stubs\Models\Product::class);
+    expect(Product::modelClass())->toBe(\Lunar\Tests\Core\Stubs\Models\Product::class);
 });
 
-test('can get morph class base model', function () {
-    ModelManifest::register(collect([
-        Product::class => \Lunar\Tests\Core\Stubs\Models\Product::class,
-    ]));
+test('can get registered model', function () {
+    expect(
+        ModelManifest::get(\Lunar\Models\Contracts\Product::class)
+    )->toBe(Product::class);
 
-    $customModels = ModelManifest::getRegisteredModels()->flip();
+    ModelManifest::replace(
+        \Lunar\Models\Contracts\Product::class,
+        \Lunar\Tests\Core\Stubs\Models\Product::class,
+    );
 
-    expect(value: $customModels->get(\Lunar\Tests\Core\Stubs\Models\Product::class))->toEqual(expected: Product::class);
+    expect(
+        ModelManifest::get(\Lunar\Models\Contracts\Product::class)
+    )->toBe(\Lunar\Tests\Core\Stubs\Models\Product::class);
 });
 
-test('can get list of registered base models', function () {
-    ModelManifest::register(collect([
-        Product::class => \Lunar\Tests\Core\Stubs\Models\Product::class,
-        ProductOption::class => \Lunar\Tests\Core\Stubs\Models\ProductOption::class,
-    ]));
+test('can guess contract class', function () {
+    expect(
+        ModelManifest::guessContractClass(Product::class)
+    )->toBe(\Lunar\Models\Contracts\Product::class);
+});
 
-    expect(ModelManifest::getBaseModelClasses())->toEqual(collect([
-        Product::class,
-        ProductOption::class,
-    ]));
+test('can guess model class', function () {
+    expect(
+        ModelManifest::guessModelClass(\Lunar\Models\Contracts\Product::class)
+    )->toBe(Product::class);
+});
+
+test('can detect lunar model', function () {
+    expect(
+        ModelManifest::isLunarModel((new Product()))
+    )->toBeTrue()
+        ->and(
+            ModelManifest::isLunarModel((new \Lunar\Tests\Core\Stubs\Models\Product()))
+        )->toBeFalse();
+});
+
+test('can detect table name', function () {
+    ModelManifest::replace(
+        \Lunar\Models\Contracts\Product::class,
+        \Lunar\Tests\Core\Stubs\Models\CustomProduct::class,
+    );
+
+    expect(
+        ModelManifest::getTable(new \Lunar\Tests\Core\Stubs\Models\CustomProduct())
+    )->toBe('products');
 });
