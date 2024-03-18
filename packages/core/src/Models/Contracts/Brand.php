d948fd59586a934bf019d126b3d930bb9984d642
Alec Ritson <hello@itsalec.co.uk>
2024-03-18 14:12:07 +0000

    Fix tests

--- packages/core/src/Models/Contracts/Brand.php
+++ packages/core/src/Models/Contracts/Brand.php
@@ -3,14 +3,13 @@
 namespace Lunar\Models\Contracts;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 interface Brand
 {
     /**
      * Get the mapped attributes relation.
      */
-    public function mappedAttributes(): MorphToMany;
+    public function mappedAttributes(): HasMany;
 
     /**
      * Return the product relationship.
