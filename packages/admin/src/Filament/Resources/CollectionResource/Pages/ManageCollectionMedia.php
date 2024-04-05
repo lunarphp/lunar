a4223d2473c3f66894ba238d483981d210d5022a
lguichard <lguichard@business-first.io>
2024-04-06 01:04:34 +0200

    Fix accidentally purge

--- packages/admin/src/Filament/Resources/CollectionResource/Pages/ManageCollectionMedia.php
+++ packages/admin/src/Filament/Resources/CollectionResource/Pages/ManageCollectionMedia.php
@@ -8,4 +8,13 @@ use Lunar\Admin\Support\Resources\Pages\ManageMediasRelatedRecords;
 class ManageCollectionMedia extends ManageMediasRelatedRecords
 {
     protected static string $resource = CollectionResource::class;
+
+    public function getBreadcrumbs(): array
+    {
+        $crumbs = static::getResource()::getCollectionBreadcrumbs($this->getRecord());
+
+        $crumbs[] = $this->getBreadcrumb();
+
+        return $crumbs;
+    }
 }
