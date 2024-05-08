92b2616c5d46b25b0bbfa0d7ca0fca3b5bc29b4c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-08 17:30:53 +0200

    test that purchasable non-eloquent classes can be added to order

--- packages/core/tests/Unit/Models/OrderLineTest.php
+++ packages/core/tests/Unit/Models/OrderLineTest.php
@@ -3,6 +3,8 @@
 namespace Lunar\Tests\Unit\Models;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Lunar\DataTypes\Price;
+use Lunar\DataTypes\ShippingOption;
 use Lunar\Exceptions\NonPurchasableItemException;
 use Lunar\Models\CartLine;
 use Lunar\Models\Channel;
@@ -10,6 +12,8 @@ use Lunar\Models\Currency;
 use Lunar\Models\Order;
 use Lunar\Models\OrderLine;
 use Lunar\Models\ProductVariant;
+use Lunar\Models\TaxClass;
+use Lunar\Tests\Stubs\DataTypes\TestPurchasable;
 use Lunar\Tests\TestCase;
 
 /**
@@ -90,5 +94,73 @@ class OrderLineTest extends TestCase
         OrderLine::factory()->create($data);
 
         $this->assertDatabaseMissing((new CartLine())->getTable(), $data);
+    }
+
+    /** @test */
+    public function purchasable_non_eloquent_models_can_be_added_to_an_order()
+    {
+        $order = Order::factory()->create();
+
+        $currency = Currency::factory()->create([
+            'default' => true,
+        ]);
+
+        $taxClass = TaxClass::factory()->create();
+
+        $shippingOption = new ShippingOption(
+            name: 'Basic Delivery',
+            description: 'Basic Delivery',
+            identifier: 'BASDEL',
+            price: new Price(500, $currency, 1),
+            taxClass: $taxClass
+        );
+
+        $data = [
+            'order_id' => $order->id,
+            'quantity' => 1,
+            'type' => $shippingOption->getType(),
+            'purchasable_type' => ShippingOption::class,
+            'purchasable_id' => $shippingOption->getIdentifier(),
+            'unit_price' => $shippingOption->getPrice()->value,
+            'unit_quantity' => $shippingOption->getUnitQuantity(),
+        ];
+
+        $orderLine = OrderLine::factory()->create($data);
+
+        $this->assertDatabaseHas(
+            (new OrderLine())->getTable(),
+            $data
+        );
+
+        $this->assertEquals(5.0, $orderLine->unit_price->decimal);
+        $this->assertEquals(5.0, $orderLine->unit_price->unitDecimal);
+
+        $testPurchasable = new TestPurchasable(
+            name: 'Test Purchasable',
+            description: 'Test Purchasable',
+            identifier: 'TESTPUR',
+            price: new Price(650, $currency, 1),
+            taxClass: $taxClass
+        );
+
+        $data = [
+            'order_id' => $order->id,
+            'quantity' => 1,
+            'type' => $testPurchasable->getType(),
+            'purchasable_type' => TestPurchasable::class,
+            'purchasable_id' => $testPurchasable->getIdentifier(),
+            'unit_price' => $testPurchasable->getPrice()->value,
+            'unit_quantity' => $testPurchasable->getUnitQuantity(),
+        ];
+
+        $orderLine = OrderLine::factory()->create($data);
+
+        $this->assertDatabaseHas(
+            (new OrderLine())->getTable(),
+            $data
+        );
+
+        $this->assertEquals(6.5, $orderLine->unit_price->decimal);
+        $this->assertEquals(6.5, $orderLine->unit_price->unitDecimal);
     }
 }
