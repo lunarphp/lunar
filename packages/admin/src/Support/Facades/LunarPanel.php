c7be70625030885ccc84471324ac77841a369bd6
Andréas Lundgren <1066486+adevade@users.noreply.github.com>
2024-05-09 22:42:08 +0200

    Add PHPDoc methods to LunarPanel facade

--- packages/admin/src/Support/Facades/LunarPanel.php
+++ packages/admin/src/Support/Facades/LunarPanel.php
@@ -4,6 +4,19 @@ namespace Lunar\Admin\Support\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Lunar\Admin\LunarPanelManager register()
+ * @method static \Lunar\Admin\LunarPanelManager panel(\Closure $closure)
+ * @method static \Filament\Panel getPanel()
+ * @method static static \Lunar\Admin\LunarPanelManager extensions(array $extensions)
+ * @method static array<class-string<\Filament\Resources\Resource>> getResources()
+ * @method static array<class-string<\Filament\Pages\Page>> getPages()
+ * @method static array<class-string<\Filament\Widgets\Widget>> getWidgets()
+ * @method static \Lunar\Admin\LunarPanelManager useRoleAsAdmin(string|array $roleHandle)
+ * @method static mixed callHook(string $class, string $hookName, ...$args)
+ *
+ * @see \Lunar\Admin\LunarPanelManager
+ */
 class LunarPanel extends Facade
 {
     protected static function getFacadeAccessor(): string
