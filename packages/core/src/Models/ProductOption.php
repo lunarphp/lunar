224fef1ce8f11083533aee8a4291451a1f29e122
Glenn Jacobs <glenn@neondigital.co.uk>
2024-05-13 09:32:17 +0100

    Update ProductOption.php

--- packages/core/src/Models/ProductOption.php
+++ packages/core/src/Models/ProductOption.php
@@ -2,6 +2,7 @@
 
 namespace Lunar\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\AsCollection;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
