daa58591cdccb7f93c91b70d05e0d1b5e32bed91
Glenn Jacobs <glenn@neondigital.co.uk>
2024-05-13 09:33:54 +0100

    Update ProductOption.php

--- packages/core/src/Models/ProductOption.php
+++ packages/core/src/Models/ProductOption.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\AsCollection;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Lunar\Base\BaseModel;
 use Lunar\Base\Traits\HasMacros;
