210b0e7a58ac3ccabf7211c9f90563822a55549c
Glenn Jacobs <glenn@neondigital.co.uk>
2024-05-20 22:15:43 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,6 @@ const preset = require('./vendor/filament/filament/tailwind.config.preset')
 module.exports = {
     presets: [preset],
     content: [
-        './resources/views/**/*.blade.php',
+        './packages/admin/resources/views/**/*.blade.php',
     ],
 }
