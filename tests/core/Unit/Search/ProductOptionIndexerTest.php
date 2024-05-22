c06d30f3e4443aadc613c15352708cd5133b6717
Alec Ritson <hello@itsalec.co.uk>
2024-05-22 08:27:01 +0100

    Update ProductOptionIndexerTest.php

--- tests/core/Unit/Search/ProductOptionIndexerTest.php
+++ tests/core/Unit/Search/ProductOptionIndexerTest.php
@@ -12,6 +12,6 @@ test('can return correct searchable data', function () {
 
     $data = app(ProductOptionIndexer::class)->toSearchableArray($productOption);
 
-    expect($data['name_en'])->toEqual($productOption->name['en'])
-        ->and($data['label_en'])->toEqual($productOption->label['en']);
+    expect($data['name_en'])->toEqual($productOption->name->en)
+        ->and($data['label_en'])->toEqual($productOption->label->en);
 });
