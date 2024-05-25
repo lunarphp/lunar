65afc6eaf75b49b6c13709ad9cd8feb09d9d2f42
wychoong <67364036+wychoong@users.noreply.github.com>
2024-05-26 03:59:48 +0800

    Create 2024_05_25_100000_update_collection_group_handle_unique.php

---
+++ packages/core/database/migrations/2024_05_25_100000_update_collection_group_handle_unique.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Lunar\Base\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table($this->prefix . 'collection_groups', function (Blueprint $table) {
+            $table->dropIndex(['handle']);
+        });
+
+        Schema::table($this->prefix . 'collection_groups', function (Blueprint $table) {
+            $table->unique(['handle']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table($this->prefix . 'collection_groups', function (Blueprint $table) {
+            $table->dropUnique(['handle']);
+        });
+
+        Schema::table($this->prefix . 'collection_groups', function (Blueprint $table) {
+            $table->index(['handle']);
+        });
+    }
+};
