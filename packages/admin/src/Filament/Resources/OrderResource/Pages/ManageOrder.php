e56e10b80db7b288a3cfca6092dff456796bbce5
lguichard <lguichard@business-first.io>
2024-06-03 09:21:36 +0200

    Cleaning

--- packages/admin/src/Filament/Resources/OrderResource/Pages/ManageOrder.php
+++ packages/admin/src/Filament/Resources/OrderResource/Pages/ManageOrder.php
@@ -354,25 +354,21 @@ class ManageOrder extends BaseViewRecord
                                     ->hiddenLabel()
                                     ->getStateUsing(fn () => __('lunarpanel::order.infolist.no_additional_info.label')),
                             ] : collect($state)
-                                ->map(function ($value, $key) {
-                                    if (! is_array($value)) {
-                                        return Infolists\Components\TextEntry::make('meta_'.$key)
-                                            ->state($value)
-                                            ->label($key)
-                                            ->copyable()
-                                            ->limit(50)->tooltip(function (Infolists\Components\TextEntry $component): ?string {
-                                                $state = $component->getState();
-                                                if (strlen($state) <= $component->getCharacterLimit()) {
-                                                    return null;
-                                                }
+                                ->map(fn ($value, $key) => is_array($value) ? Infolists\Components\KeyValueEntry::make('meta_'.$key)->state($value) :
+                                    Infolists\Components\TextEntry::make('meta_'.$key)
+                                        ->state($value)
+                                        ->label($key)
+                                        ->copyable()
+                                        ->limit(50)->tooltip(function (Infolists\Components\TextEntry $component): ?string {
+                                            $state = $component->getState();
+                                            if (strlen($state) <= $component->getCharacterLimit()) {
+                                                return null;
+                                            }
 
-                                                return $state;
-                                            });
-                                    } else {
-                                        return Infolists\Components\KeyValueEntry::make('meta_'.$key)
-                                            ->state($value);
-                                    }
-                                })
+                                            return $state;
+                                        })
+
+                                )
                                 ->toArray()),
 
                     ])
