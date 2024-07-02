42a6e36d2d2b4202bb30e2112fec44982c3e04d8
Andréas Lundgren <1066486+adevade@users.noreply.github.com>
2024-07-02 09:35:16 +0200

    Convert migrations to anonymous classes (#1834)

--- packages/table-rate-shipping/database/migrations/2022_04_28_110000_create_shipping_methods_table.php
+++ packages/table-rate-shipping/database/migrations/2022_04_28_110000_create_shipping_methods_table.php
@@ -4,9 +4,9 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Lunar\Base\Migration;
 
-class CreateShippingMethodsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::create($this->prefix.'shipping_methods', function (Blueprint $table) {
             $table->bigIncrements('id');
@@ -22,8 +22,8 @@ class CreateShippingMethodsTable extends Migration
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists($this->prefix.'shipping_methods');
     }
-}
+};
