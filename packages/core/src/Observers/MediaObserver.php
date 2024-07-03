edfe9a6035a529bef774bb111cb8f5e2c7e5eb0e
Alec Ritson <hello@itsalec.co.uk>
2024-07-03 08:38:03 +0100

    Remove nested if statements

--- packages/core/src/Observers/MediaObserver.php
+++ packages/core/src/Observers/MediaObserver.php
@@ -33,34 +33,39 @@ class MediaObserver
             $owner->getMedia($media->collection_name)
                 ->reject(fn ($collectionMedia) => $collectionMedia->id == $media->id || $collectionMedia->getCustomProperty('primary') === false)
                 ->each(fn ($collectionMedia) => $collectionMedia->setCustomProperty('primary', false)->saveQuietly());
-        } else {
-            $collection = $owner->getMedia($media->collection_name)
-                ->reject(fn ($collectionMedia) => $collectionMedia->id == $media->id);
-            $primaryCollection = $collection
-                ->filter(fn ($collectionMedia) => $collectionMedia->getCustomProperty('primary') === true);
 
-            $collectionCount = $collection->count();
-            $primaryCount = $primaryCollection->count();
-
-            if ($isDelete && $collectionCount == 0) {
-                return;
-            }
-
-            if ($collectionCount == 0) {
-                $media->refresh()->setCustomProperty('primary', true)
-                    ->saveQuietly();
-            } elseif ($primaryCount == 0) {
-                $collection->first()->setCustomProperty('primary', true)
-                    ->saveQuietly();
-            } elseif ($primaryCount > 1) {
-                $first = $primaryCollection->first();
-
-                $first->setCustomProperty('primary', true)
-                    ->saveQuietly();
-
-                $primaryCollection->reject(fn ($collectionMedia) => $collectionMedia->id == $first->id)
-                    ->each(fn ($collectionMedia) => $collectionMedia->setCustomProperty('primary', false)->saveQuietly());
-            }
+            return;
         }
+
+        $collection = $owner->getMedia($media->collection_name)
+            ->reject(fn ($collectionMedia) => $collectionMedia->id == $media->id);
+        $primaryCollection = $collection
+            ->filter(fn ($collectionMedia) => $collectionMedia->getCustomProperty('primary') === true);
+
+        if ($isDelete && $collection->isEmpty()) {
+            return;
+        }
+
+        if ($collection->isEmpty()) {
+            $media->refresh()->setCustomProperty('primary', true)
+                ->saveQuietly();
+
+            return;
+        }
+
+        if ($primaryCollection->isEmpty()) {
+            $collection->first()->setCustomProperty('primary', true)
+                ->saveQuietly();
+
+            return;
+        }
+
+        $first = $primaryCollection->first();
+
+        $first->setCustomProperty('primary', true)
+            ->saveQuietly();
+
+        $primaryCollection->reject(fn ($collectionMedia) => $collectionMedia->id == $first->id)
+            ->each(fn ($collectionMedia) => $collectionMedia->setCustomProperty('primary', false)->saveQuietly());
     }
 }
