3e3d6e73aa994b815ff2020bf2a73d53311f8b92
Glenn Jacobs <glenn@neondigital.co.uk>
2024-07-04 13:30:16 +0100

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -141,10 +141,10 @@ export default defineConfig({
                         {text: 'Access Control', link: '/admin/extending/access-control'},
                         {text: 'Add-ons', link: '/admin/extending/addons'},
                         {text: 'Attributes', link: '/admin/extending/attributes'},
-                        {text: 'Order Management', link: '/admin/extending/order-management'},
-                        {text: 'Pages', link: '/admin/extending/pages'},
                         {text: 'Panel', link: '/admin/extending/panel'},
-                        {text: 'Resources', link: '/admin/extending/resources'}
+                        {text: 'Pages', link: '/admin/extending/pages'},
+                        {text: 'Resources', link: '/admin/extending/resources'},
+                        {text: 'Order Management', link: '/admin/extending/order-management'}
                     ]
                 }
             ],
