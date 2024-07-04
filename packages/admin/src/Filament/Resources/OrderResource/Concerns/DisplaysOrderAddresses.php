c8d98d74f4ab1f4b7e22f12e292d6b124abe85b0
Alec Ritson <hello@itsalec.co.uk>
2024-07-04 13:45:23 +0100

    Allow order screens to be extended (#1843)
    This PR looks to shake up the order screen so it's possible for a
    developer to extend each component to make it their own.
    
    Currently it's only possible to customise a small subset of components
    when viewing an order, however it would be useful to be able to have a
    lot more control as a developer.
    
    ## Extending `ManageOrder`
    
    ```
    \Lunar\Facades\LunarPanel::extensions([
        \Lunar\Admin\Filament\Resources\OrderResource\Pages\ManageOrder::class => ManageOrderExtension::class
    ]);
    ```
    ### Available Methods
    
    - `extendInfolistSchema(): array`
    
    - `extendInfolistAsideSchema(): array`
      - `extendCustomerEntry(): Infolists\Components\Component`
      - `extendTagsSection(): Infolists\Components\Component`
      - `extendAdditionalInfoSection(): Infolists\Components\Component`
      - `extendShippingAddressInfolist(): Infolists\Components\Component`
      - `extendBillingAddressInfolist(): Infolists\Components\Component`
      -  `extendAddressEditSchema(): array`
    
    - `exendOrderSummaryInfolist(): Infolists\Components\Section`
    - `extendOrderSummarySchema(): array` 
      - `extendOrderSummaryNewCustomerEntry(): Infolists\Components\Entry`
      - `extendOrderSummaryStatusEntry(): Infolists\Components\Entry`
      - `extendOrderSummaryReferenceEntry(): Infolists\Components\Entry`
    - `extendOrderSummaryCustomerReferenceEntry():
    Infolists\Components\Entry`
      - `extendOrderSummaryChannelEntry(): Infolists\Components\Entry`
      - `extendOrderSummaryCreatedAtEntry(): Infolists\Components\Entry`
      - `extendOrderSummaryPlacedAtEntry(): Infolists\Components\Entry`
    - `extendTimelineInfolist(): Infolists\Components\Component`
    - `extendOrderTotalsAsideSchema(): array`
      - `extendDeliveryInstructionsEntry(): Infolists\Components\TextEntry`
      - `extendOrderNotesEntry(): Infolists\Components\TextEntry`
    - `extendOrderTotalsInfolist(): Infolists\Components\Section`
    - `extendOrderTotalsSchema(): array`
      - `extendSubTotalEntry(): Infolists\Components\TextEntry`
      - `extendDiscountTotalEntry(): Infolists\Components\TextEntry`
      - `extendShippingBreakdownGroup(): Infolists\Components\Group`
      - `extendTaxBreakdownGroup(): Infolists\Components\Group`
      - `extendTotalEntry(): Infolists\Components\TextEntry`
      - `extendPaidEntry(): Infolists\Components\TextEntry`
      - `extendRefundEntry(): Infolists\Components\TextEntry`
    
    - `extendShippingInfolist(): Infolists\Components\Section`
    - `extendTransactionsInfolist(): Infolists\Components\Component`
    - `extendTransactionsRepeatableEntry():
    Infolists\Components\RepeatableEntry`
    
    ## Extending `OrderItemsTable`
    
    ```
    \Lunar\Facades\LunarPanel::extensions([
        \Lunar\Admin\Filament\Resources\OrderResource\Pages\Components\OrderItemsTable::class => OrderItemsTableExtension::class
    ]);
    ```
    ### Available Methods
    
    - `extendOrderLinesTableColumns(): array`
    - `extendTable(): Table`
    
    ---------
    
    Co-authored-by: alecritson <alecritson@users.noreply.github.com>
    Co-authored-by: glennjacobs <glennjacobs@users.noreply.github.com>
    Co-authored-by: Glenn Jacobs <glenn@neondigital.co.uk>

---
+++ packages/admin/src/Filament/Resources/OrderResource/Concerns/DisplaysOrderAddresses.php
@@ -0,0 +1,256 @@
+<?php
+
+namespace Lunar\Admin\Filament\Resources\OrderResource\Concerns;
+
+use Filament\Facades\Filament;
+use Filament\Forms;
+use Filament\Infolists;
+use Filament\Infolists\Components\Actions\Action;
+use Filament\Support\Colors\Color;
+use Filament\Support\Enums\FontWeight;
+use Illuminate\Support\Arr;
+use Lunar\Models\Country;
+use Lunar\Models\OrderAddress;
+use Lunar\Models\State;
+
+trait DisplaysOrderAddresses
+{
+    public static function getDefaultShippingAddressInfoList(): Infolists\Components\Component
+    {
+        return self::getOrderAddressInfolistSchema('shipping');
+    }
+
+    public static function getShippingAddressInfolist(): Infolists\Components\Component
+    {
+        return self::callLunarHook('extendShippingAddressInfolist', static::getDefaultShippingAddressInfoList());
+    }
+
+    public static function getDefaultBillingAddressInfoList(): Infolists\Components\Component
+    {
+        return self::getOrderAddressInfolistSchema('billing');
+    }
+
+    public static function getBillingAddressInfoList(): Infolists\Components\Component
+    {
+        return self::callLunarHook('extendBillingAddressInfolist', static::getDefaultBillingAddressInfoList());
+    }
+
+    public static function getAddressEditSchema(): array
+    {
+        return self::callLunarHook('extendAddressEditSchema', [
+            Forms\Components\Grid::make()
+                ->schema([
+                    Forms\Components\TextInput::make('first_name')
+                        ->label(__('lunarpanel::order.form.address.first_name.label'))
+                        ->autocomplete(false)
+                        ->maxLength(255)
+                        ->required(),
+                    Forms\Components\TextInput::make('last_name')
+                        ->label(__('lunarpanel::order.form.address.last_name.label'))
+                        ->autocomplete(false)
+                        ->maxLength(255),
+                ]),
+            Forms\Components\TextInput::make('company_name')
+                ->label(__('lunarpanel::order.form.address.company_name.label'))
+                ->autocomplete(false)
+                ->maxLength(255),
+            Forms\Components\Grid::make()
+                ->schema([
+                    Forms\Components\TextInput::make('contact_phone')
+                        ->label(__('lunarpanel::order.form.address.contact_phone.label'))
+                        ->autocomplete(false)
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('contact_email')
+                        ->label(__('lunarpanel::order.form.address.contact_email.label'))
+                        ->autocomplete(false)
+                        ->maxLength(255),
+                ]),
+            Forms\Components\TextInput::make('line_one')
+                ->label(__('lunarpanel::order.form.address.line_one.label'))
+                ->autocomplete(false)
+                ->maxLength(255)
+                ->required(),
+            Forms\Components\Grid::make()
+                ->schema([
+                    Forms\Components\TextInput::make('line_two')
+                        ->label(__('lunarpanel::order.form.address.line_two.label'))
+                        ->autocomplete(false)
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('line_three')
+                        ->label(__('lunarpanel::order.form.address.line_three.label'))
+                        ->autocomplete(false)
+                        ->maxLength(255),
+                ]),
+            Forms\Components\Grid::make(3)
+                ->schema([
+                    Forms\Components\TextInput::make('city')
+                        ->label(__('lunarpanel::order.form.address.city.label'))
+                        ->maxLength(255)
+                        ->autocomplete(false)
+                        ->required(),
+                    Forms\Components\TextInput::make('state')
+                        ->label(__('lunarpanel::order.form.address.state.label'))
+                        ->autocomplete('state') // to disable browser input history while keeping datalist
+                        ->datalist(fn ($get) => State::whereCountryId($get('country_id'))->pluck('name')->toArray())
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('postcode')
+                        ->label(__('lunarpanel::order.form.address.postcode.label'))
+                        ->autocomplete(false)
+                        ->maxLength(255),
+                ]),
+            Forms\Components\Select::make('country_id')
+                ->label(__('lunarpanel::order.form.address.country_id.label'))
+                ->options(fn () => Country::get()->pluck('name', 'id'))
+                ->live()
+                ->searchable()
+                ->required(),
+        ]);
+    }
+
+    public static function getOrderAddressInfolistSchema(string $type): Infolists\Components\Section
+    {
+        $sameAsShipping = fn ($record) => $type == 'billing' && static::addressesMatch($record->shippingAddress, $record->billingAddress);
+
+        $getAddress = fn ($record) => match ($type) {
+            'billing' => $record->billingAddress,
+            default => $record->shippingAddress,
+        };
+
+        return Infolists\Components\Section::make(__("lunarpanel::order.infolist.{$type}_address.label"))
+            ->statePath($type.'Address')
+            ->compact()
+            ->headerActions([
+                fn ($record) => static::getEditAddressAction($type)->hidden($sameAsShipping($record)),
+            ])
+            ->schema(fn ($record) => $sameAsShipping($record) ? [
+                Infolists\Components\TextEntry::make('billing_matches_shipping')
+                    ->hiddenLabel()
+                    ->weight(FontWeight::SemiBold)
+                    ->getStateUsing(fn () => __('lunarpanel::order.infolist.billing_matches_shipping.label')),
+
+            ] : [
+                Infolists\Components\TextEntry::make($type.'_address')
+                    ->hiddenLabel()
+                    ->listWithLineBreaks()
+                    ->getStateUsing(function ($record) use ($getAddress) {
+                        $address = $getAddress($record);
+
+                        if ($address?->id ?? false) {
+                            return collect([
+                                'company_name' => $address->company_name,
+                                'fullName' => $address->fullName,
+                                'line_one' => $address->line_one,
+                                'line_two' => $address->line_two,
+                                'line_three' => $address->line_three,
+                                'city' => $address->city,
+                                'state' => $address->state,
+                                'postcode' => $address->postcode,
+                                'country.name' => $address->country->name,
+                            ])
+                                ->filter(fn ($value, $key) => filled($value) || in_array($key, [
+                                    'fullName', 'line_one', 'postcode', 'country.name',
+                                ]))
+                                ->toArray();
+                        } else {
+                            return __('lunarpanel::order.infolist.address_not_set.label');
+                        }
+                    }),
+                Infolists\Components\TextEntry::make($type.'_phone')
+                    ->hiddenLabel()
+                    ->icon('heroicon-o-phone')
+                    ->getStateUsing(fn ($record) => $getAddress($record)?->contact_phone ?? '-')
+                    ->url(fn ($state) => $state !== '-' ? 'tel:'.$state : false)
+                    ->color(fn ($state) => $state !== '-' ? Color::Sky : null)
+                    ->iconColor(fn ($state) => $state !== '-' ? Color::Green : null),
+                Infolists\Components\TextEntry::make($type.'_email')
+                    ->hiddenLabel()
+                    ->icon('heroicon-o-envelope')
+                    ->getStateUsing(fn ($record) => $getAddress($record)?->contact_email ?? '-')
+                    ->url(fn ($state) => $state !== '-' ? 'mailto:'.$state : false)
+                    ->color(fn ($state) => $state !== '-' ? Color::Sky : null)
+                    ->iconColor(fn ($state) => $state !== '-' ? Color::Amber : null),
+            ]);
+    }
+
+    private static function addressesMatch(OrderAddress $original = null, OrderAddress $comparison = null): bool
+    {
+        if (! $original || ! $comparison) {
+            return false;
+        }
+
+        $fieldsToCheck = Arr::except(
+            $comparison->getAttributes(),
+            ['id', 'created_at', 'updated_at', 'order_id', 'type', 'meta', 'shipping_option']
+        );
+
+        // Is the same until proven otherwise
+        $isSame = true;
+        foreach ($fieldsToCheck as $field => $value) {
+            if ($original->getAttribute($field) != $value) {
+                $isSame = false;
+            }
+        }
+
+        return $isSame;
+    }
+
+    public static function getEditAddressAction(string $type): Action
+    {
+        return Action::make("edit_{$type}_address")
+            ->modalHeading(__("lunarpanel::order.infolist.{$type}_address.label"))
+            ->modalWidth('2xl')
+            ->label(__('lunarpanel::order.action.edit_address.label'))
+            ->button()
+            ->fillForm(fn ($record) => match ($type) {
+                'shipping' => $record->shippingAddress->toArray(),
+                'billing' => $record->billingAddress->toArray(),
+                default => []
+            })
+            ->form(function () {
+                return static::getAddressEditSchema();
+            })
+            ->action(function (Action $action, $record, $data) use ($type) {
+                $addressType = match ($type) {
+                    'shipping' => 'shippingAddress',
+                    'billing' => 'billingAddress',
+                    default => null,
+                };
+
+                if (blank($addressType)) {
+                    $action->failureNotificationTitle(__('lunarpanel::order.action.edit_address.notification.error'));
+                    $action->failure();
+
+                    $action->halt();
+
+                    return;
+                }
+
+                $oldData = $record->$addressType->toArray();
+                $formFields = array_keys($data);
+
+                $record->$addressType->order_id = $record->id;
+                $record->$addressType->update($data);
+                $refreshed = $record->$addressType->refresh();
+
+                // should this be in Core as model observer?
+                $diff = collect($oldData)->only($formFields)->diff(collect($refreshed->toArray())->only($formFields));
+                if ($diff->count()) {
+                    activity()
+                        ->causedBy(Filament::auth()->user())
+                        ->performedOn($record)
+                        ->event('order-address-update')
+                        ->withProperties([
+                            'fields' => $formFields,
+                            'type' => $addressType,
+                            'new' => $record->$addressType->toArray(),
+                            'previous' => $oldData,
+                        ])->log('order-address-update');
+                }
+
+                $action->successNotificationTitle(__("lunarpanel::order.action.edit_address.notification.{$type}_address.saved"));
+
+                $action->success();
+            })
+            ->slideOver();
+    }
+}
