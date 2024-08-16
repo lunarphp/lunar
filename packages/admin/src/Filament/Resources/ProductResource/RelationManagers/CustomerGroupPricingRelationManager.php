47497ade4c66df67fd0fbca81917cd22b08ebdad
alecritson <alecritson@users.noreply.github.com>
2024-08-16 09:40:52 +0000

    chore: fix code style

--- packages/admin/src/Filament/Resources/ProductResource/RelationManagers/CustomerGroupPricingRelationManager.php
+++ packages/admin/src/Filament/Resources/ProductResource/RelationManagers/CustomerGroupPricingRelationManager.php
@@ -19,6 +19,7 @@ use Lunar\Models\Price;
 class CustomerGroupPricingRelationManager extends BaseRelationManager
 {
     protected static bool $isLazy = false;
+
     protected static string $relationship = 'prices';
 
     public static function getTitle(Model $ownerRecord, string $pageClass): string
