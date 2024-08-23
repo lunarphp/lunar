e21a17e98eca0b4ae5f3eebbed67cc77b8d32e06
alecritson <alecritson@users.noreply.github.com>
2024-08-23 07:01:56 +0000

    chore: fix code style

--- packages/admin/src/Filament/Resources/TaxRateResource/RelationManagers/TaxRateAmountRelationManager.php
+++ packages/admin/src/Filament/Resources/TaxRateResource/RelationManagers/TaxRateAmountRelationManager.php
@@ -59,7 +59,7 @@ class TaxRateAmountRelationManager extends RelationManager
                 ),
             ])->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make()
+                Tables\Actions\DeleteAction::make(),
             ]);
     }
 }
