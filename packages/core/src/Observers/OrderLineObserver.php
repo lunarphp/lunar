abfdeb51374387d58c75484fa7720a33ebe97927
wychoong <67364036+wychoong@users.noreply.github.com>
2024-09-23 13:11:06 +0800

    Update OrderLineObserver.php

--- packages/core/src/Observers/OrderLineObserver.php
+++ packages/core/src/Observers/OrderLineObserver.php
@@ -32,8 +32,12 @@ class OrderLineObserver
      */
     public function updating(OrderLine $orderLine)
     {
-        if (! in_array(Purchasable::class, class_implements($orderLine->purchasable_type, true))) {
-            throw new NonPurchasableItemException($orderLine->purchasable_type);
+        $purchasableModel = class_exists($orderLine->purchasable_type) ?
+            $orderLine->purchasable_type :
+            Relation::getMorphedModel($orderLine->purchasable_type);
+
+        if (! $purchasableModel || ! in_array(Purchasable::class, class_implements($purchasableModel, true))) {
+            throw new NonPurchasableItemException($purchasableModel);
         }
     }
 }
