6a4bd971f2c9ab2f4272759f3acd523eb8354e76
Alec Ritson <hello@itsalec.co.uk>
2024-10-04 12:30:32 +0100

    Fixes

--- packages/search/src/SearchServiceProvider.php
+++ packages/search/src/SearchServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Lunar\Search;
 
 use Illuminate\Support\ServiceProvider;
-use Lunar\Search\Contracts\InstantSearchContract;
 use Lunar\Search\Contracts\SearchManagerContract;
 
 class SearchServiceProvider extends ServiceProvider
@@ -11,7 +10,6 @@ class SearchServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(SearchManagerContract::class, fn ($app) => $app->make(SearchManager::class));
-        $this->app->singleton(InstantSearchContract::class, fn ($app) => $app->make(InstantSearch::class));
     }
 
     public function boot()
