bc06fc1dc0f8591f46a869b133c28b16226f97b7
Alec Ritson <hello@itsalec.co.uk>
2024-10-08 07:55:18 +0100

    Update search.php

--- packages/search/config/search.php
+++ packages/search/config/search.php
@@ -3,6 +3,8 @@
 return [
     'facets' => [
         \Lunar\Models\Product::class => [
+            'brand' => [],
+            'size' => [],
             'colour' => [
                 'Red' => [
                     'hex_value' => '#FF0000',
