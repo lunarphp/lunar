f5bc123c1774b83b1976239c02ba7575132752ef
Glenn Jacobs <glenn@neondigital.co.uk>
2025-03-14 11:51:13 +0000

    Update CartSessionManager.php

--- packages/core/src/Managers/CartSessionManager.php
+++ packages/core/src/Managers/CartSessionManager.php
@@ -20,7 +20,7 @@ class CartSessionManager implements CartSessionInterface
         protected AuthManager $authManager,
         protected Channel $channel,
         protected Currency $currency,
-        public Cart $cart,
+        public ?Cart $cart = null,
     ) {
         //
     }
@@ -75,7 +75,7 @@ class CartSessionManager implements CartSessionInterface
             );
         }
 
-        $this->cart = $this->createNewCart();
+        $this->cart = null;
 
         $this->sessionManager->forget('shipping_estimate_meta');
         $this->sessionManager->forget(
