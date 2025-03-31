6be1aa9cefd55e2eca82c13eeabea645c9e93c76
Alec Ritson <hello@itsalec.co.uk>
2025-03-31 13:31:11 +0100

    Tests

--- tests/admin/Feature/Filament/Resources/ProductResource/Pages/EditProductTest.php
+++ tests/admin/Feature/Filament/Resources/ProductResource/Pages/EditProductTest.php
@@ -62,3 +62,53 @@ it('can edit variant attributes', function () {
 
     expect($variant->refresh()->attr($attribute->handle))->toBe('Hello');
 });
+
+it('can save attributes', function () {
+    \Lunar\Models\Language::factory()->create([
+        'default' => true,
+    ]);
+
+    $record = \Lunar\Models\Product::factory()->create();
+
+    $group = \Lunar\Models\AttributeGroup::factory()->create([
+        'attributable_type' => 'product',
+        'name' => [
+            'en' => 'Details',
+        ],
+        'handle' => 'details',
+        'position' => 1,
+    ]);
+
+    $attribute = \Lunar\Models\Attribute::factory()->create([
+        'attribute_type' => 'product',
+        'attribute_group_id' => $group->id,
+        'position' => 1,
+        'name' => [
+            'en' => 'Name',
+        ],
+        'handle' => 'name',
+        'section' => 'main',
+        'required' => false,
+        'system' => false,
+        'searchable' => false,
+    ]);
+
+    \Illuminate\Support\Facades\DB::table('lunar_attributables')->insert([
+        'attribute_id' => $attribute->id,
+        'attributable_type' => 'brand',
+        'attributable_id' => $record->id,
+    ]);
+
+    $this->asStaff(admin: true);
+
+    \Livewire\Livewire::test(\Lunar\Admin\Filament\Resources\ProductResource\Pages\EditProduct::class, [
+        'record' => $record->getRouteKey(),
+        'pageClass' => 'productEdit',
+    ])->fillForm([
+        'attribute_data' => [
+            'name' => new \Lunar\FieldTypes\Text('New Product Name'),
+        ],
+    ])->call('save');
+
+    expect($record->refresh()->attr('name'))->toBe('New Product Name');
+});
