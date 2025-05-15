11275578b3e408769c0147df07390dd31bf95963
Andréas Lundgren <adevade@gmail.com>
2025-05-15 14:06:11 +0200

    Add facade docblocks

--- packages/core/src/Facades/StorefrontSession.php
+++ packages/core/src/Facades/StorefrontSession.php
@@ -5,9 +5,26 @@ namespace Lunar\Facades;
 use Illuminate\Support\Facades\Facade;
 use Lunar\Base\StorefrontSessionInterface;
 
+/**
+ * @method static \Lunar\Models\Contracts\Channel getChannel()
+ * @method static \Lunar\Managers\StorefrontSessionManager setChannel(\Lunar\Models\Contracts\Channel $channel)
+ * @method static \Illuminate\Support\Collection<\Lunar\Models\Contracts\CustomerGroup> getCustomerGroups()
+ * @method static \Lunar\Managers\StorefrontSessionManager setCustomerGroups(\Illuminate\Support\Collection<\Lunar\Models\Contracts\CustomerGroup> $customerGroups)
+ * @method static \Lunar\Managers\StorefrontSessionManager setCustomerGroup(\Lunar\Models\Contracts\CustomerGroup $customerGroup)
+ * @method static \Lunar\Models\Contracts\Currency getCurrency()
+ * @method static \Lunar\Managers\StorefrontSessionManager setCurrency(\Lunar\Models\Contracts\Currency $currency)
+ * @method static ?\Lunar\Models\Contracts\Customer getCustomer()
+ * @method static \Lunar\Managers\StorefrontSessionManager setCustomer(\Lunar\Models\Contracts\Customer $customer)
+ * @method static void initChannel()
+ * @method static void initCustomerGroups()
+ * @method static void initCurrency()
+ * @method static void initCustomer()
+ * @method static void forget()
+ * @method static string getSessionKey()
+ */
 class StorefrontSession extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return StorefrontSessionInterface::class;
     }
