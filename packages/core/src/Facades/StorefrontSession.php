4841042778cd5f4e7a9afa99e5c423d6364bc0a3
repl6669 <5587309+repl6669@users.noreply.github.com>
2025-07-02 15:53:32 +0000

    Update facade docblocks

--- packages/core/src/Facades/StorefrontSession.php
+++ packages/core/src/Facades/StorefrontSession.php
@@ -6,21 +6,22 @@ use Illuminate\Support\Facades\Facade;
 use Lunar\Base\StorefrontSessionInterface;
 
 /**
- * @method static void forget()
- * @method static void initCustomerGroups()
- * @method static void initChannel()
- * @method static \Lunar\Models\Contracts\Customer|null initCustomer()
- * @method static string getSessionKey()
- * @method static \Lunar\Managers\StorefrontSessionManager setChannel(\Lunar\Models\Contracts\Channel|string $channel)
- * @method static \Lunar\Managers\StorefrontSessionManager setCustomer(\Lunar\Models\Contracts\Customer $customer)
- * @method static \Lunar\Models\Contracts\Customer|null getCustomer()
+ * @method static \Lunar\Models\Contracts\Channel getChannel()
+ * @method static \Lunar\Managers\StorefrontSessionManager setChannel(\Lunar\Models\Contracts\Channel $channel)
+ * @method static \Illuminate\Support\Collection getCustomerGroups()
  * @method static \Lunar\Managers\StorefrontSessionManager setCustomerGroups(\Illuminate\Support\Collection $customerGroups)
  * @method static \Lunar\Managers\StorefrontSessionManager setCustomerGroup(\Lunar\Models\Contracts\CustomerGroup $customerGroup)
  * @method static \Lunar\Managers\StorefrontSessionManager resetCustomerGroups()
- * @method static \Lunar\Models\Contracts\Channel getChannel()
- * @method static \Illuminate\Support\Collection|null getCustomerGroups()
- * @method static \Lunar\Managers\StorefrontSessionManager setCurrency(\Lunar\Models\Contracts\Currency $currency)
  * @method static \Lunar\Models\Contracts\Currency getCurrency()
+ * @method static \Lunar\Managers\StorefrontSessionManager setCurrency(\Lunar\Models\Contracts\Currency $currency)
+ * @method static \Lunar\Models\Contracts\Customer|null getCustomer()
+ * @method static \Lunar\Managers\StorefrontSessionManager setCustomer(\Lunar\Models\Contracts\Customer $customer)
+ * @method static void initChannel()
+ * @method static void initCustomerGroups()
+ * @method static void initCurrency()
+ * @method static void initCustomer()
+ * @method static void forget()
+ * @method static string getSessionKey()
  *
  * @see \Lunar\Managers\StorefrontSessionManager
  */
