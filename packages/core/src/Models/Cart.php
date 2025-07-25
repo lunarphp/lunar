2cddcba127232f503af97b8619ef8845bee7f561
Author <actions@github.com>
2025-07-25 07:35:00 +0000

    chore: fix code style

--- packages/core/src/Models/Cart.php
+++ packages/core/src/Models/Cart.php
@@ -5,7 +5,6 @@ namespace Lunar\Models;
 use Exception;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\AsArrayObject;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
