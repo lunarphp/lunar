312b340ff5d95e1d2af0158357b3f5ba0f886cc5
Vajda Lehel-Barna <vlehelbarna@gmail.com>
2025-10-05 21:03:30 +0300

    I added the hungarian translations for admin resources

---
+++ packages/admin/resources/lang/hu/activity.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'label' => 'Tevékenység',
+
+    'plural_label' => 'Tevékenységek',
+
+    'table' => [
+        'subject' => 'Tárgy',
+        'description' => 'Leírás',
+        'log' => 'Napló',
+        'logged_at' => 'Rögzítés ideje',
+        'event' => 'Esemény',
+        'logged_from' => 'Naplózva innen',
+        'logged_until' => 'Naplózva eddig',
+    ],
+
+    'form' => [
+        'causer_type' => 'Okozó típusa',
+        'causer_id' => 'Okozó azonosító',
+        'subject_type' => 'Tárgy típusa',
+        'subject_id' => 'Tárgy azonosító',
+        'description' => 'Leírás',
+        'attributes' => 'Attribútumok',
+        'old' => 'Régi',
+    ],
+
+];
---
+++ packages/admin/resources/lang/hu/auth.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    /**
+     * Roles
+     */
+    'roles.admin.label' => 'Admin',
+    'roles.admin.description' => 'Teljes hozzáféréssel rendelkező admin',
+    'roles.staff.label' => 'Munkatárs',
+    'roles.staff.description' => 'Alapvető jogosultságokkal rendelkező munkatárs',
+    /**
+     * Permissions.
+     */
+    'permissions.settings.label' => 'Beállítások',
+    'permissions.settings.description' => 'Hozzáférést ad a Hub beállítási területéhez',
+    'permissions.settings:core.label' => 'Alapbeállítások',
+    'permissions.settings:core.description' => 'Hozzáférés az alapvető áruházbeállításokhoz (csatornák, nyelvek, pénznemek stb.)',
+    'permissions.settings:manage-staff.label' => 'Munkatársak kezelése',
+    'permissions.settings:manage-staff.description' => 'Engedélyezi más munkatársak adatainak szerkesztését',
+    'permissions.settings:manage-attributes.label' => 'Attribútumok kezelése',
+    'permissions.settings:manage-attributes.description' => 'Engedélyezi további attribútumok létrehozását és szerkesztését',
+    'permissions.catalog:manage-products.label' => 'Termékek kezelése',
+    'permissions.catalog:manage-products.description' => 'Engedélyezi a termékek, terméktípusok és márkák szerkesztését',
+    'permissions.catalog:manage-collections.label' => 'Kollekciók kezelése',
+    'permissions.catalog:manage-collections.description' => 'Engedélyezi a kollekciók és azok csoportjainak szerkesztését',
+    'permissions.sales:manage-orders.label' => 'Rendelések kezelése',
+    'permissions.sales:manage-orders.description' => 'Engedélyezi a rendelések kezelését',
+    'permissions.sales:manage-customers.label' => 'Vásárlók kezelése',
+    'permissions.sales:manage-customers.description' => 'Engedélyezi a vásárlók kezelését',
+    'permissions.sales:manage-discounts.label' => 'Kedvezmények kezelése',
+    'permissions.sales:manage-discounts.description' => 'Engedélyezi a kedvezmények kezelését',
+];
