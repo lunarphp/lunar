a2e0395837b195dce34d9639b21b7ffc1cebdf4a
Alec Ritson <hello@itsalec.co.uk>
2025-10-29 08:43:22 +0000

    Add database indexes

---
+++ packages/core/database/migrations/2025_10_29_084000_add_missing_indexes_to_tables.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Lunar\Base\Migration;
+
+return new class extends Migration
+{
+    /**
+     * The list of tables and JSON columns to update.
+     */
+    private $columnsToUpdate = [
+        'channelables' => [
+            'enabled',
+            'channelable_type',
+            'channelable_id',
+            'starts_at',
+            'ends_at',
+        ],
+        'products' => [
+            'deleted_at',
+        ],
+        'product_variants' => [
+            'deleted_at',
+        ],
+    ];
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Only run for PostgreSQL - MySQL has no change and SQLite will error when trying to change
+        if (DB::getDriverName() !== 'pgsql') {
+            return;
+        }
+
+        foreach ($this->columnsToUpdate as $table => $columns) {
+            $fullTableName = $this->getTableName($table);
+
+            Schema::table($fullTableName, function (Blueprint $tableBlueprint) use ($columns) {
+                foreach ($columns as $column) {
+                    $tableBlueprint->index($column);
+                }
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        foreach ($this->columnsToUpdate as $table => $columns) {
+            $fullTableName = $this->getTableName($table);
+
+            Schema::table($fullTableName, function (Blueprint $tableBlueprint) use ($columns) {
+                foreach ($columns as $column) {
+                    $tableBlueprint->dropIndex([$column]);
+                }
+            });
+        }
+    }
+
+    /**
+     * Get the full table name with appropriate prefix.
+     */
+    private function getTableName(string $table): string
+    {
+        return in_array($table, ['activity_log', 'media']) ? $table : $this->prefix.$table;
+    }
+};
