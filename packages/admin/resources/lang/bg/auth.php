2f17af74be8a053f4ed18305d7e0a6c7fe8a4f5a
pavelpalashturov <pa.palashturov@gmail.com>
2025-12-09 17:12:55 +0200

    Add Bulgarian translations

---
+++ packages/admin/resources/lang/bg/auth.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    /**
+     * Roles
+     */
+    'roles.admin.label' => 'Админ',
+    'roles.admin.description' => 'Админ с пълен достъп',
+    'roles.staff.label' => 'Персонал',
+    'roles.staff.description' => 'Персонал с основен достъп',
+    /**
+     * Permissions.
+     */
+    'permissions.settings.label' => 'Настройки',
+    'permissions.settings.description' => 'Дава достъп до зоната с настройки на хъба',
+    'permissions.settings:core.label' => 'Основни настройки',
+    'permissions.settings:core.description' => 'Достъп до основни настройки на магазина, като канали, езици, валути и др.',
+    'permissions.settings:manage-staff.label' => 'Управление на персонал',
+    'permissions.settings:manage-staff.description' => 'Позволява на член на персонала да редактира други служители',
+    'permissions.settings:manage-attributes.label' => 'Управление на атрибути',
+    'permissions.settings:manage-attributes.description' => 'Позволява на член на персонала да редактира и създава допълнителни атрибути',
+    'permissions.catalog:manage-products.label' => 'Управление на продукти',
+    'permissions.catalog:manage-products.description' => 'Позволява на член на персонала да редактира продукти, типове продукти и марки',
+    'permissions.catalog:manage-collections.label' => 'Управление на колекции',
+    'permissions.catalog:manage-collections.description' => 'Позволява на член на персонала да редактира колекции и техните групи',
+    'permissions.sales:manage-orders.label' => 'Управление на поръчки',
+    'permissions.sales:manage-orders.description' => 'Позволява на член на персонала да управлява поръчки',
+    'permissions.sales:manage-customers.label' => 'Управление на клиенти',
+    'permissions.sales:manage-customers.description' => 'Позволява на член на персонала да управлява клиенти',
+    'permissions.sales:manage-discounts.label' => 'Управление на отстъпки',
+    'permissions.sales:manage-discounts.description' => 'Позволява на член на персонала да управлява отстъпки',
+];
---
+++ packages/admin/resources/lang/bg/language.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'label' => 'Език',
+
+    'plural_label' => 'Езици',
+
+    'table' => [
+        'name' => [
+            'label' => 'Име',
+        ],
+        'code' => [
+            'label' => 'Код',
+        ],
+        'default' => [
+            'label' => 'По подразбиране',
+        ],
+    ],
+
+    'form' => [
+        'name' => [
+            'label' => 'Име',
+        ],
+        'code' => [
+            'label' => 'Код',
+        ],
+        'default' => [
+            'label' => 'По подразбиране',
+        ],
+    ],
+
+];
