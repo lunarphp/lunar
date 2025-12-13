c162aab5bbe51f073c165bec31c502f829cabca3
Omar Elshopky <omarhelshopky@gmail.com>
2025-12-13 04:14:39 +0000

    Add admin hub package arabic translations

---
+++ packages/admin/resources/lang/ar/user.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'label' => 'مستخدم',
+
+    'plural_label' => 'المستخدمين',
+
+    'table' => [
+        'name' => [
+            'label' => 'الاسم',
+        ],
+        'email' => [
+            'label' => 'الايميل',
+        ],
+    ],
+
+    'form' => [
+        'email' => [
+            'label' => 'الايميل',
+        ],
+        'password' => [
+            'label' => 'كلمة المرور الجديدة',
+        ],
+        'password_confirmation' => [
+            'label' => 'تأكيد كلمة المرور الجديدة',
+        ],
+    ],
+];
