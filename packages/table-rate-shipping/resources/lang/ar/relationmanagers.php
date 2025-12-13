2a9d8179b850a5a31c71ca91fc1a4684f0bb0a33
Omar Elshopky <omarhelshopky@gmail.com>
2025-12-13 05:05:27 +0000

    Add table-rate-shipping package arabic translations

---
+++ packages/table-rate-shipping/resources/lang/ar/relationmanagers.php
@@ -0,0 +1,87 @@
+<?php
+
+return [
+    'shipping_methods' => [
+        'customer_groups' => [
+            'description' => "اربط مجموعات العملاء بطريقة الشحن هذه لتحديد توافرها.",
+        ],
+    ],
+    'shipping_rates' => [
+        'title_plural' => 'رسوم الشحن',
+        'actions' => [
+            'create' => [
+                'label' => 'إنشاء سعر شحن',
+            ],
+        ],
+        'notices' => [
+            'prices_incl_tax' => 'جميع الأسعار تشمل الضريبة، والتي سيتم أخذها في الاعتبار عند حساب الحد الأدنى للإنفاق.',
+            'prices_excl_tax' => 'جميع الأسعار لا تشمل الضريبة، وسيتم حساب الحد الأدنى للإنفاق استنادًا إلى المجموع الفرعي للسلة.',
+        ],
+        'form' => [
+            'shipping_method_id' => [
+                'label' => 'طريقة الشحن',
+            ],
+            'price' => [
+                'label' => 'السعر',
+            ],
+            'prices' => [
+                'label' => 'شرائح الأسعار',
+                'repeater' => [
+                    'customer_group_id' => [
+                        'label' => 'مجموعة العملاء',
+                        'placeholder' => 'أي',
+                    ],
+                    'currency_id' => [
+                        'label' => 'العملة',
+                    ],
+                    'min_spend' => [
+                        'label' => 'الحد الأدنى للإنفاق',
+                    ],
+                    'min_weight' => [
+                        'label' => 'الحد الأدنى للوزن',
+                    ],
+                    'price' => [
+                        'label' => 'السعر',
+                    ],
+                ],
+            ],
+        ],
+        'table' => [
+            'enabled' => [
+                'label' => 'مفعّل',
+            ],
+            'disabled' => [
+                'label' => 'معطّل',
+            ],
+            'shipping_method' => [
+                'label' => 'طريقة الشحن',
+                'disabled' => 'معطّل',
+            ],
+            'price' => [
+                'label' => 'السعر',
+            ],
+            'price_breaks_count' => [
+                'label' => 'عدد الشرائح',
+            ],
+        ],
+    ],
+    'exclusions' => [
+        'title_plural' => 'استثناءات الشحن',
+        'form' => [
+            'purchasable' => [
+                'label' => 'المنتج',
+            ],
+        ],
+        'actions' => [
+            'create' => [
+                'label' => 'إضافة قائمة استثناءات الشحن',
+            ],
+            'attach' => [
+                'label' => 'إضافة قائمة استثناء',
+            ],
+            'detach' => [
+                'label' => 'إزالة',
+            ],
+        ],
+    ],
+];
