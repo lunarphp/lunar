7799aaa88c6786a253fe2613a5e85f5e305a23e4
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-04-13 15:24:00 +0330

    Add Persian (fa) language translations for admin package. (#2385)
    Add Persian (fa) language translations for all packages.
    
    ---------
    
    Co-authored-by: Alec Ritson <hello@itsalec.co.uk>

---
+++ packages/admin/resources/lang/fa/activity.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'label' => 'Activity',
+
+    'plural_label' => 'Activities',
+
+    'table' => [
+        'subject' => 'Subject',
+        'description' => 'Description',
+        'log' => 'Log',
+        'logged_at' => 'Logged At',
+        'event' => 'Event',
+        'logged_from' => 'Logged From',
+        'logged_until' => 'Logged Until',
+    ],
+
+    'form' => [
+        'causer_type' => 'Causer Type',
+        'causer_id' => 'Causer Id',
+        'subject_type' => 'Subject type',
+        'subject_id' => 'Subject Id',
+        'description' => 'Description',
+        'attributes' => 'Attributes',
+        'old' => 'Old',
+    ],
+
+];
