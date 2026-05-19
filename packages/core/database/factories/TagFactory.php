f54f7cd913fb13eff3f07d07e379a559afec756d
Glenn Jacobs <glenn@neondigital.co.uk>
2026-05-19 09:53:43 +0100

    Tweaks

--- packages/core/database/factories/TagFactory.php
+++ packages/core/database/factories/TagFactory.php
@@ -2,7 +2,6 @@
 
 namespace Lunar\Database\Factories;
 
-use Illuminate\Support\Str;
 use Lunar\Models\Tag;
 
 class TagFactory extends BaseFactory
@@ -12,7 +11,7 @@ class TagFactory extends BaseFactory
     public function definition(): array
     {
         return [
-            'value' => Str::upper($this->faker->word),
+            'value' => $this->faker->word,
         ];
     }
 }
