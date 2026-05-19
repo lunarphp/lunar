75148a653bfdc652180954c547d773c6018b5fd6
wychoong <67364036+wychoong@users.noreply.github.com>
2026-05-19 12:38:17 +0800

    improve product option indexer

--- tests/core/Unit/Search/ProductOptionIndexerTest.php
+++ tests/core/Unit/Search/ProductOptionIndexerTest.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Lunar\Models\ProductOption;
+use Lunar\Models\ProductOptionValue;
 use Lunar\Search\ProductOptionIndexer;
 use Lunar\Tests\Core\TestCase;
 
@@ -12,8 +13,16 @@ uses(RefreshDatabase::class);
 test('can return correct searchable data', function () {
     $productOption = ProductOption::factory()->create();
 
+    $value = ProductOptionValue::factory()->create([
+        'product_option_id' => $productOption->id,
+        'name' => [
+            'en' => 'Small',
+        ],
+    ]);
+
     $data = app(ProductOptionIndexer::class)->toSearchableArray($productOption);
 
     expect($data['name_en'])->toEqual($productOption->name->en)
-        ->and($data['label_en'])->toEqual($productOption->label->en);
+        ->and($data['label_en'])->toEqual($productOption->label->en)
+        ->and($data['option_'.$value->id.'_en'])->toEqual('Small');
 });
